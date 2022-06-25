b90b74c60148b7774f13fbdc68a3beb6b050ac91
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2022-06-25 15:51:44 +0530

    Update 20.Valid_Parentheses.cpp

--- LeetCode/20.Valid_Parentheses.cpp
+++ LeetCode/20.Valid_Parentheses.cpp
@@ -1,38 +1,46 @@
 // 20. Valid Parentheses
 
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
     bool isValid(string s) {
+        
         ios_base::sync_with_stdio(false);
         cin.tie(NULL);
+        
         stack<char> st;
         bool ans = true;
         int i = 0;
+        
         while(s.length()!=0 && i<s.length()) {
+            
+            // find any open parentheses then push it to the stack
             if(s[i]=='(' || s[i]=='[' || s[i]=='{') {
                 st.push(s[i]);
             }
+            
+            // find any closing parentheses then compare it with the top element of the satck
             else if(!st.empty() && s[i]==')'){
                 if(st.top()!='(') {
                     return false;
                 }
                 st.pop();
             }
+            
             else if(!st.empty() && s[i]==']'){
                 if(st.top()!='[') {
                     return false;
                 }
                 st.pop();
             }
+            
             else if(!st.empty() && s[i]=='}'){
                 if(st.top()!='{') {
                     return false;
                 }
                 st.pop();
-            } else {
+            } 
+            
+            else {
                 st.push(s[i]);
             }
             i++;
@@ -43,9 +51,3 @@ public:
         return ans;
     }
 };
-
-int main() {
-    Solution s1;
-    cout<<s1.isValid(")")<<endl;
-    return 0;
-}

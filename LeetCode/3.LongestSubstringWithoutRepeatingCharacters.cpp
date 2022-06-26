8cee865987ec5e920975676124fa9682cea18307
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2022-06-27 00:07:08 +0530

    Add files via upload

---
+++ LeetCode/3.LongestSubstringWithoutRepeatingCharacters.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+        
+        vector<int> letters(256, -1);
+        int maxLen = 0, start = -1;
+        
+        for(int i=0; i<s.length(); i++) {
+            if(letters[s[i]]>start)
+                start = letters[s[i]];
+            letters[s[i]] = i;
+            maxLen = max(maxLen, i-start);
+        }
+        return maxLen;
+    }
+};

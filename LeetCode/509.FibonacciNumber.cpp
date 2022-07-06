372dc8c6718ca0731bd3675849c58b8141eda9ee
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2022-07-06 16:44:34 +0530

    Update 509.FibonacciNumber.cpp

--- LeetCode/509.FibonacciNumber.cpp
+++ LeetCode/509.FibonacciNumber.cpp
@@ -1,1 +1,25 @@
+// 509. Fibonacci Number
 
+class Solution
+{
+public:
+    int fib(int n) {
+
+        ios_base::sync_with_stdio(false);
+        cin.tie(NULL);
+
+        if (n == 1 || n == 0) {
+            return n;
+        }
+        else {
+            int a = 0, b = 1, c = 1;
+            for (int i = 2; i <= n; i++) {
+                c = a + b;
+                a = b;
+                b = c;
+            }
+            return c;
+        }
+        return -1;
+    }
+};

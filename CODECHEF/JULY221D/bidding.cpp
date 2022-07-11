8bb7647a110cf3fc9b0fb3d005e0edf9bfff5729
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2022-07-11 18:09:03 +0530

    Add files via upload

---
+++ CODECHEF/JULY221D/bidding.cpp
@@ -0,0 +1,24 @@
+// Bidding
+// Problem Code:AUCTION
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+        if(a>=b && a>=c) {
+            cout << "Alice" << endl;
+        }
+        else if (b >= c) {
+            cout << "Bob" << endl;
+        }
+        else {
+            cout << "Charlie" << endl;
+        }
+    }
+    return 0;
+}

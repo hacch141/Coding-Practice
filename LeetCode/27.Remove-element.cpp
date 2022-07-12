3c7477434d1283433d1259eb28270080eaff8e35
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2022-07-12 12:18:44 +0530

    Create 27.Remove-element.cpp

---
+++ LeetCode/27.Remove-element.cpp
@@ -0,0 +1,16 @@
+// 27. Remove Element
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        vector<int> ans;
+        for(int i=0; i<nums.size(); i++) {
+            if(nums[i]==val) {
+                continue;
+            }
+            ans.push_back(nums[i]);
+        }
+        nums = ans;
+        return ans.size();
+    }
+};

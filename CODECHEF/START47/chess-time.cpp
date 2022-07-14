50f21ef512c4c3cbcd374323735217bbc7b97941
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2022-07-14 23:45:07 +0530

    Create chess-time.cpp

---
+++ CODECHEF/START47/chess-time.cpp
@@ -0,0 +1,17 @@
+// Chess Time
+// Problem Code:CHESSTIME
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int n;
+        cin >> n;
+        int availableMins = n*60;
+        cout << availableMins/20 << endl;
+    }
+    return 0;
+}

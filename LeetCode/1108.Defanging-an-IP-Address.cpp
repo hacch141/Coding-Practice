5957fab376077e5feb3bf04bebb30600ba7a1305
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2022-07-19 23:59:49 +0530

    Create 1108.Defanging-an-IP-Address.cpp

---
+++ LeetCode/1108.Defanging-an-IP-Address.cpp
@@ -0,0 +1,17 @@
+// 1108. Defanging an IP Address
+
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string ans = "";
+        for(int i=0; i<address.length(); i++) {
+            if(address[i] == '.') {
+                ans += "[.]";
+            }
+            else {
+                ans += address[i];
+            }
+        }
+        return ans;
+    }
+};

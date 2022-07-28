a2e0a144ecb42e0e1c23da1d17bcf7a6896df3b8
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2022-07-28 22:04:31 +0530

    Add files via upload

---
+++ CODECHEF/JULY222/Insurance.cpp
@@ -0,0 +1,21 @@
+// Insurance
+// Problem Code:INSURANCE
+
+#include "bits/stdc++.h"
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int X, Y;
+        cin >> X >> Y;
+        if(Y<=X) {
+            cout << Y << endl;
+        }
+        else {
+            cout << X << endl;
+        }
+    }
+    return 0;
+}

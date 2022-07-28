0d6689d0cb2e8915e6fc19509dfd76fc97a00aa4
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2022-07-28 22:05:26 +0530

    Create Chef-in-his-Office.cpp

---
+++ CODECHEF/JULY222/Chef-in-his-Office.cpp
@@ -0,0 +1,15 @@
+// Chef in his Office
+
+#include "bits/stdc++.h"
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int X, Y;
+        cin >> X >> Y;
+        cout << (X*4)+Y << endl;
+    }
+    return 0;
+}

8e97c0e1eb1ceb9685f56032ba5f5fb9771d3bc0
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2022-08-03 09:47:42 +0530

    Create 191.Number-of-1-Bits.cpp

---
+++ LeetCode/191.Number-of-1-Bits.cpp
@@ -0,0 +1,13 @@
+// 191. Number of 1 Bits
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int cnt = 0;
+        while(n>0) {
+            cnt++;
+            n = n&(n-1);
+        }
+        return cnt;
+    }
+};

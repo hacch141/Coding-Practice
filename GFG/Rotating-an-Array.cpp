4a5403ff1ac7ce9cef8a3258eb5604051d0dc8f5
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2022-08-05 18:58:13 +0530

    Create Rotating-an-Array.cpp

---
+++ GFG/Rotating-an-Array.cpp
@@ -0,0 +1,53 @@
+// Rotating an Array
+
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    void leftRotate(int arr[], int n, int d) {
+        // code here
+        int temp[d];
+        for(int i=0; i<d; i++) {
+            temp[i] = arr[i];
+        }
+        for(int j=d; j<n; j++) {
+            arr[j-d] = arr[j];
+        }
+        for(int i=0; i<d; i++) {
+            arr[n-d+i] = temp[i];
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, d;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        cin >> d;
+        Solution ob;
+        ob.leftRotate(arr, n, d);
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+
+
+
+// } Driver Code Ends

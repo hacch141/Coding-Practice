442d3253f9a41d5bd495da8707ce1b498c08056b
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2022-08-05 19:01:23 +0530

    Update Rotating-an-Array.cpp

--- GFG/Rotating-an-Array.cpp
+++ GFG/Rotating-an-Array.cpp
@@ -24,6 +24,21 @@ public:
             arr[n-d+i] = temp[i];
         }
     }
+    
+    void leftRotate(int arr[], int n, int d) { //2nd Method with less aux space
+        // code here
+        reverse(arr, 0, d-1);
+        reverse(arr, d, n-1);
+        reverse(arr, 0, n-1);
+    }
+    
+    void reverse(int arr[], int low, int high) {
+        while(low<high) {
+            swap(arr[low],arr[high]);
+            low++;
+            high--;
+        }
+    }
 };
 
 //{ Driver Code Starts.

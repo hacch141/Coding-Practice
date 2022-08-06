07247ff67954e8145fa283f8806e685f6b135416
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2022-08-06 22:35:27 +0530

    Update 566.Reshape-the-Matrix.cpp

--- LeetCode/566.Reshape-the-Matrix.cpp
+++ LeetCode/566.Reshape-the-Matrix.cpp
@@ -12,4 +12,24 @@ public:
         }
         return ans;
     }
+    
+//     vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {  // 2nd method
+//         if((mat.size()*mat[0].size()) != (r*c)) return mat;
+//         int row = 0;
+//         int col = 1;
+//         vector<vector<int>> ans(r);
+//         for(int i=0; i<mat.size(); i++) {
+//             for(int j=0; j<mat[0].size(); j++) {
+//                 ans[row].push_back(mat[i][j]);
+//                 if(col<c) {
+//                     col++;
+//                 }
+//                 else {
+//                     row++;
+//                     col = 1;
+//                 }
+//             }
+//         }
+//         return ans;
+//     }
 };

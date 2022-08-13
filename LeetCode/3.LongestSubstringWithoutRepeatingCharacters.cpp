b549878719359292f3fb2d19bf1f9f222b63feb2
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2022-08-14 02:26:50 +0530

    Update 3.LongestSubstringWithoutRepeatingCharacters.cpp

--- LeetCode/3.LongestSubstringWithoutRepeatingCharacters.cpp
+++ LeetCode/3.LongestSubstringWithoutRepeatingCharacters.cpp
@@ -2,20 +2,35 @@
 
 class Solution {
 public:
+    
     int lengthOfLongestSubstring(string s) {
+        vector<int> prev(256, -1);
+        int n = s.length();
+        int ans = 0;
+        int i = 0;
         
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        
-        vector<int> letters(256, -1);
-        int maxLen = 0, start = -1;
-        
-        for(int i=0; i<s.length(); i++) {
-            if(letters[s[i]]>start)
-                start = letters[s[i]];
-            letters[s[i]] = i;
-            maxLen = max(maxLen, i-start);
+        for(int j=0; j<s.length(); j++) {
+            i = max(i,prev[s[j]]+1);
+            int maxend = j-i+1;
+            ans = max(ans,maxend);
+            prev[s[j]] = j;
         }
-        return maxLen;
-    }
+        return ans;
+    
+//     int lengthOfLongestSubstring(string s) { // 2nd Method
+        
+//         ios_base::sync_with_stdio(false);
+//         cin.tie(NULL);
+        
+//         vector<int> letters(256, -1);
+//         int maxLen = 0, start = -1;
+        
+//         for(int i=0; i<s.length(); i++) {
+//             if(letters[s[i]]>start)
+//                 start = letters[s[i]];
+//             letters[s[i]] = i;
+//             maxLen = max(maxLen, i-start);
+//         }
+//         return maxLen;
+//     }
 };

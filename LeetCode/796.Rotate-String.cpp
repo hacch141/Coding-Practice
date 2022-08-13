98e1b6ecf22ccd30e8087227338c686f6df4c110
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2022-08-13 19:30:01 +0530

    Create 796.Rotate-String.cpp

---
+++ LeetCode/796.Rotate-String.cpp
@@ -0,0 +1,11 @@
+// 796. Rotate String
+
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        if(s.length() != goal.length()) {
+            return false;
+        }
+        return ((s+s).find(goal) != string::npos);
+    }
+};

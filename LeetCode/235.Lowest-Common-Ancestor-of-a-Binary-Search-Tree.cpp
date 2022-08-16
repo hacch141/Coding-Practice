9c9ac5acedc3d1ae3b4c7df957e21118eb94b83a
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2022-08-16 19:55:43 +0530

    Create 235.Lowest-Common-Ancestor-of-a-Binary-Search-Tree.cpp

---
+++ LeetCode/235.Lowest-Common-Ancestor-of-a-Binary-Search-Tree.cpp
@@ -0,0 +1,30 @@
+// 235. Lowest Common Ancestor of a Binary Search Tree
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        TreeNode* curr = root;
+        while(curr) {
+            if((curr->val > p->val) && (curr->val > q->val)) {
+                curr = curr->left;
+            }
+            else if((curr->val < p->val) && (curr->val < q->val)) {
+                curr = curr->right;
+            }
+            else {
+                return curr;
+            }
+        }
+        return NULL;
+    }
+};

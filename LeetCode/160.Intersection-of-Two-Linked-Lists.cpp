c37646120a4ccaabd8c247915547bad48c8643e2
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2022-08-19 23:25:54 +0530

    Update 160.Intersection-of-Two-Linked-Lists.cpp

--- LeetCode/160.Intersection-of-Two-Linked-Lists.cpp
+++ LeetCode/160.Intersection-of-Two-Linked-Lists.cpp
@@ -10,18 +10,51 @@
  */
 class Solution {
 public:
+    
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        unordered_map<ListNode*,int> mp;
-        while(headA != NULL) {
-            mp[headA]++;
-            headA = headA->next;
+        int ll1Len = 0;
+        int ll2Len = 0;
+        ListNode* curr1 = headA;
+        ListNode* curr2 = headB;
+        while(curr1 != NULL) {
+            ll1Len++;
+            curr1 = curr1->next;
         }
-        while(headB != NULL) {
-            if(mp[headB]>0) {
-                return headB;
+        while(curr2 != NULL) {
+            ll2Len++;
+            curr2 = curr2->next;
+        }
+        
+        int len = abs(ll1Len-ll2Len);
+        if(ll1Len > ll2Len) {
+            for(int i=0; i<len; i++) {
+                headA = headA->next;
             }
+        }
+        else {
+            for(int i=0; i<len; i++) {
+                headB = headB->next;
+            }
+        }
+        while(headA != headB) {
+            headA = headA->next;
             headB = headB->next;
         }
-        return NULL;
+        return headA;
+    }
+    
+//     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {  // 2nd Method
+//         unordered_map<ListNode*,int> mp;
+//         while(headA != NULL) {
+//             mp[headA]++;
+//             headA = headA->next;
+//         }
+//         while(headB != NULL) {
+//             if(mp[headB]>0) {
+//                 return headB;
+//             }
+//             headB = headB->next;
+//         }
+//         return NULL;
     }
 };

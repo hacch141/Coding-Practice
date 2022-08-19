725a23cbde8a272df9208115801db89f94c7b4fd
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2022-08-20 02:49:52 +0530

    Update 21.Merge-Two-Sorted-Lists.cpp

--- LeetCode/21.Merge-Two-Sorted-Lists.cpp
+++ LeetCode/21.Merge-Two-Sorted-Lists.cpp
@@ -12,6 +12,7 @@
  */
 class Solution {
 public:
+    
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
         if(list1 == NULL) {
             return list2;
@@ -19,15 +20,54 @@ public:
         if(list2 == NULL) {
             return list1;
         }
-        ListNode* start = NULL;
-        if(list1->val > list2->val) {
-            start = list2;
-            start->next = mergeTwoLists(list1,list2->next);
+        ListNode* head = NULL;
+        ListNode* tail = NULL;
+        if(list1->val < list2->val) {
+            head = tail = list1;
+            list1 = list1->next;
         }
         else {
-            start = list1;
-            start->next = mergeTwoLists(list1->next,list2);
+            head = tail = list2;
+            list2 = list2->next;
         }
-        return start;
+        while(list1 != NULL && list2 != NULL) {
+            if(list1->val < list2->val) {
+                tail->next = list1;
+                tail = list1;
+                list1 = list1->next;
+            }
+            else {
+                tail->next = list2;
+                tail = list2;
+                list2 = list2->next;
+            }
+        }
+        if(list1 == NULL) {
+            tail->next = list2;
+        }
+        else {
+            tail->next = list1;
+        }
+        return head;
     }
+
+    
+//     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {  //  2nd Method
+//         if(list1 == NULL) {
+//             return list2;
+//         }
+//         if(list2 == NULL) {
+//             return list1;
+//         }
+//         ListNode* start = NULL;
+//         if(list1->val > list2->val) {
+//             start = list2;
+//             start->next = mergeTwoLists(list1,list2->next);
+//         }
+//         else {
+//             start = list1;
+//             start->next = mergeTwoLists(list1->next,list2);
+//         }
+//         return start;
+//     }
 };

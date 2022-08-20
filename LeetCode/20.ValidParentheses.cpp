ffda6c23a77f3543d802b682a802084266d293f3
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2022-08-20 12:55:15 +0530

    Update 20.ValidParentheses.cpp

--- LeetCode/20.ValidParentheses.cpp
+++ LeetCode/20.ValidParentheses.cpp
@@ -2,52 +2,27 @@
 
 class Solution {
 public:
+    
+    bool matching(char a, char b) {
+        return (a == '{' && b == '}') || (a == '[' && b == ']') || (a == '(' && b == ')');
+    }
+    
     bool isValid(string s) {
-        
-        ios_base::sync_with_stdio(false);
-        cin.tie(NULL);
-        
         stack<char> st;
-        bool ans = true;
-        int i = 0;
-        
-        while(i < s.length()) {
-            
-            // find any open parentheses then push it to the stack
-            if(s[i]=='(' || s[i]=='[' || s[i]=='{') {
+        for(int i=0; i<s.length(); i++) {
+            if(s[i] == '{' || s[i] == '[' || s[i] == '(') {
                 st.push(s[i]);
             }
-            
-            // find any closing parentheses then compare it with the top element of the satck
-            else if(!st.empty() && s[i]==')'){
-                if(st.top()!='(') {
-                    return false;
-                }
-                st.pop();
-            }
-            
-            else if(!st.empty() && s[i]==']'){
-                if(st.top()!='[') {
-                    return false;
-                }
-                st.pop();
-            }
-            
-            else if(!st.empty() && s[i]=='}'){
-                if(st.top()!='{') {
-                    return false;
-                }
-                st.pop();
-            } 
-            
             else {
-                st.push(s[i]);
+                if(st.empty()) {
+                    return false;
+                }
+                else if(matching(st.top(),s[i]) == false) {
+                    return false;
+                }
+            st.pop();
             }
-            i++;
         }
-        if(!st.empty()) {
-            ans = false;
-        }
-        return ans;
+        return st.empty();
     }
 };

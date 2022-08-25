71779bd0d168c3660f861ad360258518a414b3f8
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2022-08-26 00:23:12 +0530

    Create 890.Find-and-Replace-Pattern.cpp

---
+++ LeetCode/890.Find-and-Replace-Pattern.cpp
@@ -0,0 +1,25 @@
+// 890. Find and Replace Pattern
+
+class Solution {
+public:
+    vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
+        vector<string> ans;
+        
+        int freqPar[26] = {0};
+        int n = pattern.length();
+        for(int i=0; i<words.size(); i++) {
+            string s = words[i];
+            int freqword[26] = {0};   
+            int k;
+            for(k=0; k<n; k++) {
+                if(pattern.find(pattern[k]) != s.find(s[k])) {
+                    break;
+                }
+            }
+            if(k == n) {
+                ans.push_back(s);   
+            }
+        }
+        return ans;
+    }
+};

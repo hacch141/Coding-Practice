434c8bbcdcd9de5dc253d63efceacf26eb89c157
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2022-09-20 10:42:43 +0530

    Update Diameter-of-a-Binary-Tree.cpp

--- GFG/Tree/Diameter-of-a-Binary-Tree.cpp
+++ GFG/Tree/Diameter-of-a-Binary-Tree.cpp
@@ -94,24 +94,46 @@ struct Node
 class Solution {
   public:
     // Function to return the diameter of a Binary Tree.
+    int ans = 0;
+    
     int height(Node* root) {
         if(root == NULL) {
             return 0;
         }
-        return 1+max(height(root->left),height(root->right));
+        int lh = height(root->left);
+        int rh = height(root->right);
+        ans = max(ans, 1+lh+rh);
+        return 1+max(lh,rh);
     }
     
     int diameter(Node* root) {
         // Your code here
-        if(root == NULL) {
-            return 0;
-        }
-        int d1 = 1+height(root->left)+height(root->right);
-        int d2 = diameter(root->left);
-        int d3 = diameter(root->right);
-        return max(d1, max(d2,d3));
+        height(root);
+        return ans;
     }
 };
+
+// class Solution {   // 2nd Method
+//   public:
+//     // Function to return the diameter of a Binary Tree.
+//     int height(Node* root) {
+//         if(root == NULL) {
+//             return 0;
+//         }
+//         return 1+max(height(root->left),height(root->right));
+//     }
+    
+//     int diameter(Node* root) {
+//         // Your code here
+//         if(root == NULL) {
+//             return 0;
+//         }
+//         int d1 = 1+height(root->left)+height(root->right);
+//         int d2 = diameter(root->left);
+//         int d3 = diameter(root->right);
+//         return max(d1, max(d2,d3));
+//     }
+// };
 
 //{ Driver Code Starts.
 

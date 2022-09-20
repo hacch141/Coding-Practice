e67c35b542ce36646dfb849e7bad7e318953ca8b
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2022-09-20 10:46:04 +0530

    Update 543.Diameter-of-Binary-Tree.cpp

--- LeetCode/543.Diameter-of-Binary-Tree.cpp
+++ LeetCode/543.Diameter-of-Binary-Tree.cpp
@@ -14,20 +14,36 @@
 class Solution {
 public:
     
+    int ans = 0;
     int height(TreeNode* root) {
         if(root == NULL) {
             return 0;
         }
-        return 1+max(height(root->left),height(root->right));
+        int lh = height(root->left);
+        int rh = height(root->right);
+        ans = max(ans,1+lh+rh);
+        return 1+max(lh,rh);
     }
     
     int diameterOfBinaryTree(TreeNode* root) {
-        if(root == NULL) {
-            return 0;
-        }
-        int d1 = height(root->left)+height(root->right);
-        int d2 = diameterOfBinaryTree(root->left);
-        int d3 = diameterOfBinaryTree(root->right);
-        return max(d1,max(d2,d3));
+        height(root);
+        return ans-1;
     }
+    
+//     int height(TreeNode* root) {   // 2nd Method
+//         if(root == NULL) {
+//             return 0;
+//         }
+//         return 1+max(height(root->left),height(root->right));
+//     }
+    
+//     int diameterOfBinaryTree(TreeNode* root) {
+//         if(root == NULL) {
+//             return 0;
+//         }
+//         int d1 = height(root->left)+height(root->right);
+//         int d2 = diameterOfBinaryTree(root->left);
+//         int d3 = diameterOfBinaryTree(root->right);
+//         return max(d1,max(d2,d3));
+//     }
 };

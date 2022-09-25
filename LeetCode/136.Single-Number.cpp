89219a24d9a5d86b4a89eb59f92818cb4e71488e
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2022-09-25 21:24:02 +0530

    Create 136.Single-Number.cpp

---
+++ LeetCode/136.Single-Number.cpp
@@ -0,0 +1,12 @@
+// 136. Single Number
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+        for(auto i : nums) {
+            ans = ans^i;
+        }
+        return ans;
+    }
+};

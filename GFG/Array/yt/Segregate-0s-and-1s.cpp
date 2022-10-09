fc786edf486f935ff1604c618144699ce64e6ca4
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2022-10-10 00:08:19 +0530

    Add files via upload

---
+++ GFG/Array/yt/Segregate-0s-and-1s.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    void segregate0and1(int arr[], int n) {
+        // code here
+        int left = 0;
+        int right = n-1;
+        while(left < right) {
+            while(arr[left] == 0) {
+                left++;
+            }
+            while(arr[right] == 1) {
+                right--;
+            }
+            if(left < right) {
+                arr[left++] = 0;
+                arr[right--] = 1;
+            }
+        }
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        Solution ob;
+        ob.segregate0and1(arr, n);
+        for (int i = 0; i < n; i++) {
+            cout << arr[i] << " ";
+        }
+        cout << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends

11ebdac806e5959da79df294e2975b21ca250e88
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2022-10-10 01:05:42 +0530

    Update Pair-with-given-sum-in-a-sorted-array.cpp

--- GFG/Array/yt/Pair-with-given-sum-in-a-sorted-array.cpp
+++ GFG/Array/yt/Pair-with-given-sum-in-a-sorted-array.cpp
@@ -5,35 +5,50 @@
 using namespace std;
 
 
-
-
 // } Driver Code Ends
 
 class Solution{
   public:
+	
     int Countpair(int arr[], int n, int sum){
         
         // Complete the function
-        int i = 0;
-        int j = n-1;
-        int cnt = 0;
-        
-        while(i<j) {
-            if(arr[i]+arr[j] == sum) {
-                cnt++;
-                i++;
-                j--;
+        int ans = 0;
+        unordered_map<int,int> mp;
+        for(int i=0; i<n; i++) {
+            if(mp.find(sum-arr[i]) != mp.end()) {
+                ans++;
             }
-            else if(arr[i]+arr[j] > sum) {
-                j--;
-            }
-            else if(arr[i]+arr[j] < sum) {
-                i++;
-            }
+            mp[arr[i]]++;
         }
-        return cnt > 0 ? cnt : -1;
-        
+        return ans != 0 ? ans : -1;
     }
+	
+	
+// 	2nd Method
+//     int Countpair(int arr[], int n, int sum){
+        
+//         // Complete the function
+//         int i = 0;
+//         int j = n-1;
+//         int cnt = 0;
+        
+//         while(i<j) {
+//             if(arr[i]+arr[j] == sum) {
+//                 cnt++;
+//                 i++;
+//                 j--;
+//             }
+//             else if(arr[i]+arr[j] > sum) {
+//                 j--;
+//             }
+//             else if(arr[i]+arr[j] < sum) {
+//                 i++;
+//             }
+//         }
+//         return cnt > 0 ? cnt : -1;
+        
+//     }
 };
 
 //{ Driver Code Starts.

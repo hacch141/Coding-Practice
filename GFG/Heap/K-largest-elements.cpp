fc63c969a4794753e72b2f5faa7990d2d4daa2fa
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2022-10-14 10:56:34 +0530

    Update K-largest-elements.cpp

--- GFG/Heap/K-largest-elements.cpp
+++ GFG/Heap/K-largest-elements.cpp
@@ -10,24 +10,45 @@ class Solution
 {
     public:
     //Function to return k largest elements from an array.
+    
     vector<int> kLargest(int arr[], int n, int k)
     {
         // code here
-        priority_queue<int,vector<int>,greater<int>> pq(arr,arr+k);
-        for(int i=k; i<n; i++) {
-            if(pq.top() < arr[i]) {
+        vector<int> ans;
+        priority_queue<int, vector<int>, greater<int>> pq;
+        for(int i=0; i<n; i++) {
+            pq.push(arr[i]);
+            if(pq.size() > k) {
                 pq.pop();
-                pq.push(arr[i]);
             }
         }
-        
-        vector<int> ans(k);
-        for(int i=k-1; i>=0; i--) {
-            ans[i] = pq.top();
+        while(pq.size() != 0) {
+            ans.push_back(pq.top());
             pq.pop();
         }
+        reverse(ans.begin(), ans.end());
         return ans;
     }
+    
+//     2nd Method
+//     vector<int> kLargest(int arr[], int n, int k)
+//     {
+//         // code here
+//         priority_queue<int,vector<int>,greater<int>> pq(arr,arr+k);
+//         for(int i=k; i<n; i++) {
+//             if(pq.top() < arr[i]) {
+//                 pq.pop();
+//                 pq.push(arr[i]);
+//             }
+//         }
+        
+//         vector<int> ans(k);
+//         for(int i=k-1; i>=0; i--) {
+//             ans[i] = pq.top();
+//             pq.pop();
+//         }
+//         return ans;
+//     }
 };
 
 //{ Driver Code Starts.

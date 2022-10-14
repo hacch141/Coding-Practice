3bc91a30c1be818b151ca07ef019d6e65668455d
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2022-10-14 18:32:17 +0530

    Create 56.Merge-Intervals.cpp

---
+++ LeetCode/56.Merge-Intervals.cpp
@@ -0,0 +1,27 @@
+// 56. Merge Intervals
+
+class Solution {
+public:
+    
+    static bool compare(vector<int> v1, vector<int> v2) {
+        return v1[0] < v2[0];
+    }
+    
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(), compare);
+        int j = 0;
+        int n = intervals.size();
+        vector<vector<int>> ans;
+        ans.push_back(intervals[0]);
+        for(int i=1; i<n;i++) {
+            if(intervals[j][1] >= intervals[i][0]) {
+                ans[j][1] = max(intervals[j][1],intervals[i][1]);
+            }
+            else {
+                ans.push_back(intervals[i]);
+                j++;
+            }
+        }
+        return ans;
+    }
+};

a459225c02b483ebcd8d113cab98296ee0de32f7
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2022-12-08 22:21:29 +0530

    Update Topological-sort.cpp

--- GFG/Graph/Topological-sort.cpp
+++ GFG/Graph/Topological-sort.cpp
@@ -2,6 +2,7 @@ class Solution
 {
 	public:
 	//Function to return list containing vertices in Topological order. 
+	// Breath First Search (BFS)
 	vector<int> topoSort(int V, vector<int> adj[]) 
 	{
 	    // code here
@@ -29,6 +30,40 @@ class Solution
 	                q.push(it);
 	            }
 	        }
+	    }
+	    return ans;
+	}
+	
+	
+    // Depth First Search (DFS)
+    private:
+    void dfs(int curr, int vis[], stack<int> &st, vector<int> adj[]) {
+        vis[curr] = 1;
+        for(auto it : adj[curr]) {
+            if(!vis[it]) {
+                dfs(it, vis, st, adj);
+            }
+        }
+        st.push(curr);
+    }
+    
+	public:
+	//Function to return list containing vertices in Topological order. 
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    // code here
+	    int vis[V] = {0};
+	    stack<int> st;
+	    for(int i=0; i<V; i++) {
+	        if(!vis[i]) {
+	            dfs(i, vis, st, adj);
+	        }
+	    }
+	    
+	    vector<int> ans;
+	    while(!st.empty()) {
+	        ans.push_back(st.top());
+	        st.pop();
 	    }
 	    return ans;
 	}

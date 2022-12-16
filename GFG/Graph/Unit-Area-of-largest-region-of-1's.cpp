f17cc88d77a3a39800492f5faff612105f059fa8
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2022-12-17 00:08:22 +0530

    Update Unit-Area-of-largest-region-of-1's.cpp

--- GFG/Graph/Unit-Area-of-largest-region-of-1's.cpp
+++ GFG/Graph/Unit-Area-of-largest-region-of-1's.cpp
@@ -1,46 +1,35 @@
-class Solution {
+class Solution
+{
     private:
-    void dfs(int row, int col, vector<vector<int>> &vis, vector<vector<int>>& matrix, int delRow[], int delCol[], int n, int m) {
+    int dfs(int row, int col, vector<vector<int>> &vis, vector<vector<int>>& grid, int delRow[], int delCol[]) {
         vis[row][col] = 1;
-        for(int i=0; i<4; i++) {
+        int cnt = 1;
+        int n = grid.size();
+        int m = grid[0].size();
+        for(int i=0; i<8; i++) {
             int nRow = row + delRow[i];
             int nCol = col + delCol[i];
-            if(nRow>=0 && nRow<n && nCol>=0 && nCol<m && !vis[nRow][nCol] && matrix[nRow][nCol]==1) {
-                dfs(nRow, nCol, vis, matrix, delRow, delCol, n, m);
-                
+            if(nRow>=0 && nRow<n && nCol>=0 && nCol<m && !vis[nRow][nCol] && grid[nRow][nCol]==1) {
+                cnt += dfs(nRow, nCol, vis, grid, delRow, delCol);
             }
         }
+        return cnt;
     }
     
     public:
-    int closedIslands(vector<vector<int>>& matrix, int N, int M) {
+    //Function to find unit area of the largest region of 1s.
+    int findMaxArea(vector<vector<int>>& grid) {
         // Code here
-        vector<vector<int>> vis(N, vector<int> (M, 0));
-        int delRow[] = {-1, 0, +1, 0};
-        int delCol[] = {0, +1, 0, -1};
-        for(int i=0; i<N; i++) {
-            if(!vis[i][0] && matrix[i][0]==1) {
-                dfs(i, 0, vis, matrix, delRow, delCol, N, M);
-            }
-            if(!vis[i][M-1] && matrix[i][M-1]==1) {
-                dfs(i, M-1, vis, matrix, delRow, delCol, N, M);
-            }
-        }
-        for(int j=0; j<M; j++) {
-            if(!vis[0][j] && matrix[0][j]==1) {
-                dfs(0, j, vis, matrix, delRow, delCol, N, M);
-            }
-            if(!vis[N-1][j] && matrix[N-1][j]) {
-                dfs(N-1, j, vis, matrix, delRow, delCol, N, M);
-            }
-        }
-        
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> vis(n, vector<int> (m, 0));
+        int delRow[] = {-1, 0, +1, 0, 1, 1, -1, -1};
+        int delCol[] = {0, +1, 0, -1, 1, -1, 1, -1};
         int ans = 0;
-        for(int i=0; i<N; i++) {
-            for(int j=0; j<M; j++) {
-                if(!vis[i][j] && matrix[i][j]==1) {
-                    ans++;
-                    dfs(i, j, vis, matrix, delRow, delCol, N, M);
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                if(!vis[i][j] && grid[i][j]==1) {
+                    ans = max(ans,dfs(i, j, vis, grid, delRow, delCol));
                 }
             }
         }

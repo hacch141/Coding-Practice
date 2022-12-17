a64a02075760be43d6bc0951298fa7171ebbec74
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2022-12-17 18:53:21 +0530

    Create Cheapest-Flights-Within-K-Stops.cpp

---
+++ GFG/Graph/Cheapest-Flights-Within-K-Stops.cpp
@@ -0,0 +1,34 @@
+class Solution {
+  public:
+    int CheapestFLight(int n, vector<vector<int>>& flights, int src, int dst, int K)  {
+        // Code here
+        vector<pair<int,int>> adj[n];
+        for(auto it : flights) {
+            adj[it[0]].push_back({it[1],it[2]});
+        }
+        
+        queue< pair<int,pair<int,int>> > q;
+        q.push({0,{src,0}});
+        vector<int> dist(n,1e9);
+        dist[src] = 0;
+        
+        while(!q.empty()) {
+            int stops = q.front().first;
+            int u = q.front().second.first;
+            int cost = q.front().second.second;
+            q.pop();
+            
+            if(stops>K) continue;
+            for(auto it : adj[u]) {
+                int v = it.first;
+                int weight = it.second;
+                if(cost+weight < dist[v] && stops<=K) {
+                    dist[v] = cost+weight;
+                    q.push({stops+1,{v,dist[v]}});
+                }
+            }
+        }
+        if(dist[dst]==1e9) return -1;
+        return dist[dst];
+    }
+};

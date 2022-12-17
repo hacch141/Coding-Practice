1cf7d59de93c07dac1f1cf3a04b669e79ef8c529
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2022-12-17 08:01:04 +0530

    Update Implementing-Dijkstra-Algorithm.cpp

--- GFG/Graph/Implementing-Dijkstra-Algorithm.cpp
+++ GFG/Graph/Implementing-Dijkstra-Algorithm.cpp
@@ -28,3 +28,39 @@ class Solution
         return dist;
     }
 };
+
+// Using Set 
+class Solution
+{
+	public:
+	//Function to find the shortest distance of all the vertices
+    //from the source vertex S.
+    vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
+    {
+        // Code here
+        set<pair<int,int>> st;
+        st.insert({0,S});
+        vector<int> dist(V,1e9);
+        dist[S] = 0;
+        
+        while(!st.empty()) {
+            auto it = *(st.begin());
+            int u = it.second;
+            int d = it.first;
+            st.erase(it);
+            
+            for(auto it : adj[u]) {
+                int v = it[0];
+                int weight = it[1];
+                if(d+weight < dist[v]) {
+                    if(dist[v] != 1e9) {
+                        st.erase({dist[v],v});
+                    }
+                    dist[v] = d + weight;
+                    st.insert({dist[v],v});
+                }
+            }
+        }
+        return dist;
+    }
+};

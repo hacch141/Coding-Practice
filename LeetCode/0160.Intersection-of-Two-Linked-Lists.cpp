397f3cb2b956d8615f89a6d042e82fac9cc04d4b
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-01-07 15:46:34 +0530

    Update and rename 160.Intersection-of-Two-Linked-Lists.cpp to 0160.Intersection-of-Two-Linked-Lists.cpp

--- LeetCode/0160.Intersection-of-Two-Linked-Lists.cpp
+++ LeetCode/0160.Intersection-of-Two-Linked-Lists.cpp
@@ -8,6 +8,21 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
+
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        ListNode* a = headA;
+        ListNode* b = headB;
+
+        while(a != b) {
+            a = a == NULL ? headB : a->next;
+            b = b == NULL ? headA : b->next;
+        }
+        return a;
+    }
+};
+
 class Solution {
 public:
     

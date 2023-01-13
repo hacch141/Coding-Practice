94cadb8a3b4e0861b822ddfe48564709e0bc574a
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-01-13 23:30:32 +0530

    Update 0046.Permutations.cpp

--- LeetCode/0046.Permutations.cpp
+++ LeetCode/0046.Permutations.cpp
@@ -29,3 +29,30 @@ public:
         return ans;
     }
 };
+
+
+// ==========================================================
+
+
+class Solution {
+
+private:
+    void helper(int idx, vector<int>& nums, vector<vector<int>>& ans) {
+        if(idx==nums.size()) {
+            ans.push_back(nums);
+            return;
+        }
+        for(int i=idx; i<nums.size(); i++) {
+            swap(nums[i],nums[idx]);
+            helper(idx+1,nums,ans);
+            swap(nums[i],nums[idx]);
+        }
+    }
+
+public:
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        helper(0,nums,ans);
+        return ans;
+    }
+};

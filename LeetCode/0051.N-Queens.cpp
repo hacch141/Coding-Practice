315fd5770c0ced07d53faa832e97d6a7d3535672
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-01-14 10:16:51 +0530

    Update 0051.N-Queens.cpp

--- LeetCode/0051.N-Queens.cpp
+++ LeetCode/0051.N-Queens.cpp
@@ -55,3 +55,46 @@ public:
         return ans;
     }
 };
+
+
+// ======================================================================
+
+
+
+class Solution {
+
+private:
+
+    void solve(int col, vector<string>& board, vector<vector<string>>& ans, int n, vector<int>& leftRow, vector<int>& upperDiagonal, vector<int>& lowerDiagonal) {
+        if(col==n) {
+            ans.push_back(board);
+            return;
+        }
+        for(int row=0; row<n; row++) {
+            if(leftRow[row]==0 && lowerDiagonal[row+col]==0 && upperDiagonal[n-1+col-row]==0) {
+                board[row][col] = 'Q';
+                leftRow[row] = 1;
+                lowerDiagonal[row+col] = 1;
+                upperDiagonal[n-1+col-row] = 1;
+                solve(col+1,board,ans,n,leftRow,upperDiagonal,lowerDiagonal);
+                leftRow[row] = 0;
+                lowerDiagonal[row+col] = 0;
+                upperDiagonal[n-1+col-row] = 0;
+                board[row][col] = '.';
+            }
+        }
+    }
+
+public:
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<string> board(n);
+        vector<int> leftRow(n,0), upperDiagonal(2*n-1,0), lowerDiagonal(2*n-1,0);
+        string s(n,'.');
+        for(int i=0; i<n; i++) {
+            board[i] = s;
+        }
+        solve(0,board,ans,n,leftRow,upperDiagonal,lowerDiagonal);
+        return ans;
+    }
+};

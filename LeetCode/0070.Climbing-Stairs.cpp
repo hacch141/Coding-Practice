777eada9d57378bda31dff3b1b0a7d30189ad55e
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-01-18 02:50:17 +0530

    Update 0070.Climbing-Stairs.cpp

--- LeetCode/0070.Climbing-Stairs.cpp
+++ LeetCode/0070.Climbing-Stairs.cpp
@@ -2,6 +2,41 @@
 
 class Solution {
 
+public:
+    int climbStairs(int n) {
+        if(n<=1) return n;
+        int prev = 1;
+        int prev2 = 1;
+        for(int i=2; i<=n; i++) {
+            int curr = prev + prev2;
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
+    }
+};
+
+// =======================================================================
+
+class Solution {
+
+public:
+    int climbStairs(int n) {
+        if(n<=1) return n;
+        vector<int> dp(n+1,0);
+        dp[0] = 1;
+        dp[1] = 1;
+        for(int i=2; i<=n; i++) {
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+};
+
+// ============================================================================
+
+class Solution {
+
 private:
     int solve(int idx, vector<int>& dp) {
         if(dp[idx] != 0) return dp[idx];
@@ -20,23 +55,6 @@ public:
         if(n<=1) return n;
         vector<int> dp(n+1,0);
         solve(n,dp);
-        return dp[n];
-    }
-};
-
-
-
-class Solution {
-
-public:
-    int climbStairs(int n) {
-        if(n<=1) return n;
-        vector<int> dp(n+1,0);
-        dp[0] = 1;
-        dp[1] = 1;
-        for(int i=2; i<=n; i++) {
-            dp[i] = dp[i-1]+dp[i-2];
-        }
         return dp[n];
     }
 };

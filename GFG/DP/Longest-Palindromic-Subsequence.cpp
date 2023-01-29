8e843ab16ba2df5c3ad5277cf1dfd389545169cb
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-01-29 22:59:34 +0530

    Create Longest-Palindromic-Subsequence.cpp

---
+++ GFG/DP/Longest-Palindromic-Subsequence.cpp
@@ -0,0 +1,23 @@
+class Solution{
+  public:
+    int longestPalinSubseq(string A) {
+        //code here
+        string B = A;
+        reverse(B.begin(),B.end());
+        int m = A.length();
+        vector<int> dp(m+1,0), curr(m+1,0);
+        
+        for(int i=1; i<=m; i++) {
+            for(int j=1; j<=m; j++) {
+                if(A[i-1] == B[j-1]) {
+                    curr[j] = 1 + dp[j-1];
+                }
+                else {
+                    curr[j] = max(curr[j-1],dp[j]);
+                }
+            }
+            dp = curr;
+        }
+        return dp[m];
+    }
+};

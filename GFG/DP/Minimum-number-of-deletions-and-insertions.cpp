bb447d0731539f8a7495cb65081ac02c45d4d784
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-01-29 23:10:42 +0530

    Create Minimum-number-of-deletions-and-insertions.cpp

---
+++ GFG/DP/Minimum-number-of-deletions-and-insertions.cpp
@@ -0,0 +1,23 @@
+class Solution{
+	public:
+	int minOperations(string str1, string str2) 
+	{ 
+	    // Your code goes here
+	    int n = str1.length();
+	    int m = str2.length();
+	    vector<int> dp(m+1,0), curr(m+1,0);
+	    
+	    for(int i=1; i<=n; i++) {
+	        for(int j=1; j<=m; j++) {
+	            if(str1[i-1] == str2[j-1]) {
+	                curr[j] = 1 + dp[j-1];
+	            }
+	            else {
+	                curr[j] = max(curr[j-1],dp[j]);
+	            }
+	        }
+	        dp = curr;
+	    }
+	    return n+m-(2*dp[m]);
+	} 
+};

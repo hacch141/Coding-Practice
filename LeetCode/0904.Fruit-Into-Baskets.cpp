e79cb1e6fc5dce0a4c42fd5688f6e9f635cb223a
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-02-07 22:28:20 +0530

    Update 0904.Fruit-Into-Baskets.cpp

--- LeetCode/0904.Fruit-Into-Baskets.cpp
+++ LeetCode/0904.Fruit-Into-Baskets.cpp
@@ -27,3 +27,36 @@ public:
         return maxi;
     }
 }; 
+
+
+
+
+
+// ============================================
+
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int n = fruits.size();
+        unordered_map<int,int> mp;
+        int maxi = 0;
+        int cnt = 0;
+        queue<int> q;
+        
+        for(int i=0; i<n; i++) {
+            mp[fruits[i]]++;
+            q.push(fruits[i]);
+            if(mp.size()>2) {
+                while(mp.size()>2) {
+                    int remove = q.front();
+                    q.pop();
+                    mp[remove]--;
+                    cnt--;
+                    if(mp[remove] == 0) mp.erase(remove);
+                }
+            }
+            if(q.size()>maxi) maxi = q.size();
+        }
+        return maxi;
+    }
+}; 

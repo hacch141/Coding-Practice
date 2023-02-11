b10e7e5843cc0ed45653c9ee3404317a3221791f
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-02-11 10:20:49 +0530

    Create 0055.Jump-Game.cpp

---
+++ LeetCode/0055.Jump-Game.cpp
@@ -0,0 +1,25 @@
+// 55. Jump Game
+
+class Solution {
+
+private:
+    bool solve(int i, int n, vector<int>& nums, vector<int>& dp) {
+        if(i==n-1) return true;
+        if(nums[i]==0) return false;
+        if(dp[i] != -1) return dp[i];
+        bool isJump = false;
+        for(int jump=1; jump<=nums[i] && i+jump<n; jump++) {
+            isJump = solve(i+jump,n,nums,dp);
+            if(isJump) return true;
+        }
+        return dp[i] = isJump;
+    }
+
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        if(nums.size()==1) return true;
+        vector<int> dp(n+1,-1);
+        return solve(0,n,nums,dp);
+    }
+};

7d1b4119306c026659370c37b0fc33e1505f695d
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-02-19 12:36:56 +0530

    Update 103.Binary-Tree-Zigzag-Level-Order-Traversal.cpp

--- LeetCode/103.Binary-Tree-Zigzag-Level-Order-Traversal.cpp
+++ LeetCode/103.Binary-Tree-Zigzag-Level-Order-Traversal.cpp
@@ -1,30 +1,44 @@
 // 103. Binary Tree Zigzag Level Order Traversal
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+
 class Solution {
 public:
-    
-    /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+        if(!root) return {};
+        bool rev = true;
+        queue<TreeNode*> q;
+        q.push(root);
+        vector<vector<int>> ans;
+        ans.push_back({root->val});
+        vector<int> temp;
+
+        while(!q.empty()) {
+            int len = q.size();
+            for(int i=0; i<len; i++) {
+                TreeNode* curr = q.front();
+                q.pop();
+                if(curr->left) {
+                    q.push(curr->left);
+                    temp.push_back(curr->left->val);
+                }
+                if(curr->right) {
+                    q.push(curr->right);
+                    temp.push_back(curr->right->val);
+                }
+            }
+            if(rev) {
+                reverse(temp.begin(),temp.end());
+            }
+            if(temp.size()>0) ans.push_back(temp);
+            rev = !rev;
+            temp.clear();
+        }
+        return ans;
+    }
+};
+
+
+
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
@@ -71,45 +85,45 @@ public:
     }
 };
     
-//     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {  // 2nd Method
-//         if(root == NULL) {
-//             return {};
-//         }
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {  // 2nd Method
+        if(root == NULL) {
+            return {};
+        }
         
-//         vector<vector<int>> ans;
-//         queue<TreeNode*> q;
-//         stack<int> st;
-//         q.push(root);
-//         bool reverse = false;
+        vector<vector<int>> ans;
+        queue<TreeNode*> q;
+        stack<int> st;
+        q.push(root);
+        bool reverse = false;
         
-//         while(q.empty() == false) {
-//             int cnt = q.size();
-//             vector<int> v;
-//             for(int i=0; i<cnt; i++) {
-//                 TreeNode* curr = q.front();
-//                 q.pop();
-//                 if(reverse) {
-//                     st.push(curr->val);
-//                 }
-//                 else {
-//                     v.push_back(curr->val);
-//                 }
-//                 if(curr->left) {
-//                     q.push(curr->left);
-//                 }
-//                 if(curr->right) {
-//                     q.push(curr->right);
-//                 }
-//             }
-//             if(reverse) {
-//                 while(st.empty() == false) {
-//                     v.push_back(st.top());
-//                     st.pop();
-//                 }
-//             }
-//             ans.push_back(v);
-//             reverse = !reverse;
-//         }
-//         return ans;
-//     }
+        while(q.empty() == false) {
+            int cnt = q.size();
+            vector<int> v;
+            for(int i=0; i<cnt; i++) {
+                TreeNode* curr = q.front();
+                q.pop();
+                if(reverse) {
+                    st.push(curr->val);
+                }
+                else {
+                    v.push_back(curr->val);
+                }
+                if(curr->left) {
+                    q.push(curr->left);
+                }
+                if(curr->right) {
+                    q.push(curr->right);
+                }
+            }
+            if(reverse) {
+                while(st.empty() == false) {
+                    v.push_back(st.top());
+                    st.pop();
+                }
+            }
+            ans.push_back(v);
+            reverse = !reverse;
+        }
+        return ans;
+    }
 };

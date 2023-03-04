f068088c6e0da38f6cd5ab4b81497c90af43ee95
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-03-04 23:52:50 +0530

    Update 0189.Rotate-Array.cpp

--- LeetCode/0189.Rotate-Array.cpp
+++ LeetCode/0189.Rotate-Array.cpp
@@ -9,14 +9,38 @@ public:
         }
         k = k%n;
         vector<int> temp;
-        for(int i=n-1; i>=n-1-k; i--) {
+        for(int i=n-k; i<n; i++) {
             temp.push_back(nums[i]);
         }
-        for(int i=n-1; i>=k; i--) {
+        for(int i=n-1; i>k-1; i--) {
             nums[i] = nums[i-k];
         }
         for(int i=k-1; i>=0; i--) {
-            nums[i] = temp[k-i-1];
+            nums[i] = temp[i];
         }
     }
 };
+
+// ======================================================
+
+
+    void rev(int start, int end, vector<int> &nums) {
+        while(start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
+
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        if(n==0 || n==1 || n==k) {
+            return;
+        }
+        k = k%n;
+        rev(0,n-k-1,nums);
+        rev(n-k,n-1,nums);
+        rev(0,n-1,nums);
+    }

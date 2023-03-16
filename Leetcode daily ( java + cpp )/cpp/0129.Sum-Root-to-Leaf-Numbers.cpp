bdb9e4cfe6fe1f3c4f62844fff91de4ae9502e38
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-03-16 19:46:28 +0530

    Update 0129.Sum-Root-to-Leaf-Numbers.cpp

--- Leetcode daily ( java + cpp )/cpp/0129.Sum-Root-to-Leaf-Numbers.cpp
+++ Leetcode daily ( java + cpp )/cpp/0129.Sum-Root-to-Leaf-Numbers.cpp
@@ -21,3 +21,27 @@ public:
         return sum;
     }
 };
+
+// ===========================================================
+
+class Solution {
+public:
+
+    void helper(TreeNode* root, int curr, int& sum) {
+        if(root) {
+            curr = (curr*10) + root->val;
+            if(!root->left && !root->right) {
+                sum += curr;
+            }
+        } else return;
+        helper(root->left,curr,sum);
+        helper(root->right,curr,sum);
+    }
+
+    int sumNumbers(TreeNode* root) {
+        int sum = 0;
+        int curr = 0;
+        helper(root,curr,sum);
+        return sum;
+    }
+};

60e8a6b5e58ce8fe0472ebc230f617876dbc1b01
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-03-25 03:41:55 +0530

    Update 1466.Reorder-Routes-to-Make-All-Paths-Lead-to-the-City-Zero.cpp

--- LeetCode/1466.Reorder-Routes-to-Make-All-Paths-Lead-to-the-City-Zero.cpp
+++ LeetCode/1466.Reorder-Routes-to-Make-All-Paths-Lead-to-the-City-Zero.cpp
@@ -3,6 +3,48 @@
 class Solution {
 public:
 
+    void bfs(int& cnt, vector<vector<int>>& adj, int& n) {
+        queue<int> q;
+        q.push(0);
+        vector<int> vis(n,0);
+        vis[0] = 1;
+
+        while(!q.empty()) {
+            int curr = q.front();
+            q.pop();
+            for(auto i : adj[curr]) {
+                int next = abs(i);
+                if(!vis[next]) {
+                    vis[next] = 1;
+                    q.push(next);
+                    if(i>0) cnt++;
+                }
+            }
+        }
+    }
+
+    int minReorder(int n, vector<vector<int>>& connections) {
+        vector<vector<int>> adj(n);
+
+        for(auto& i : connections) {
+            adj[i[0]].push_back(i[1]);
+            adj[i[1]].push_back(-i[0]);
+        }
+
+        int cnt = 0;
+        bfs(cnt,adj,n);
+        return cnt;
+    }
+};
+
+
+// =================================================================================================
+
+
+
+class Solution {
+public:
+
     void bfs(int& cnt, unordered_map<string,int>& mp, vector<vector<int>>& adj, int& n) {
         queue<int> q;
         q.push(0);

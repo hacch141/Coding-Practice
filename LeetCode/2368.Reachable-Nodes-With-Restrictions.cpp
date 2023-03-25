ec1b31130ae78823fa71f093d7be55d20fb23d9d
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-03-25 14:37:22 +0530

    Update 2368.Reachable-Nodes-With-Restrictions.cpp

--- LeetCode/2368.Reachable-Nodes-With-Restrictions.cpp
+++ LeetCode/2368.Reachable-Nodes-With-Restrictions.cpp
@@ -3,13 +3,13 @@
 class Solution {
 public:
 
-    void dfs(int start, vector<int>& vis, vector<vector<int>>& adj, unordered_map<int,int>& mp, int& cnt) {
+    void dfs(int start, vector<int>& vis, vector<vector<int>>& adj, int& cnt) {
         vis[start] = 1;
         cnt++;
 
         for(auto i : adj[start]) {
-            if(!vis[i] && mp.find(i)==mp.end()) {
-                dfs(i,vis,adj,mp,cnt);
+            if(!vis[i]) {
+                dfs(i,vis,adj,cnt);
             }
         }
     }
@@ -17,17 +17,16 @@ public:
     int reachableNodes(int n, vector<vector<int>>& edges, vector<int>& restricted) {
         vector<vector<int>> adj(n);
         vector<int> vis(n,0);
-        unordered_map<int,int> mp;
         
         for(auto i : edges) {
             adj[i[0]].push_back(i[1]);
             adj[i[1]].push_back(i[0]);
         }
 
-        for(auto i : restricted) mp[i]++;
+        for(auto i : restricted) vis[i]=1;
 
         int cnt = 0;
-        dfs(0,vis,adj,mp,cnt);
+        dfs(0,vis,adj,cnt);
 
         return cnt;
     }

620a57b2da3374e66194e2fa0fd82b6b8fadd258
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-03-29 20:33:25 +0530

    Update 1402.Reducing-Dishes.cpp

--- LeetCode/1402.Reducing-Dishes.cpp
+++ LeetCode/1402.Reducing-Dishes.cpp
@@ -3,6 +3,29 @@
 class Solution {
 public:
 
+    int maxSatisfaction(vector<int>& satisfaction) {
+        int n = satisfaction.size();
+        
+        sort(satisfaction.begin(),satisfaction.end());
+        int ans = 0;
+        int curr_sum = 0;
+        int window = 0;
+
+        for(int i=n-1; i>=0; i--) {
+            curr_sum += satisfaction[i];
+            window += curr_sum;
+            ans = max(ans,window);
+        }
+
+        return ans;
+    }
+};
+
+// =========================================================================
+
+class Solution {
+public:
+
     int solve(int idx, int mul, vector<int>& satisfaction, int& n, vector<vector<int>>& dp) {
         if(idx==n) return 0;
 

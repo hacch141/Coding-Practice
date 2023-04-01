5041b15b5b63517184d5ec2345a85c4912540f91
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-04-02 05:16:33 +0530

    Update Insertion-Sort.cpp

--- GFG/Sorting/Insertion-Sort.cpp
+++ GFG/Sorting/Insertion-Sort.cpp
@@ -13,3 +13,24 @@ void insertionSort(int arr[], int n)
         }
     }
 }
+
+
+// Recursive
+void helper(int arr[], int i, int n) {
+    if(i==n) return;
+    int j = i;
+    while(j>0 && arr[j-1] > arr[j]) {
+        int temp = arr[j-1];
+        arr[j-1] = arr[j];
+        arr[j] = temp;
+        j--;
+    }
+    helper(arr,i+1,n);
+}
+    
+    //Function to sort the array using insertion sort algorithm.
+void insertionSort(int arr[], int n)
+{
+    //code here
+    helper(arr,1,n);
+}

c44efe5773621bd3a197a16f1450f4fc5761923d
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-04-02 04:23:52 +0530

    Create Insertion-Sort.cpp

---
+++ GFG/Sorting/Insertion-Sort.cpp
@@ -0,0 +1,15 @@
+// Insertion Sort
+
+void insertionSort(int arr[], int n)
+{
+    //code here
+    for(int i=0; i<n; i++) {
+        int j=i;
+        while(j>0 && arr[j-1] > arr[j]) {
+            int temp = arr[j-1];
+            arr[j-1] = arr[j];
+            arr[j] = temp;
+            j--;
+        }
+    }
+}

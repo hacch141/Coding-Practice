b909efecbb653b601304b5decdfe103acabeeb9c
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-04-03 19:01:56 +0530

    Update 0073.Set-Matrix-Zeroes.cpp

--- GFG/Array/medium/leetcode/0073.Set-Matrix-Zeroes.cpp
+++ GFG/Array/medium/leetcode/0073.Set-Matrix-Zeroes.cpp
@@ -45,3 +45,40 @@ public:
         }
     }
 };
+
+
+// ================================================================================
+
+// optimized
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        set<int> rows;
+        set<int> cols;
+
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                if(matrix[i][j]==0) {
+                    rows.insert(i);
+                    cols.insert(j);
+                }
+            }
+        }
+
+        for(auto i : rows) {
+            for(int j=0; j<m; j++) {
+                matrix[i][j] = 0;
+            }
+        }
+
+        for(auto j : cols) {
+            for(int i=0; i<n; i++) {
+                matrix[i][j] = 0;
+            }
+        }
+        
+    }
+};

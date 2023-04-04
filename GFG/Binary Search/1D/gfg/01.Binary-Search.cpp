32b933a614dc99ca60e25b84342dc0737e232721
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-04-04 18:53:02 +0530

    Create 01.Binary-Search.cpp

---
+++ GFG/Binary Search/1D/gfg/01.Binary-Search.cpp
@@ -0,0 +1,29 @@
+class Solution {
+    
+  public:
+  
+    int binarysearch(int arr[], int n, int k) {
+        // code here
+        int low = 0;
+        int high = n-1;
+        
+        while(low <= high) {
+            
+            int mid = (high-low)/2 + low;
+            
+            if(arr[mid]==k) {
+                return mid;
+            }
+            
+            else if(arr[mid] < k) {
+                low = mid+1;
+            }
+            
+            else {
+                high = mid-1;
+            }
+            
+        }
+        return -1;
+    }
+};

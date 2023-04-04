021bc1b361fd0185420e4a90439a282e24343160
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-04-04 15:36:26 +0530

    Update 01.Find-all-pairs-with-a-given-sum.cpp

--- GFG/Array/medium/gfg/01.Find-all-pairs-with-a-given-sum.cpp
+++ GFG/Array/medium/gfg/01.Find-all-pairs-with-a-given-sum.cpp
@@ -1,5 +1,7 @@
 class Solution{
     public:
+    
+    // using hashmap
     vector<pair<int,int>> allPairs(int A[], int B[], int N, int M, int X)
     {
         // Your code goes here   
@@ -18,6 +20,42 @@ class Solution{
         
         sort(ans.begin(),ans.end());
         
+        return ans;
+    }
+    
+// ==========================================================================================
+    
+    // using binary search
+    vector<pair<int,int>> allPairs(int A[], int B[], int N, int M, int X)
+    {
+        // Your code goes here   
+        vector<pair<int,int>> ans;
+        sort(A,A+N);
+        sort(B,B+M);
+        
+        for(int i=0; i<N; i++)  {
+            
+            int target = X-A[i];
+            int low = 0;
+            int high = M-1;
+            
+            while(low<=high) {
+                
+                int mid = (high-low)/2 + low;
+                
+                if(B[mid] == target) {
+                    ans.push_back({A[i],target});
+                    break;
+                }
+                else if(B[mid] < target) {
+                    low = mid + 1;
+                }
+                else {
+                    high = mid - 1;
+                }
+                
+            }
+        }
         return ans;
     }
 };

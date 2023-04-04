9cd633b2c4a54f0911afe5dc35ad132adc3733c4
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-04-04 19:49:40 +0530

    Create 05.Search-Insert-Position.cpp

---
+++ GFG/Binary Search/1D/leetcode/05.Search-Insert-Position.cpp
@@ -0,0 +1,30 @@
+// 35. Search Insert Position
+
+class Solution {
+public:
+
+    int searchInsert(vector<int>& nums, int target) {
+
+        int low = 0;
+        int high = nums.size()-1;
+
+        while(low<=high) {
+
+            int mid = (high-low)/2 + low;
+
+            if(target == nums[mid]) {
+                return mid;
+            }
+
+            else if(target > nums[mid]) {
+                low = mid+1;
+            }
+
+            else {
+                high = mid-1;
+            }
+            
+        }
+        return low;
+    }
+};

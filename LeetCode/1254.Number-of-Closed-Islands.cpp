6214046abb968304f08201d722073b5b80121e14
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-04-06 22:28:41 +0530

    Update 1254.Number-of-Closed-Islands.cpp

--- LeetCode/1254.Number-of-Closed-Islands.cpp
+++ LeetCode/1254.Number-of-Closed-Islands.cpp
@@ -1,60 +1,139 @@
 // 1254. Number of Closed Islands
 
+// DFS
 class Solution {
 
-private:
-    void dfs(int row, int col, vector<vector<int>> &vis, vector<vector<int>>& grid, int delRow[], int delCol[]) {
+public:
+
+    void dfs(int row, int col, vector<vector<int>>& vis, vector<vector<int>>& grid, int delRow[], int delCol[], int& n, int& m) {
+        
         vis[row][col] = 1;
-        int n = grid.size();
-        int m = grid[0].size();
+        
         for(int i=0; i<4; i++) {
+            
             int nRow = row + delRow[i];
             int nCol = col + delCol[i];
+            
             if(nRow>=0 && nRow<n && nCol>=0 && nCol<m && !vis[nRow][nCol] && grid[nRow][nCol]==0) {
-                dfs(nRow, nCol, vis, grid, delRow, delCol);
+                dfs(nRow,nCol,vis,grid,delRow,delCol,n,m);
+            }
+            
+        }
+    }
+
+    int closedIsland(vector<vector<int>>& grid) {
+        
+        int n = grid.size();
+        int m = grid[0].size();
+        vector<vector<int>> vis(n, vector<int> (m,0));
+
+        int delRow[4] = {-1,0,+1,0};
+        int delCol[4] = {0,-1,0,+1};
+
+        for(int i=0; i<n; i++) {
+            if(grid[i][0]==0 && !vis[i][0]) {
+                dfs(i,0,vis,grid,delRow,delCol,n,m);
+            }
+            if(grid[i][m-1]==0 && !vis[i][m-1]) {
+                dfs(i,m-1,vis,grid,delRow,delCol,n,m);
+            }
+        }
+
+        for(int j=0; j<m; j++) {
+            if(grid[0][j]==0 && !vis[0][j]) {
+                dfs(0,j,vis,grid,delRow,delCol,n,m);
+            }
+            if(grid[n-1][j]==0 && !vis[n-1][j]) {
+                dfs(n-1,j,vis,grid,delRow,delCol,n,m);
+            }
+        }
+
+        int ans = 0;
+        for(int i=1; i<n-1; i++) {
+            for(int j=0; j<m-1; j++) {
+                if(grid[i][j]==0 && !vis[i][j]) {
+                    ans++;
+                    dfs(i,j,vis,grid,delRow,delCol,n,m);
+                }
+            }
+        }
+
+        return ans;
+    }
+};
+
+
+// =====================================================================================
+
+// BFS
+
+class Solution {
+
+public:
+
+    void bfs(int row, int col, vector<vector<int>>& vis, vector<vector<int>>& grid, int delRow[], int delCol[], int& n, int& m) {
+
+        vis[row][col] = 1;
+
+        queue<pair<int,int>> q;
+        q.push({row,col});
+
+        while(!q.empty()) {
+            
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+
+            for(int i=0; i<4; i++) {
+
+                int nRow = row + delRow[i];
+                int nCol = col + delCol[i];
+
+                if(nRow>=0 && nRow<n && nCol>=0 && nCol<m && !vis[nRow][nCol] && grid[nRow][nCol]==0) {
+                    vis[nRow][nCol] = 1;
+                    q.push({nRow,nCol});
+                }
+                
             }
         }
     }
 
-public:
     int closedIsland(vector<vector<int>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
         vector<vector<int>> vis(n, vector<int> (m,0));
-        int delRow[] = {-1, 0, +1, 0};
-        int delCol[] = {0, +1, 0, -1};
-        // for(int i=0; i<n; i++) {
-        //     if(!vis[i][0] && grid[i][0]==0) {
-        //         dfs(i, 0, vis, grid, delRow, delCol);
-        //     }
-        //     if(!vis[i][m-1] && grid[i][m-1]==0) {
-        //         dfs(i, m-1, vis, grid, delRow, delCol);
-        //     }
-        // }
-        // for(int j=0; j<m; j++) {
-        //     if(!vis[0][j] && grid[0][j]==0) {
-        //         dfs(0, j, vis, grid, delRow, delCol);
-        //     }
-        //     if(!vis[n-1][j] && grid[n-1][j]==0) {
-        //         dfs(n-1, j, vis, grid, delRow, delCol);
-        //     }
-        // }
+
+        int delRow[4] = {-1,0,+1,0};
+        int delCol[4] = {0,-1,0,+1};
+
         for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
-                if((i==0 || j==0 || i==n-1 || j==m-1) && !vis[i][j] && grid[i][j]==0) {
-                    dfs(i, j, vis, grid, delRow, delCol);
-                }
+            if(grid[i][0]==0 && !vis[i][0]) {
+                bfs(i,0,vis,grid,delRow,delCol,n,m);
+            }
+            if(grid[i][m-1]==0 && !vis[i][m-1]) {
+                bfs(i,m-1,vis,grid,delRow,delCol,n,m);
             }
         }
+
+        for(int j=0; j<m; j++) {
+            if(grid[0][j]==0 && !vis[0][j]) {
+                bfs(0,j,vis,grid,delRow,delCol,n,m);
+            }
+            if(grid[n-1][j]==0 && !vis[n-1][j]) {
+                bfs(n-1,j,vis,grid,delRow,delCol,n,m);
+            }
+        }
+
         int ans = 0;
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
-                if(!vis[i][j] && grid[i][j]==0) {
-                    dfs(i, j, vis, grid, delRow, delCol);
+        for(int i=1; i<n-1; i++) {
+            for(int j=0; j<m-1; j++) {
+                if(grid[i][j]==0 && !vis[i][j]) {
                     ans++;
+                    bfs(i,j,vis,grid,delRow,delCol,n,m);
                 }
             }
         }
+
         return ans;
     }
 };

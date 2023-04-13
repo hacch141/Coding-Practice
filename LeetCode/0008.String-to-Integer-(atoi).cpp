3c54674c28f76d111b13a10d4add2a517ae61f8b
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-04-14 02:36:28 +0530

    Create 0008.String-to-Integer-(atoi).cpp

---
+++ LeetCode/0008.String-to-Integer-(atoi).cpp
@@ -0,0 +1,47 @@
+// 8. String to Integer (atoi)
+
+class Solution {
+public:
+
+    int helper(string s, bool& flag) {
+        int ans = 0;
+        for(auto i : s) {
+            if(flag) {
+                if((long long)ans*10 + (i-'0') > INT_MAX) {
+                    return INT_MIN;
+                }
+            }
+            else {
+                if((long long)ans*10 + (i-'0') >= INT_MAX) {
+                    return INT_MAX;
+                }
+            }
+            ans = ans*10 + (i-'0');
+        }
+        return flag ? -ans : ans;
+    }
+
+    int myAtoi(string s) {
+        int n = s.length();
+        string num = "";
+        bool flag = false;
+        for(int i=0; i<n; i++) {
+            if(s[i]==' ') continue;
+            else if(i<n && ((s[i]=='-') | s[i]=='+' | (s[i]>='0' && s[i]<='9'))) {
+                if(s[i]=='-') {
+                    flag = true;
+                    i++;
+                }
+                else if(s[i]=='+') i++;
+                while(i<n && s[i]>='0' && s[i]<='9') {
+                    num += s[i];
+                    i++;
+                }
+                break;
+            }
+            return 0;
+        }
+
+        return helper(num,flag);
+    }
+};

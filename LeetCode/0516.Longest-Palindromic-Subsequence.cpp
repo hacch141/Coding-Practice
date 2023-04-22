5a92ab9eaa024f326c36e8fd3ee2d25c85772766
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-04-22 22:14:19 +0530

    Update 0516.Longest-Palindromic-Subsequence.cpp

--- LeetCode/0516.Longest-Palindromic-Subsequence.cpp
+++ LeetCode/0516.Longest-Palindromic-Subsequence.cpp
@@ -1,5 +1,38 @@
 // 516. Longest Palindromic Subsequence
 
+
+// Recursive
+class Solution {
+public:
+
+    int helper(int start, int end, string& s, vector<vector<int>>& dp) {
+
+        if(start==end) return 1;
+        if(start>end) return 0;
+
+        if(dp[start][end]!=-1) return dp[start][end];
+
+        if(s[start]==s[end]) return 2 + helper(start+1,end-1,s,dp);
+
+        int left = helper(start,end-1,s,dp);
+        int right = helper(start+1,end,s,dp);
+
+        return dp[start][end] = max(left,right);
+    }
+
+    int longestPalindromeSubseq(string s) {
+        int n = s.length();
+        vector<vector<int>> dp(n, vector<int>(n,-1));
+        return helper(0,n-1,s,dp);
+    }
+};
+
+=======================================================================================
+    
+    
+// Tabulation
+
+
 class Solution {
 public:
     int longestPalindromeSubseq(string s) {

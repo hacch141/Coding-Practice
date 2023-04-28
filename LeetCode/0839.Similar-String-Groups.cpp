6cd0411b17e9ab3aded82f0691029b4588677a18
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-04-29 00:54:10 +0530

    Create 0839.Similar-String-Groups.cpp

---
+++ LeetCode/0839.Similar-String-Groups.cpp
@@ -0,0 +1,76 @@
+// 839. Similar String Groups
+
+// DFS
+class Solution {
+public:
+
+    void dfs(int idx, string currStr, vector<string>& strs, vector<bool>& vis, int& n) {
+        vis[idx] = true;
+        for(int i=0; i<n; i++) {
+            if(!vis[i] && isValid(currStr,strs[i])) {
+                dfs(i,strs[i],strs,vis,n);
+            }
+        }
+    }
+
+    bool isValid(string& s1, string& s2) {
+        int cnt = 0;
+        for(int i=0; i<s1.size(); i++) {
+            if(s1[i] != s2[i]) cnt++;
+        }
+        return cnt==0 || cnt==2;
+    }
+
+    int numSimilarGroups(vector<string>& strs) {
+        int n = strs.size();
+        vector<bool> vis(n,false);
+        int ans = 0;
+
+        for(int i=0; i<n; i++) {
+            if(!vis[i]) {
+                dfs(i,strs[i],strs,vis,n);
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+};
+
+
+// BFS
+class Solution {
+public:
+
+    void dfs(int idx, string currStr, vector<string>& strs, vector<bool>& vis, int& n) {
+        vis[idx] = true;
+        for(int i=0; i<n; i++) {
+            if(!vis[i] && isValid(currStr,strs[i])) {
+                dfs(i,strs[i],strs,vis,n);
+            }
+        }
+    }
+
+    bool isValid(string& s1, string& s2) {
+        int cnt = 0;
+        for(int i=0; i<s1.size(); i++) {
+            if(s1[i] != s2[i]) cnt++;
+        }
+        return cnt==0 || cnt==2;
+    }
+
+    int numSimilarGroups(vector<string>& strs) {
+        int n = strs.size();
+        vector<bool> vis(n,false);
+        int ans = 0;
+
+        for(int i=0; i<n; i++) {
+            if(!vis[i]) {
+                dfs(i,strs[i],strs,vis,n);
+                ans++;
+            }
+        }
+
+        return ans;
+    }
+};

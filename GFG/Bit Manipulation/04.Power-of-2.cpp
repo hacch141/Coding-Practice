0dab095c5ee23a8c15cf4814a9d5e04440e89525
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-05-31 23:16:41 +0530

    Create 04.Power-of-2.cpp

---
+++ GFG/Bit Manipulation/04.Power-of-2.cpp
@@ -0,0 +1,39 @@
+// Power of 2
+
+class Solution{
+    public:
+    // Function to check if given number n is a power of two.
+    bool isPowerofTwo(long long n){
+        
+        // Your code here    
+        int zero = n & (n-1);
+        
+        return zero == 0 ? true : false;
+    }
+};
+
+// T : O(1)
+// S : O(1)
+
+
+class Solution{
+    public:
+    // Function to check if given number n is a power of two.
+    bool isPowerofTwo(long long n){
+        
+        // Your code here    
+        int cntOnes = 0;
+        while(n>0) {
+            if(n&1) {
+                // if(cntOnes!=0) return false;
+                cntOnes++;
+            }
+            n = n>>1;
+        }
+        
+        return cntOnes == 1 ? true : false;
+    }
+};
+
+// T : O(N)
+// S : O(1)

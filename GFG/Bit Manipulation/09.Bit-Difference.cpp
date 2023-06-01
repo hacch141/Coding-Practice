f2d5be69e15ab476ab5611912e2c8960dc752785
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-06-01 19:47:29 +0530

    Create 09.Bit-Difference.cpp

---
+++ GFG/Bit Manipulation/09.Bit-Difference.cpp
@@ -0,0 +1,20 @@
+// Bit Difference
+
+class Solution{
+    public:
+    // Function to find number of bits needed to be flipped to convert A to B
+    int countBitsFlip(int a, int b){
+        
+        // Your logic here
+        int cnt = 0;
+        while(a || b) {
+            if((a&1) != (b&1)) cnt++;
+            a >>= 1;
+            b >>= 1;
+        }
+        return cnt;
+    }
+};
+
+// T : O(Log N)
+// S : O(1)

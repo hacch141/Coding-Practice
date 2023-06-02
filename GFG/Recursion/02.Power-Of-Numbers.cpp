6f65261b1a49ba9af18c07c5e1c6c13592679e6f
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-06-03 00:36:57 +0530

    Update 02.Power-Of-Numbers.cpp

--- GFG/Recursion/02.Power-Of-Numbers.cpp
+++ GFG/Recursion/02.Power-Of-Numbers.cpp
@@ -39,3 +39,35 @@ class Solution{
 
 // T : O(Log N)
 // S : O(1)
+
+
+
+
+class Solution{
+    public:
+    //You need to complete this fucntion
+    
+    long long power(int N,int R)
+    {
+        //Your code here
+        bool sign = false;
+        if(R<0) {
+            sign = true;
+            R = -R;
+        }
+        long long ans = 1;
+        long long num = (double)N;
+        long long modulo = 1e9+7;
+        
+        while(R) {
+            if(R&1) {
+                ans = (ans*num)%modulo;
+            }
+            num = (num*num)%modulo;
+            R >>= 1;
+        }
+        
+        if(sign) return (long long)1/ans;
+        return (long long)ans;
+    }
+};

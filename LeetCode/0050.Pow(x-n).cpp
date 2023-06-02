390a6fc1e15e77c42c13ca630f3d6dc453235f38
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-06-03 01:55:28 +0530

    Update 0050.Pow(x-n).cpp

--- LeetCode/0050.Pow(x-n).cpp
+++ LeetCode/0050.Pow(x-n).cpp
@@ -1,5 +1,26 @@
 // 50. Pow(x, n)
 
+// Recursion
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n==0) return 1;
+        if(x==1 || x==0) return x;
+        if(n<0) return 1/x*myPow(1/x,-(n+1));
+
+        double half = myPow(x,n/2);
+        double ans = half*half;
+        if(n%2 == 1) {
+            ans = ans*x;
+        }
+
+        return ans;
+    }
+};
+
+
+
+// Iterative
 class Solution {
 public:
     double myPow(double x, int n) {

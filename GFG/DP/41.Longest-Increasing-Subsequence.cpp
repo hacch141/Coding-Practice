8a592c75096b2e940e595844d496864f83b07a12
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-06-17 04:18:51 +0530

    Update 41.Longest-Increasing-Subsequence.cpp

--- GFG/DP/41.Longest-Increasing-Subsequence.cpp
+++ GFG/DP/41.Longest-Increasing-Subsequence.cpp
@@ -73,3 +73,26 @@ class Solution
        return curr[-1+1];
     }
 };
+
+
+class Solution
+{
+class Solution
+{
+    public:
+    //Function to find length of longest increasing subsequence.
+    int longestSubsequence(int n, int a[]) {
+       // your code here
+       vector<int> dp(n,1);
+       int maxi = 1;
+       for(int i=0; i<n; i++) {
+           for(int prev=0; prev<i; prev++) {
+               if(a[i]>a[prev] && 1+dp[prev]>dp[i]) {
+                   dp[i] = 1+dp[prev];
+               }
+           }
+           maxi = max(maxi,dp[i]);
+       }
+       return maxi;
+    }
+};

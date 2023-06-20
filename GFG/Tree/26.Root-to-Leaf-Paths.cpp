a9cf88ebe802abcd22eeea05f3966a3e8506813f
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-06-21 01:09:21 +0530

    Update 26.Root-to-Leaf-Paths.cpp

--- GFG/Tree/26.Root-to-Leaf-Paths.cpp
+++ GFG/Tree/26.Root-to-Leaf-Paths.cpp
@@ -1,5 +1,18 @@
 // Root to Leaf Paths
 
+// STRIVER
+bool getPath(node* root, vector<int>& arr, int x) {
+  if (!root) return false;
+  arr.push_back(root -> data);
+  if (root -> data == x) return true;
+  bool l = getPath(root -> left, arr, x);
+  bool r = getPath(root -> right, arr, x);
+  if (l || r) return true;
+  arr.pop_back();
+  return false;
+}
+
+// me
 void solve(Node* root, vector<int>& curr, vector<vector<int>>& ans) {
     curr.push_back(root->data);
     if(!root->left && !root->right) {

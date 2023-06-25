cdac0d5f66e54a9cf51e6575a66c2972200cb299
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-06-25 18:29:08 +0530

    Update 10.Iterative-Inorder-Traversal.cpp

--- GFG/Tree/10.Iterative-Inorder-Traversal.cpp
+++ GFG/Tree/10.Iterative-Inorder-Traversal.cpp
@@ -2,6 +2,33 @@
 
 class Solution {
   public:
+    // Return the Kth smallest element in the given BST
+    int KthSmallestElement(Node *root, int K) {
+        // add code here.
+        stack<Node*> st;
+        Node* curr = root;
+        vector<int> ans;
+        
+        while(curr || !st.empty()) {
+            while(curr) {
+                st.push(curr);
+                curr = curr -> left;
+            }
+            curr = st.top();
+            st.pop();
+            ans.push_back(curr -> data);             
+            curr = curr -> right;
+        }
+        return ans;
+    }
+};
+// T : O(N)
+// S : O(N)
+
+// ===============================================
+
+class Solution {
+  public:
     // Function to return a list containing the inorder traversal of the tree.
     vector<int> inOrder(Node* root) {
         // Your code here

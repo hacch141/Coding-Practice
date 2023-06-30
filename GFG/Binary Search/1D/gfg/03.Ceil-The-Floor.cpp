f95374567450d61c5bdc4ffaa8deb4ddfcf60929
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-06-30 17:18:08 +0530

    Update and rename 04.Ceil-The-Floor.cpp to 03.Ceil-The-Floor.cpp

--- GFG/Binary Search/1D/gfg/03.Ceil-The-Floor.cpp
+++ GFG/Binary Search/1D/gfg/03.Ceil-The-Floor.cpp
@@ -1,3 +1,5 @@
+// Ceil The Floor
+
 pair<int, int> getFloorAndCeil(int arr[], int n, int x) {
     // code here
     sort(arr,arr+n);
@@ -7,21 +9,16 @@ pair<int, int> getFloorAndCeil(int arr[], int n, int x) {
     pair<int,int> p;
     
     while(low <= high) {
-        
         int mid = (high-low)/2 + low;
-        
         if(x == arr[mid]) {
             return {x,x};
         }
-        
         else if(x < arr[mid]) {
             high = mid-1;
         }
-        
         else {
             low = mid+1;
         }
-        
     }
     
     if(high == -1) return {-1,arr[low]};
@@ -30,3 +27,30 @@ pair<int, int> getFloorAndCeil(int arr[], int n, int x) {
     
     return {arr[high],arr[low]};
 }
+
+// T : O(LogN)
+// S : O(1)
+
+
+// STRIVER
+// Implement Upper Bound
+int upperBound(vector<int> &arr, int x, int n) {
+    int low = 0, high = n - 1;
+    int ans = n;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        // maybe an answer
+        if (arr[mid] > x) {
+            ans = mid;
+            //look for smaller index on the left
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1; // look on the right
+        }
+    }
+    return ans;
+}
+// T : O(LogN)
+// S : O(1)

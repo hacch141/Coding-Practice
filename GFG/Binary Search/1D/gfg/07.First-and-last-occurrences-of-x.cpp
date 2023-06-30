4ac2bfa616b615619569d4c604437c0f658a0af4
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-06-30 18:06:55 +0530

    Update 07.First-and-last-occurrences-of-x.cpp

--- GFG/Binary Search/1D/gfg/07.First-and-last-occurrences-of-x.cpp
+++ GFG/Binary Search/1D/gfg/07.First-and-last-occurrences-of-x.cpp
@@ -1,40 +1,53 @@
+// First and last occurrences of x
+
+int find_first(int arr[], int n , int x) {
+    int low = 0;
+    int high = n -1;
+    int first = -1;
+    
+    while(low <= high) {
+        int mid = (high-low)/2 + low;
+        if(arr[mid] == x) {
+            first = mid;
+            high = mid - 1;
+        }
+        else if(arr[mid] > x) {
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1;
+        }
+    }
+    return first;
+}
+
+int find_last(int arr[], int n , int x) {
+    int low = 0;
+    int high = n -1;
+    int last = -1;
+    
+    while(low <= high) {
+        int mid = (high-low)/2 + low;
+        if(arr[mid] == x) {
+            last = mid;
+            low = mid + 1;
+        }
+        else if(arr[mid] < x) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+    return last;
+}
+
 vector<int> find(int arr[], int n , int x ) {
     // code here
-    int first, last;
-    
-    int low = 0;
-    int high = n-1;
-    bool flag = false;
-    
-    // binary search for first occurance
-    while(low<=high) {
-        int mid = (high-low)/2 +low;
-        if(arr[mid]==x) flag = true;
-        if(arr[mid] >= x) {
-            high = mid-1;
-        }
-        else {
-            low = mid+1;
-        }
-    }
-    first = low;
-    
-    
-    low = 0;
-    high = n-1;
-    
-    // binary search for last occurance
-    while(low<=high) {
-        int mid = (high-low)/2 +low;
-        if(arr[mid] <= x) {
-            low = mid+1;
-        }
-        else {
-            high = mid-1;
-        }
-    }
-    last = high;
-    
-    if(!flag) return {-1,-1};
+    int first = find_first(arr,n,x);
+    int last = find_last(arr,n,x);
     return {first,last};
 }
+
+// T : O(LogN)
+// S : O(1)

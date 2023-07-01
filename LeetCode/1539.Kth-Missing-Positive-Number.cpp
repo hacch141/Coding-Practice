019d41c7630c32c64beade0824ada77fa620c74b
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-07-02 01:41:22 +0530

    Update 1539.Kth-Missing-Positive-Number.cpp

--- LeetCode/1539.Kth-Missing-Positive-Number.cpp
+++ LeetCode/1539.Kth-Missing-Positive-Number.cpp
@@ -1,5 +1,35 @@
 // 1539. Kth Missing Positive Number
 
+// BS
+class Solution {
+public:
+    int findKthPositive(vector<int>& arr, int k) {
+        int n = arr.size();
+        
+        int low = 0;
+        int high = arr.size() - 1;
+
+        while(low <= high) {
+            int mid = (high-low)/2 + low;
+
+            if(arr[mid]-mid-1 >= k) {
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+
+        // arr[high] + more
+        // arr[high] + (k - misstill)
+        // arr[high] + (k - ((arr[high] - high - 1)))
+        // k + high + 1
+        return low + k;
+    }
+};
+
+
+
 class Solution {
 public:
     int findKthPositive(vector<int>& arr, int k) {

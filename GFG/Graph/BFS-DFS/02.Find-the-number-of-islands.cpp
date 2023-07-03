a9f8908b22de915c3ebffb5fd9445753e5c78be7
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-07-04 03:36:37 +0530

    Create 02.Find-the-number-of-islands.cpp

---
+++ GFG/Graph/BFS-DFS/02.Find-the-number-of-islands.cpp
@@ -0,0 +1,99 @@
+// Find the number of islands
+
+// BFS
+class Solution {
+    
+    private:
+    void bfs(int row, int col, vector<vector<char>>& grid, vector<vector<int>>& vis, int& n, int& m) {
+        vis[row][col] = 1;
+        queue<pair<int,int>> q;
+        q.push({row,col});
+        while(!q.empty()) {
+            int r = q.front().first;
+            int c = q.front().second;
+            q.pop();
+            for(int i=-1; i<=1; i++) {
+                for(int j=-1; j<=1; j++) {
+                    int nRow = r + i;
+                    int nCol = c + j;
+                    if(nRow>=0 && nRow<n && nCol>=0 && nCol<m && !vis[nRow][nCol] && grid[nRow][nCol]=='1') {
+                        vis[nRow][nCol] = 1;
+                        q.push({nRow,nCol});
+                        
+                    }
+                    
+                }
+            }
+        }
+    }
+        
+    public:
+    // Function to find the number of islands.
+    int numIslands(vector<vector<char>>& grid) {
+        // Code here
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        vector<vector<int>> vis(n, vector<int>(m,0));
+        int ans = 0;
+        
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                if(!vis[i][j] && grid[i][j]=='1') {
+                    ans++;
+                    bfs(i,j,grid,vis,n,m);
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
+
+// T : O(N*M)
+// S : O(N*M)
+
+
+// DFS
+class Solution {
+    
+    private:
+    void dfs(int row, int col, vector<vector<char>>& grid, vector<vector<int>>& vis, int& n, int& m) {
+        vis[row][col] = 1;
+        
+        for(int i=-1; i<=1; i++) {
+            for(int j=-1; j<=1; j++) {
+                int nRow = row + i;
+                int nCol = col + j;
+                if(nRow>=0 && nRow<n && nCol>=0 && nCol<m && !vis[nRow][nCol] && grid[nRow][nCol]=='1') {
+                    dfs(nRow,nCol,grid,vis,n,m);
+                }
+            }
+        }
+    }
+        
+    public:
+    // Function to find the number of islands.
+    int numIslands(vector<vector<char>>& grid) {
+        // Code here
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        vector<vector<int>> vis(n, vector<int>(m,0));
+        int ans = 0;
+        
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<m; j++) {
+                if(!vis[i][j] && grid[i][j]=='1') {
+                    ans++;
+                    dfs(i,j,grid,vis,n,m);
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
+
+// T : O(N*M)
+// S : O(N*M)

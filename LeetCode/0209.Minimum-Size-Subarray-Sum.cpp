96a56b2c3c5f330ba47661b3e5f76c377c641819
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-07-06 23:46:21 +0530

    Update 0209.Minimum-Size-Subarray-Sum.cpp

--- LeetCode/0209.Minimum-Size-Subarray-Sum.cpp
+++ LeetCode/0209.Minimum-Size-Subarray-Sum.cpp
@@ -4,19 +4,18 @@ class Solution {
 public:
     int minSubArrayLen(int target, vector<int>& nums) {
         int ans = INT_MAX;
-        int len = 0;
-        int currSum = 0;
-        int slow=0, fast=0;
-        int n = nums.size();
-        while(fast<n) {
-            while(fast<n && currSum<target) {
-                currSum += nums[fast++];
-            }
-            while(slow<=fast && currSum>=target) {
-                ans = min(ans,fast-slow);
-                currSum -= nums[slow++];
+        int left = 0;
+        int right = 0;
+        int sum = 0;
+
+        while(right < nums.size()) {
+            sum += nums[right++];
+            while(left <= right && sum >= target) {
+                ans = min(ans,right - left);
+                sum -= nums[left++];
             }
         }
-        return ans==INT_MAX ? 0 : ans;
+
+        return ans == INT_MAX ? 0 : ans;
     }
 };

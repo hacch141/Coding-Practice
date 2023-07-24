7c021eaefe394f7b12c6f1c98c90c11899fbf22b
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-07-25 05:06:51 +0530

    Create 12.Find-the-number-that-appears-once-and-the-other-numbers-twice.cpp

---
+++ GFG/Array/01.Easy/12.Find-the-number-that-appears-once-and-the-other-numbers-twice.cpp
@@ -0,0 +1,16 @@
+// Find the number that appears once, and the other numbers twice
+
+int getSingleElement(vector<int> &arr) {
+    //size of the array:
+    int n = arr.size();
+
+    // XOR all the elements:
+    int xorr = 0;
+    for (int i = 0; i < n; i++) {
+        xorr = xorr ^ arr[i];
+    }
+    return xorr;
+}
+
+// T : O(N)
+// S : O(1)

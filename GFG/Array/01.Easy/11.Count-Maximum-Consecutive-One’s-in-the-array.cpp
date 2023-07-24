0061e956a443dc04f7e51c990a219ffc11293b44
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-07-25 05:02:16 +0530

    Create 11.Count-Maximum-Consecutive-One’s-in-the-array.cpp

---
+++ GFG/Array/01.Easy/11.Count-Maximum-Consecutive-One’s-in-the-array.cpp
@@ -0,0 +1,19 @@
+// Count Maximum Consecutive One’s in the array
+
+int findMaxConsecutiveOnes(vector < int > & nums) {
+      int cnt = 0;
+      int maxi = 0;
+      for (int i = 0; i < nums.size(); i++) {
+        if (nums[i] == 1) {
+          cnt++;
+        } else {
+          cnt = 0;
+        }
+
+        maxi = max(maxi, cnt);
+      }
+      return maxi;
+    }
+
+// T : O(N)
+// S : O(1)

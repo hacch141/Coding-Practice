b43ec6ca98ce06f3cd43374bc33da743a8197b6b
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-07-25 04:37:02 +0530

    Create 08.Linear-Search.cpp

---
+++ GFG/Array/01.Easy/08.Linear-Search.cpp
@@ -0,0 +1,15 @@
+// Linear Search
+
+int linearSearch(int n, int num, vector<int> &arr)
+{
+    // Write your code here.
+    for(int i=0; i<n; i++) {
+        if(arr[i] == num) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+// T : O(N)
+// S : O(1)

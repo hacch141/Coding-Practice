f27ba5ecd88ff05c526613b99bae15762a36ee0d
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-07-26 03:10:28 +0530

    Update 05.Left-Rotate-an-Array-by-One.cpp

--- GFG/Array/01.Easy/05.Left-Rotate-an-Array-by-One.cpp
+++ GFG/Array/01.Easy/05.Left-Rotate-an-Array-by-One.cpp
@@ -2,7 +2,7 @@
 
 vector <int> rotateArray(vector<int>& arr, int n) {
     int firstElement = arr[0];
-    for (int  =0; i<n-1; i++) {
+    for (int  i=0; i<n-1; i++) {
         arr[i] = arr[i+1];
     }
     arr[n-1] = firstElement;

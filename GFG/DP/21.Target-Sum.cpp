837a96d123d2b546caaf16222f0ef3c505212d44
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-07-26 18:28:18 +0530

    Update 21.Target-Sum.cpp

--- GFG/DP/21.Target-Sum.cpp
+++ GFG/DP/21.Target-Sum.cpp
@@ -1,5 +1,39 @@
 // Target Sum
 
+// Recursion
+int countPartitionsUtil(int ind, int target, vector<int>& arr, vector<vector<int>> &dp){
+     if(ind == 0) {
+        if(target==0 && arr[0]==0) return 2;
+        if(target==0 || target == arr[0]) return 1;
+        return 0;
+    }
+    
+    if(dp[ind][target]!=-1) return dp[ind][target];
+        
+    int notTaken = countPartitionsUtil(ind-1,target,arr,dp);
+    int taken = 0;
+    if(arr[ind]<=target) {
+        taken = countPartitionsUtil(ind-1,target-arr[ind],arr,dp);
+    }   
+    return dp[ind][target]= (notTaken + taken);
+}
+
+int targetSum(int n,int target, vector<int>& arr){
+    int totSum = 0;
+    for(int i=0; i<arr.size();i++){
+        totSum += arr[i];
+    }
+    
+    //Checking for edge cases
+    if(totSum-target<0) return 0;
+    if((totSum-target)%2==1) return 0;
+    
+    int s2 = (totSum-target)/2;
+    
+    vector<vector<int>> dp(n,vector<int>(s2+1,-1));
+    return countPartitionsUtil(n-1,s2,arr,dp);
+}
+
 class Solution {
     
   private:
@@ -22,35 +56,6 @@ class Solution {
         }
         return dp[n-1][sum];
     }
-    
-    
-    
-// Recursion
-class Solution {
-  public:
-  
-    int solve(int idx, int& n, vector<int>&A, int curr, int& target, vector<vector<int>>& dp, int& add) {
-        if(idx==n) {
-            return curr==target ? 1 : 0;
-        }
-        
-        if(dp[idx][curr+add] != -1) return dp[idx][curr+add];
-        
-        int take = solve(idx+1,n,A,curr+A[idx],target,dp,add);
-        int remove = solve(idx+1,n,A,curr-A[idx],target,dp,add);
-        return dp[idx][curr+add] = take+remove;
-    }
-  
-    int findTargetSumWays(vector<int>&A ,int target) {
-        //Your code here
-        int n = A.size();
-        int curr = 0;
-        int sum = 0;
-        for(auto i : A) sum += i;
-        vector<vector<int>> dp(n,vector<int>((2*sum) + 2,-1));
-        return solve(0,n,A,curr,target,dp,sum);
-    }
-};
     
   public:
     int findTargetSumWays(vector<int>&A ,int target) {

0d5240f59806fdf922488b1d0a68321a8c50fdd4
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-07-30 17:52:10 +0530

    Update 11.Iterative-Postorder-Traversal-2Stacks.cpp

--- GFG/Tree/11.Iterative-Postorder-Traversal-2Stacks.cpp
+++ GFG/Tree/11.Iterative-Postorder-Traversal-2Stacks.cpp
@@ -24,3 +24,23 @@ vector <int> postOrder(Node* root) {
 
 // T : O(N)
 // S : O(N)
+
+// BY USING REV 
+vector<int> postorderTraversal(TreeNode* root)
+{
+    // Write your code here	
+    vector<int> ans;
+    stack<TreeNode*> st;
+    st.push(root);
+    while(!st.empty()) {
+        TreeNode* curr = st.top();
+        st.pop();
+        ans.push_back(curr->val);
+        if(curr->left) st.push(curr->left);
+        if(curr->right) st.push(curr->right);
+    }
+    reverse(ans.begin(),ans.end());
+    return ans;
+}
+// T : O(N)
+// S : O(N)

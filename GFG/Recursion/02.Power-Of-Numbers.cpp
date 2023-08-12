4e9f93c6603bb0489762e7fbde216cf9cda4b445
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-08-12 20:25:37 +0530

    Update 02.Power-Of-Numbers.cpp

--- GFG/Recursion/02.Power-Of-Numbers.cpp
+++ GFG/Recursion/02.Power-Of-Numbers.cpp
@@ -1,98 +1,51 @@
 // Power Of Numbers
 
-
 // Recursion
-class Solution{
-    public:
-    //You need to complete this fucntion
-    int modulo = 1e9+7;
-    
-    long long power(int N,int R)
-    {
-        //Your code here
-        if(N==1 || N==0) return N;
-        if(R==0) return 1;
-        
-        long long half = power(N,R/2);
-        long long ans = (half*half)%modulo;
-        if(R%2==1) {
-            ans = (ans*N)%modulo;
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if(n==0) return 1;
+        if(x==1 || x==0) return x;
+
+        // for n = -2147483648 we convert this n to positive and it is overflow 
+        // Hence we used 1times * -(n+1)times
+        if(n<0) return 1/x*myPow(1/x,-(n+1));
+
+        double half = myPow(x,n/2);
+        double ans = half*half;
+        if(n%2 == 1) {
+            ans = ans*x;
         }
-        
+
         return ans;
     }
 };
+// T : O(Log N)
+// S : O(Log N)
 
 
 // Iterative
-class Solution{
-    public:
-    //You need to complete this fucntion
-    
-    long long power(int N,int R)
-    {
-        //Your code here
-        bool sign = false;
-        if(R<0) {
-            sign = true;
-            R = -R;
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double num = x;
+        long long pow = n;
+        double ans = 1;
+
+        if(n<0) {
+            pow = -(1LL*n);
         }
-        long long ans;
-        long long num = (double)N;
-        long long modulo = 1e9+7;
-        
-        if(R&1) {
-            ans = N;
-        }
-        else {
-            ans = 1;
-        }
-        
-        R >>= 1;
-        while(R) {
-            num = (num*num)%modulo;
-            if(R&1) {
-                ans = (ans*num)%modulo;
+
+        while(pow) {
+            if(pow&1) {
+                ans = ans*num;
             }
-            R >>= 1;
+            num = (num*num);
+            pow >>= 1;
         }
         
-        if(sign) return (long long)1.0/ans;
-        return (long long)ans;
+        return n > 0 ? ans : 1/ans;
     }
 };
-
 // T : O(Log N)
 // S : O(1)
-
-
-
-
-class Solution{
-    public:
-    //You need to complete this fucntion
-    
-    long long power(int N,int R)
-    {
-        //Your code here
-        bool sign = false;
-        if(R<0) {
-            sign = true;
-            R = -R;
-        }
-        long long ans = 1;
-        long long num = (double)N;
-        long long modulo = 1e9+7;
-        
-        while(R) {
-            if(R&1) {
-                ans = (ans*num)%modulo;
-            }
-            num = (num*num)%modulo;
-            R >>= 1;
-        }
-        
-        if(sign) return (long long)1/ans;
-        return (long long)ans;
-    }
-};

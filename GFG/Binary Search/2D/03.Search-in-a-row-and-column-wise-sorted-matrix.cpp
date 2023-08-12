2af30f7dba31e66d28a5cb9dbbb084faa1e46930
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-08-13 02:17:55 +0530

    Update 03.Search-in-a-row-and-column-wise-sorted-matrix.cpp

--- GFG/Binary Search/2D/03.Search-in-a-row-and-column-wise-sorted-matrix.cpp
+++ GFG/Binary Search/2D/03.Search-in-a-row-and-column-wise-sorted-matrix.cpp
@@ -35,3 +35,29 @@ public:
 };
 // T : O(m * Log n)
 // S : O(1)
+
+// Optimised
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int i = 0;
+        int j = m - 1;
+        while(i<n && j>=0) {
+            if(matrix[i][j] == target) {
+                return true;
+            }
+            else if(matrix[i][j] < target) {
+                i++;
+            }
+            else if(matrix[i][j] > target) {
+                j--;
+            }
+        }
+        return false;
+    }
+};
+
+// T : O(n+m)
+// S : O(1)

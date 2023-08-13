77a351cdceee893ca21c7523e21e8e100a3d8205
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-08-13 19:42:36 +0530

    Update 17.Possible-Words-From-Phone-Digits.cpp

--- GFG/Recursion/17.Possible-Words-From-Phone-Digits.cpp
+++ GFG/Recursion/17.Possible-Words-From-Phone-Digits.cpp
@@ -1,32 +1,31 @@
 // Possible Words From Phone Digits
 
-class Solution
-{
-    public:
-    
-    void solve(int idx, int a[], int& N, string curr, vector<string>& ans, vector<string>& keypad) {
-        if(idx==N) {
-            ans.push_back(curr);
-            return;
-        }
-        string str = keypad[a[idx]];
-        for(int i=0; i<str.length(); i++) {
-            solve(idx+1,a,N,curr+str[i],ans,keypad);
-        }
+void solve(int idx, string& curr, string& s,
+ vector<string>& ans, vector<string>& phone) {
+    if(idx == s.size()) {
+        ans.push_back(curr);
+        return;
     }
-    
-    //Function to find list of all words possible by pressing given numbers.
-    vector<string> possibleWords(int a[], int N)
-    {
-        //Your code here
-        vector<string> keypad = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
-        vector<string> ans;
-        string curr = "";
-        solve(0,a,N,curr,ans,keypad);
-        sort(ans.begin(),ans.end());
-        return ans;
-    }
-};
 
-// T : O(n*4^n)
+    string letters = phone[s[idx]-'0']; 
+    for(int i=0; i<letters.size(); i++) {
+        curr += letters[i];
+        solve(idx+1,curr,s,ans,phone);
+        curr.pop_back();
+    }
+}
+
+vector<string> letterCombinations(string s) {
+    // Write your code here.
+    vector<string> phone = {"0","1","abc","def","ghi","jkl","mno","pqrs",
+    "tuv","wxyz"};
+
+    vector<string> ans;
+    string curr = "";
+
+    solve(0,curr,s,ans,phone);
+    return ans;
+}
+
+// T : O(N * 4^N) => N is the length of the input string s
 // S : O(N)

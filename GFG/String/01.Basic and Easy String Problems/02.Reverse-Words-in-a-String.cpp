c7908d2c278f10bffb340d0622cfd84734720764
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-08-15 18:48:12 +0530

    Create 02.Reverse-Words-in-a-String.cpp

---
+++ GFG/String/01.Basic and Easy String Problems/02.Reverse-Words-in-a-String.cpp
@@ -0,0 +1,80 @@
+// Reverse Words in a String
+
+#include <bits/stdc++.h>
+
+string reverseString(string &str){
+  	// Write your code here.
+  	string curr = "";
+  	string ans = "";
+  	for(auto i : str) {
+    		if(i != ' ') {
+    			  curr += i;
+    		}
+    		else {
+      			if(curr.length() > 0) {
+        				if(ans.length() > 0) {
+        					  ans = curr + " " + ans;
+        				}
+        				else {
+        					  ans = curr;
+        				}
+      			}
+      			curr = "";
+    		}
+  	}
+  
+  	if(curr.length() > 0) {
+  		  if(ans.length() > 0) {
+  			    ans = curr + " " + ans;
+  		  }
+  		  else {
+  			    ans = curr;
+  		  }  		
+  	}
+  
+  	return ans;
+}
+
+// T : O(N)
+// S : O(N)
+
+
+/*
+    Time Complexity  = O(N * 2)
+    Space Complexity = O(N)
+   
+    Where N is the length of the string.
+*/
+
+string reverseString(string &str)
+{
+    int n = str.length();
+  	string ans;
+  	int i = 0;
+  
+  	while (i < n) 
+  	{
+  		  int j = i;
+    		// Skip multiple spaces.
+    		while (j < n && str[j] == ' ') {
+    			  j++;
+    		}
+  		  string currentWord;
+  		  // Get the current word.
+  		  while (j < n && str[j] != ' ') {
+  			    currentWord.push_back(str[j]);
+  			    j++;
+  		  }
+    		// Add current word in the ans with a space.
+    		if (currentWord.length() != 0) {
+    			  ans.insert(0, currentWord + " ");
+    		}
+    		i = j + 1;
+    }
+  
+  	if (ans.length() == 0) {
+    		return ans;
+  	}
+  	// Remove the last space.
+  	return ans.substr(0, ans.length() - 1);
+}

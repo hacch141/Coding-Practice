7abd9baecb36710383e2bb79dc99a626d48301b9
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-08-20 16:46:26 +0530

    Create 02.Majority-Element-(n|3-times).cpp

---
+++ GFG/Array/03.Hard/02.Majority-Element-(n|3-times).cpp
@@ -0,0 +1,107 @@
+// Majority Element (n/3 times)
+
+#include <bits/stdc++.h>
+
+vector<int> majorityElement(vector<int> v) {
+  	// Write your code here
+  	int n = v.size();
+  
+  	int cnt1=0, cnt2=0;
+  	int ele1=INT_MAX, ele2=INT_MAX;
+  
+  	for(int i=0; i<n; i++) {
+    		if(cnt1==0 && v[i]!=ele2) {
+      			cnt1 = 1;
+      			ele1 = v[i];
+    		}
+    		else if(cnt2==0 && v[i]!=ele1) {
+      			cnt2 = 1;
+      			ele2 = v[i];
+    		}
+    		else if(v[i] == ele1) {
+    			  cnt1++;
+    		}
+    		else if(v[i] == ele2) {
+    			  cnt2++;
+    		}
+    		else {
+      			cnt1--;
+      			cnt2--;
+    		}
+  	}
+  
+  	vector<int> ans;
+  	int c1=0, c2=0;
+  	for(auto i : v) {
+    		if(i==ele1) c1++;
+  		  if(i==ele2) c2++;
+  	}
+  
+  	if(c1>n/3) ans.push_back(ele1);
+  	if(c2>n/3) ans.push_back(ele2);
+  
+  	sort(ans.begin(),ans.end());
+  	
+  	return ans;
+}
+// T : O(N)
+// S : O(1)
+
+
+vector<int> majorityElement(vector<int> v) {
+    int n = v.size(); //size of the array
+    vector<int> ls; // list of answers
+
+    //declaring a map:
+    map<int,int> mpp;
+
+    // least occurrence of the majority element:
+    int mini = int(n / 3) + 1;
+
+    //storing the elements with its occurnce:
+    for (int i = 0; i < n; i++) {
+        mpp[v[i]]++;
+
+        //checking if v[i] is
+        // the majority element:
+        if (mpp[v[i]] == mini) {
+            ls.push_back(v[i]);
+        }
+        if (ls.size() == 2) break;
+    }
+
+    return ls;
+}
+// T : O(N * Log N)
+// S : O(N)
+
+
+vector<int> majorityElement(vector<int> v) {
+    int n = v.size(); //size of the array
+    vector<int> ls; // list of answers
+
+    for (int i = 0; i < n; i++) {
+        //selected element is v[i]:
+        // Checking if v[i] is not already
+        // a part of the answer:
+        if (ls.size() == 0 || ls[0] != v[i]) {
+            int cnt = 0;
+            for (int j = 0; j < n; j++) {
+                // counting the frequency of v[i]
+                if (v[j] == v[i]) {
+                    cnt++;
+                }
+            }
+
+            // check if frquency is greater than n/3:
+            if (cnt > (n / 3))
+                ls.push_back(v[i]);
+        }
+
+        if (ls.size() == 2) break;
+    }
+
+    return ls;
+}
+// T : O(N^2)
+// S : O(1)

1400de9265f42a66d793428ac3e94119e298edaf
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-08-22 02:59:19 +0530

    Update 05.Postfix-to-Infix.cpp

--- GFG/Stack and Queue/02.Prefix, Infix, PostFix Conversion Problems/05.Postfix-to-Infix.cpp
+++ GFG/Stack and Queue/02.Prefix, Infix, PostFix Conversion Problems/05.Postfix-to-Infix.cpp
@@ -29,3 +29,65 @@ string postToInfix(string postfix) {
 
 // T : O(N)
 // S : O(N)
+
+
+#include <bits/stdc++.h>
+
+class Node {
+    public:
+    char data;
+    Node *left, *right;
+    
+    Node(char data) {
+        this->data = data;
+        this->left = NULL;
+        this->right = NULL;
+    }
+};
+
+bool isOperator(char c) {
+    return (!isalpha(c) && !isdigit(c));
+}
+
+void helper(Node*& head, string& ans) {
+	if(!head) return;
+	ans += '(';
+	helper(head->left,ans);
+	ans.pop_back();
+	ans += head->data;
+	helper(head->right,ans);
+	ans += ')';
+}
+
+string postToInfix(string postfix) {
+    // Write your code here.
+    int n = postfix.length();
+	stack<Node*> st;
+
+    for(int i=0; i<n; i++) {
+        if(isOperator(postfix[i])) {
+            Node* b = st.top();
+            st.pop();
+            Node* a = st.top();
+            st.pop();
+
+            Node* newHead = new Node(postfix[i]);
+            newHead->left = a;
+            newHead->right = b;
+
+            st.push(newHead);
+        }
+        else {
+            Node* a = new Node(postfix[i]);
+            st.push(a);
+        }
+    }
+
+    string ans;
+	helper(st.top(),ans);
+	ans.resize(ans.size()-1);
+
+	return ans;
+}
+// T : O(N)
+// S : O(N)

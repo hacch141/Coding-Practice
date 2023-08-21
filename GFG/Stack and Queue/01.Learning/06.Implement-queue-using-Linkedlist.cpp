867b94c9584df9f5ef207b1195fac115badb8af5
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-08-21 20:39:19 +0530

    Create 06.Implement-queue-using-Linkedlist.cpp

---
+++ GFG/Stack and Queue/01.Learning/06.Implement-queue-using-Linkedlist.cpp
@@ -0,0 +1,76 @@
+// Implement queue using Linkedlist
+
+class QueueNode
+{
+ public: 
+    int val;
+    QueueNode *next;
+    QueueNode(int data)
+    {
+       val = data;
+       next = nullptr;
+    }
+};  
+QueueNode *Front = nullptr, *Rare = nullptr;
+
+class Queue
+{
+public:
+    int size = 0;
+    bool Empty();
+    void Enqueue(int value);
+    void Dequeue();
+    int Peek();
+};  
+
+bool Queue ::  Empty()
+{
+    return Front == nullptr;
+}  
+
+int Queue :: Peek()
+{
+    if(Empty())  
+     {  cout<<"Queue is Empty"<<endl;
+        return -1;
+     } 
+    else 
+      return Front->val;
+}   
+
+void Queue :: Enqueue(int value)
+{
+    QueueNode *Temp;
+    Temp = new QueueNode(value); 
+    if (Temp == nullptr)  //When heap exhausted 
+        cout << "Queue is Full" << endl;
+    else
+    {
+        if (Front == nullptr)
+        {
+            Front = Temp;
+            Rare = Temp;
+        } 
+        else
+        {
+            Rare->next = Temp;
+            Rare = Temp;
+        }
+        cout<<value <<" Inserted into Queue "<<endl;
+        size++;
+    } 
+}    
+
+void Queue :: Dequeue() 
+{
+    if (Front == nullptr) 
+        cout << "Queue is Empty" << endl;
+    else
+    { 
+        cout<<Front->val <<" Removed From Queue"<<endl;
+        QueueNode *Temp = Front;
+        Front = Front->next;
+        delete Temp;
+        size--;
+    }  
+}   

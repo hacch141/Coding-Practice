dde5597df32d14fe10ea988366c588d35e7f3819
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-08-28 23:39:53 +0530

    Update 03.Integer-To-Roman-Numeral.cpp

--- GFG/String/02.Medium String Problems/03.Integer-To-Roman-Numeral.cpp
+++ GFG/String/02.Medium String Problems/03.Integer-To-Roman-Numeral.cpp
@@ -1,5 +1,30 @@
 // Integer To Roman Numeral
 
+class Solution {
+public:
+    string intToRoman(int num) {
+
+        string m[] = { "", "M", "MM", "MMM" };
+        string c[] = { "", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM" };
+        string x[] = { "", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC" };
+        string i[] = { "", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+
+        // Converting diffrent places digits into roman.
+        string thousands = m[num / 1000];
+        string hundereds = c[(num % 1000) / 100];
+        string tens = x[(num % 100) / 10];
+        string ones = i[num % 10];
+
+        string ans = thousands + hundereds + tens + ones;
+
+        return ans;
+    }
+};
+// T : O(1)
+// S : O(1)
+
+
+
 #include <bits/stdc++.h> 
 
 string intToRoman(int num) {

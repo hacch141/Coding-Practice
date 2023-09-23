964a98774e74ed7fad4d7a36b912d2a697ba9b28
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-09-24 03:08:30 +0530

    Update 04.Minimum-Platforms.cpp

--- GFG/Greedy/02.Medium-Hard/04.Minimum-Platforms.cpp
+++ GFG/Greedy/02.Medium-Hard/04.Minimum-Platforms.cpp
@@ -1,5 +1,27 @@
 // Minimum Platforms
 
+    int findPlatform(int arr[], int dep[], int n)
+    {
+    	// Your code here
+    	vector<int> pt(2461,0);
+    	
+    	for(int i=0; i<n; i++) {
+    	    pt[arr[i]]++;
+    	    pt[dep[i]+1]--;
+    	    
+    	}
+    	
+    	int curr = 0, ans=1;
+    	for(auto i : pt) {
+    	    curr += i;
+    	    ans = max(ans,curr);
+    	}
+    	
+    	return ans;
+    }
+// T : O(N)
+// S : O(1)
+
 class Solution{
     public:
     //Function to find the minimum number of platforms required at the

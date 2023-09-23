9fbfdb3a5c6dc75afb5dcabeca3b983ff8388f2e
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-09-23 16:08:59 +0530

    Update 04.Minimum-Window-Subsequence.cpp

--- GFG/Sliding Window & Two Pointer Combined Problems/02.Hard Problems/04.Minimum-Window-Subsequence.cpp
+++ GFG/Sliding Window & Two Pointer Combined Problems/02.Hard Problems/04.Minimum-Window-Subsequence.cpp
@@ -29,3 +29,46 @@ string minWindow(string S, string T)
 }
 // T : O(N^2)
 // S : O(N)
+
+
+
+
+string minWindow(string S, string T)
+{
+	// Write your Code here
+	int n = S.length();
+	int m = T.length();
+
+	string ans = "";
+
+	int i=0, j=0, mini=n;
+
+	while(i<n) {
+		if(S[i] == T[j]) {
+			j++;
+
+			if(j==m) {
+				int end = i;
+				j--;
+
+				while(j>=0) {
+					if(S[i] == T[j]) {
+						j--;
+					}
+					i--;
+				}
+
+				i++;
+				j++;
+
+				if(end-i < mini) {
+					mini = end - i;
+					ans = S.substr(i,end-i+1);
+				}
+			}
+		}
+		i++;
+	}
+
+	return ans;
+}

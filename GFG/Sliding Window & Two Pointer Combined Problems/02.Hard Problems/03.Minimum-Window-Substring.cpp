907a354eab87d370c1584f761265b16d563f7504
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-09-23 13:56:36 +0530

    Create 03.Minimum-Window-Substring.cpp

---
+++ GFG/Sliding Window & Two Pointer Combined Problems/02.Hard Problems/03.Minimum-Window-Substring.cpp
@@ -0,0 +1,77 @@
+// Minimum Window Substring
+
+#include <bits/stdc++.h> 
+string minSubstring(string &a, string &b)
+{
+    // Write your code here.
+    if(a.length()==0 || a.length()<b.length()) {
+        return "";
+    }
+
+    unordered_map<char,int> mpa, mpb;
+    for(auto ch : b) {
+        mpb[ch]++;
+    }
+
+    int i=0, j=0, cnt=0;
+    int n = a.length();
+    string ans = "";
+
+    while(j<n) {
+        mpa[a[j]]++;
+        if(mpa[a[j]] <= mpb[a[j]]) {
+            cnt++;
+        }
+
+        while(i<j && cnt==b.length()) {
+            string curr = a.substr(i,j-i+1);
+            if(ans.length()==0 || curr.length() < ans.length()) {
+                ans = curr;
+            }
+            mpa[a[i]]--;
+            if(mpa[a[i]] < mpb[a[i]]) {
+                cnt--;
+            }
+            i++;
+        }
+        
+        j++;
+    }
+
+    return ans;
+}
+// T : O(N)
+// S : O(N)
+
+
+
+#include <bits/stdc++.h> 
+
+// Function to check whether the current window contains
+// all the characters of b or not
+
+bool isEnough(int curr[],int req[]) {
+    for(int i=0;i<26;i++) {
+        if(curr[i]<req[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int minSubstring(string &a, string &b) {
+    int req[26]={0},curr[26]={0},j=0,ans=INT_MAX;
+    for(int i=0;i<b.length();i++) {
+        req[b[i]-'a']++;
+    }
+    for(int i=0;i<a.length();i++) {
+        curr[a[i]-'a']++;
+        while(isEnough(curr,req)) {
+            ans=min(ans,i-j+1);
+            curr[a[j++]-'a']--;
+        }
+    }
+    return ans==INT_MAX?-1:ans;
+}
+// T : O(N)
+// S : O(1)

ab8adef3a5c10cb77ae4c390c77db4f166c5dc51
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-09-30 04:21:20 +0530

    Update 0028.Find-the-Index-of-the-First-Occurrence-in-a-String.cpp

--- LeetCode/0028.Find-the-Index-of-the-First-Occurrence-in-a-String.cpp
+++ LeetCode/0028.Find-the-Index-of-the-First-Occurrence-in-a-String.cpp
@@ -1,6 +1,58 @@
 // 28. Find the Index of the First Occurrence in a String
 
 class Solution {
+public:
+
+    bool isValid(string& a, string& b, int i, int j) {
+        int n = a.length();
+        int m = b.length();
+        while(i<n && j<m) {
+            if(a[i] != b[j]) return false;
+            i++;
+            j++;
+        }
+        return true;
+    }
+
+    int strStr(string haystack, string needle) {
+        int n = haystack.length(), m = needle.length();
+        const long long p = 26, mod = 1e9 + 7;
+
+        long long max_pow = 1;
+        for(int i=1; i<=m-1; i++) {
+            max_pow = (max_pow * p) % mod;
+        }
+
+        long long hash_pat=0, hash_text=0;
+        for(int i=0; i<m; i++) {
+            hash_pat = ((hash_pat*p)%mod + needle[i]-'a'+1)%mod;
+            hash_text = ((hash_text*p)%mod + haystack[i]-'a'+1)%mod;
+        }
+
+        if(hash_pat == hash_text) {
+            if(isValid(haystack,needle,0,0)) {
+                return 0;
+            }
+        }
+
+        for(int i=0; i<=n-m; i++) {
+            hash_text = (hash_text - ((haystack[i]-'a'+1)*max_pow)%mod + mod)%mod;
+            hash_text = ((hash_text*p)%mod + haystack[i+m]-'a'+1)%mod;
+            if(hash_pat == hash_text) {
+                if(isValid(haystack,needle,i+1,0)) {
+                    return i+1;
+                }
+            }
+        }
+
+        return -1;
+    }
+};
+
+
+
+
+class Solution {
 
 private:
     bool isValid(int start, int m, string haystack, string needle) {
@@ -19,49 +71,4 @@ public:
         }
         return -1;
     }
-    
-    // =======================================================
-    
-    
-    int strStr(string haystack, string needle) {
-      return haystack.find(needle);
-    }
-
-    // =======================================================
-    
-    string st(string & haystack, int i, int j ){
-        string temp = "";
-        for (int k = i; k<=j; k++){
-            temp += haystack[k];
-        }
-        return temp;
-
-    }
-    int strStr(string haystack, string needle) {
-        int m = haystack.size();
-        int n = needle.size();
-        if ((n>m) || (n==m && haystack != needle ))
-        return -1;
-
-        for (int i = n-1; i<m; i++){
-            if (st(haystack, i-n+1, i) == needle){
-                return i-n+1;
-            }
-        }
-        
-        return -1;
-    }
-
-    // =======================================================
-    int strStr(string h, string n) {
-        if(n.length()>h.length()) return -1;
-        for(int i = 0; i<h.length()-n.length()+1; i++){
-            if(h.substr(i, n.length())==n) return i;
-        }
-        return -1;
-    }
-    
-    // =======================================================
-    
-    
 };

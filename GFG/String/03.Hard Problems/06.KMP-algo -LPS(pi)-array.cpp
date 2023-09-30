7cabc00729b29c014dcf9935006bc69f840aab0f
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-09-30 05:35:22 +0530

    Update 06.KMP-algo -LPS(pi)-array.cpp

--- GFG/String/03.Hard Problems/06.KMP-algo -LPS(pi)-array.cpp
+++ GFG/String/03.Hard Problems/06.KMP-algo -LPS(pi)-array.cpp
@@ -1,53 +1,58 @@
-/*
-    Time complexity: O(n + m)
-    Space complexity: O(m)
+#include <bits/stdc++.h>
 
-    Where 'n' and 'm' are the lengths of 'text' and 'pattern' respectively.
-*/
-
-vector<int> stringMatch(string text, string pattern) {
-
-    int n = text.size(), m = pattern.size();
-
-    // List storing the indices of occurrences
-    vector<int> ans;
-
-    // Initializing LPS array
+vector<int> compute_LPS(string& pattern, int m) {
     vector<int> lps(m);
     lps[0] = 0;
-    int i = 1, j = 0;
-    while (i < m) {
-        if (pattern[i] == pattern[j]) {
+    int j=0, i=1;
+    while(i<m) {
+        if(pattern[i] == pattern[j]) {
             lps[i] = j + 1;
             i++;
             j++;
-        } else if (j > 0)
-            j = lps[j - 1];
+        }
         else {
-            lps[i] = 0;
-            i++;
+            if(j > 0) {
+                j = lps[j-1];
+            }
+            else {
+                lps[i] = 0;
+                i++;
+            }
         }
     }
+    return lps;
+}
 
-    i = 0;
-    j = 0;
-    while (i < n) {
-        if (text[i] == pattern[j]) {
+vector<int> stringMatch(string text, string pattern) {
+	// Write your code here.
+	vector<int> ans;
+    int n = text.length();
+    int m = pattern.length();
+
+    vector<int> lps = compute_LPS(pattern, m);
+
+    int i=0, j=0;
+    while(i<n) {
+        if(text[i] == pattern[j]) {
             i++;
             j++;
-        } else if (j > 0)
-            j = lps[j - 1];
-        else
-            i++;
-
-        // If the pattern matches
-        if (j == m) {
-            ans.push_back(i - m + 1);
-            j = lps[j - 1];
+        }
+        else {
+            if(j > 0) {
+                j = lps[j-1];
+            }
+            else {
+                i++;
+            }
+        }
+        if(j == m) {
+            ans.push_back(i-m+1);
+            j = lps[j-1];
         }
     }
 
-    return ans;
+	return ans;
 }
-// T : O(M+N)
-// S : O(len of pattern)
+
+// T : O(N+M)
+// S : O(M)

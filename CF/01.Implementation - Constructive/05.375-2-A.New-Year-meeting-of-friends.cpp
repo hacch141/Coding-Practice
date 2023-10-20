c6a7dbf87db273048cf63393f2e2d644cb37fdf6
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-10-21 00:58:45 +0530

    Create 05.375-2-A.New-Year-meeting-of-friends.cpp

---
+++ CF/01.Implementation - Constructive/05.375-2-A.New-Year-meeting-of-friends.cpp
@@ -0,0 +1,15 @@
+// A. New Year: meeting of friends
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+    int x1, x2, x3;
+    cin >> x1 >> x2 >> x3;
+    int maxi = max(x1, max(x2,x3));
+    int mini = min(x1, min(x2,x3));
+    cout << maxi-mini;
+    
+    return 0;
+}

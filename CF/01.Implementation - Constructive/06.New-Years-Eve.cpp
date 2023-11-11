09edc55dd17f16d981036e459dba2dc0765ef97a
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-11-11 13:23:14 +0530

    Create 06.New-Years-Eve.cpp

---
+++ CF/01.Implementation - Constructive/06.New-Years-Eve.cpp
@@ -0,0 +1,32 @@
+// 06-New-Years-Eve
+
+#include <bits/stdc++.h>
+using namespace std;
+
+long long int find_pow(long long num) {
+    long long int cnt = 0;
+    while(num > 0) {
+        cnt++;
+        num >>= 1;
+    }
+    return cnt;
+}
+
+int main() {
+    long long int n, k, ans;
+    cin >> n >> k;
+    long long int maxPow = find_pow(n);
+
+    if(k == 1) ans = n;
+    else {
+        long long int val = 1;
+        int power=1;
+        for(int i=0; i<maxPow; i++) {
+            val *= 2;
+        }
+        ans = val-1;
+    }
+
+    cout << ans << endl;
+    return 0;
+}

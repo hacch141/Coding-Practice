b3a482576a7bab8413f33498b7160f9f70ecdb65
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-11-18 02:36:07 +0530

    Update 0501.Find-Mode-in-Binary Search-Tree.cpp

--- LeetCode/0501.Find-Mode-in-Binary Search-Tree.cpp
+++ LeetCode/0501.Find-Mode-in-Binary Search-Tree.cpp
@@ -3,6 +3,47 @@
 class Solution {
 public:
 
+    void solve(TreeNode* root, int& prev, int& freq, int& maxFreq, vector<int>& ans) {
+        if(!root) return;
+
+        solve(root->left,prev,freq,maxFreq,ans);
+        if(prev == root->val) {
+            freq++;
+        }
+        else {
+            freq = 1;
+        }
+
+        if(freq == maxFreq) {
+            ans.push_back(root->val);
+        }
+        else if(freq > maxFreq) {
+            maxFreq = freq;
+            ans = {root->val};
+        }
+
+        prev = root->val;
+        solve(root->right,prev,freq,maxFreq,ans);
+    }
+
+    vector<int> findMode(TreeNode* root) {
+        vector<int> ans;
+        int prev = -1e9;
+        int freq = 1;
+        int maxFreq = 1;
+        solve(root,prev,freq,maxFreq,ans);
+        return ans;
+    }
+};
+
+
+// ===========================================================================
+
+
+// using map
+class Solution {
+public:
+
     void helper(TreeNode* root, unordered_map<int,int>& mp) {
         if(root) {
             mp[root->val]++;

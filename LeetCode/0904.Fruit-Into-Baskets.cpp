de4bb072e54f53dfc0f2d5f03121cbbe71808820
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-11-18 20:11:09 +0530

    Update 0904.Fruit-Into-Baskets.cpp

--- LeetCode/0904.Fruit-Into-Baskets.cpp
+++ LeetCode/0904.Fruit-Into-Baskets.cpp
@@ -3,60 +3,18 @@
 class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
-        int n = fruits.size();
+        int l=0, r=0, ans=0, n=fruits.size();
         unordered_map<int,int> mp;
-        int maxi = 0;
-        int cnt = 0;
-        int ptr = 0;
-        
-        for(int i=0; i<n; i++) {
-            mp[fruits[i]]++;
-            cnt++;
-            if(mp.size() <= 2) {
-                maxi = max(maxi,cnt);
-            }
 
-            while(mp.size()>2) {
-                mp[fruits[ptr]]--;
-                cnt--;
-                if(mp[fruits[ptr]] == 0) mp.erase(fruits[ptr]);
-                ptr++;
+        for(r=0; r<n; r++) {
+            mp[fruits[r]]++;
+            while(mp.size() > 2) {
+                mp[fruits[l]]--;
+                if(mp[fruits[l]] == 0) mp.erase(fruits[l]);
+                l++;
             }
-            maxi = max(maxi,cnt);
+            ans = max(ans, r-l+1);
         }
-        return maxi;
+        return ans;
     }
-}; 
-
-
-
-
-
-// ============================================
-
-class Solution {
-public:
-    int totalFruit(vector<int>& fruits) {
-        int n = fruits.size();
-        unordered_map<int,int> mp;
-        int maxi = 0;
-        int cnt = 0;
-        queue<int> q;
-        
-        for(int i=0; i<n; i++) {
-            mp[fruits[i]]++;
-            q.push(fruits[i]);
-            if(mp.size()>2) {
-                while(mp.size()>2) {
-                    int remove = q.front();
-                    q.pop();
-                    mp[remove]--;
-                    cnt--;
-                    if(mp[remove] == 0) mp.erase(remove);
-                }
-            }
-            if(q.size()>maxi) maxi = q.size();
-        }
-        return maxi;
-    }
-}; 
+};

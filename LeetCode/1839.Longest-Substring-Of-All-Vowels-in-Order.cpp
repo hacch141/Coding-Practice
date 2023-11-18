7ad6cb342a82304a5a24bbdac17a3f6bf94f2660
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-11-18 22:26:12 +0530

    Update 1839.Longest-Substring-Of-All-Vowels-in-Order.cpp

--- LeetCode/1839.Longest-Substring-Of-All-Vowels-in-Order.cpp
+++ LeetCode/1839.Longest-Substring-Of-All-Vowels-in-Order.cpp
@@ -1,5 +1,40 @@
 // 1839. Longest Substring Of All Vowels in Order
 
+
+// without map
+class Solution {
+public:
+
+    int longestBeautifulSubstring(string word) {
+        int l = 0, r = 0, ans = 0, n = word.length(), len = 1, cnt=1;
+        
+        for(r=1; r<n; r++) {
+            if(word[r-1] == 'a') {
+                while (r < n && word[r] >= word[r-1]) {
+                    len++;
+                    if(word[r] > word[r-1]) {
+                        cnt++;
+                    }
+                    r++;
+                }
+                
+                if(cnt == 5) {
+                    ans = max(ans, len);
+                }
+
+                cnt = 1;
+                len = 1;                
+            }
+        }
+        return ans;
+    }
+};
+
+
+// ===================================================================================
+
+
+// using map
 class Solution {
 public:
 

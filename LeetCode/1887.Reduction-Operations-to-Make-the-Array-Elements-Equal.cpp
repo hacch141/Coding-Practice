c15a46c45146db2ebe2ec968d6f7262e4ce580a3
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-11-19 13:21:24 +0530

    Update 1887.Reduction-Operations-to-Make-the-Array-Elements-Equal.cpp

--- LeetCode/1887.Reduction-Operations-to-Make-the-Array-Elements-Equal.cpp
+++ LeetCode/1887.Reduction-Operations-to-Make-the-Array-Elements-Equal.cpp
@@ -1,5 +1,33 @@
 // 1887. Reduction Operations to Make the Array Elements Equal
 
+
+class Solution {
+public:
+    int reductionOperations(vector<int>& nums) {
+        int size = 50000, mini=INT_MAX, maxi=INT_MIN;
+        vector<int> freq(size+1,0);
+
+        for(auto i : nums) {
+            freq[i]++;
+            mini = min(mini,i);
+            maxi = max(maxi,i);
+        }
+
+        int ans = 0, cnt = 0;
+        for(int i=mini+1; i <= maxi; i++) {
+            if(freq[i] != 0) {
+                cnt++;
+                ans += (freq[i] * cnt);
+            }
+        }
+
+        return ans;
+    }
+};
+
+// ========================================================================
+
+
 class Solution {
 public:
     int reductionOperations(vector<int>& nums) {

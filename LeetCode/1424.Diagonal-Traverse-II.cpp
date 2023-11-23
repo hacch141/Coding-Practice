e61d062088f6250b05b38c706c5840fdf919e9e7
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-11-23 10:21:12 +0530

    Update 1424.Diagonal-Traverse-II.cpp

--- LeetCode/1424.Diagonal-Traverse-II.cpp
+++ LeetCode/1424.Diagonal-Traverse-II.cpp
@@ -3,25 +3,74 @@
 class Solution {
 public:
     vector<int> findDiagonalOrder(vector<vector<int>>& nums) {
-        vector<int> ans;
+        unordered_map<int,vector<int>> mp;
 
-        int row = nums.size();
-        int col = 0;
-
-        for(auto v : nums) {
-            col = max(col , (int)v.size());
-        }
- 
-        vector<vector<int>> mp(row+col);
-
-        for(int r=row-1; r>=0; r--) {
-            for(int c=0; c<nums[r].size(); c++) {
-                mp[r+c].push_back(nums[r][c]);
+        int maxi = 0;
+        int n = nums.size();
+        for(int i=n-1; i>=0; i--) {
+            for(int j=0; j<nums[i].size(); j++) {
+                mp[i+j].push_back(nums[i][j]);
+                maxi = max(maxi,i+j);
             }
         }
 
-        for(auto v : mp) {
-            ans.insert(ans.end(),v.begin(),v.end());
+        vector<int> ans;
+        for(int i=0; i<=maxi; i++) {
+            for(auto ele : mp[i]) {
+                ans.push_back(ele);
+            }
+        }
+
+        return ans;
+    }
+};
+
+
+class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& nums) {
+        int n = nums.size();
+        queue<pair<int,int>> q;
+        q.push({0,0});
+
+        vector<int> ans;
+        while(!q.empty()) {
+            int row = q.front().first;
+            int col = q.front().second;
+            ans.push_back(nums[row][col]);
+            q.pop();
+            if(col == 0) {
+                if(row+1 < n) q.push({row+1,col});
+                if(col+1 < nums[row].size())q.push({row,col+1});
+            }
+            else {
+                if(col+1 < nums[row].size())q.push({row,col+1});
+            }
+        }
+
+        return ans;
+    }
+};
+
+
+class Solution {
+public:
+    vector<int> findDiagonalOrder(vector<vector<int>>& nums) {
+        int n = nums.size();
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>> > pq;
+
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<nums[i].size(); j++) {
+                pq.push({i+j,j});
+            }
+        }
+
+        vector<int> ans;
+        while(!pq.empty()) {
+            int col = pq.top().second;
+            int row = pq.top().first - col;
+            pq.pop();
+            ans.push_back(nums[row][col]);
         }
 
         return ans;

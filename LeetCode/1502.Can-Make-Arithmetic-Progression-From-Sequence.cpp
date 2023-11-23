8a71e507356beefbcaa6eeec8a1cd92054d76922
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2023-11-23 11:26:25 +0530

    Create 1502.Can-Make-Arithmetic-Progression-From-Sequence.cpp

---
+++ LeetCode/1502.Can-Make-Arithmetic-Progression-From-Sequence.cpp
@@ -0,0 +1,13 @@
+// 1502. Can Make Arithmetic Progression From Sequence
+
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+        sort(arr.begin(),arr.end());
+        int d = arr[1] - arr[0];
+        for(int i=0; i<arr.size()-1; i++) {
+            if(arr[i+1] - arr[i] != d) return false;
+        }
+        return true;
+    }
+};

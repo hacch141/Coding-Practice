1d3b1ef607edf7faaa9561e96f724b4620268b68
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2024-01-05 19:58:48 +0530

    Update 0300.Longest-Increasing-Subsequence.cpp

--- LeetCode/0300.Longest-Increasing-Subsequence.cpp
+++ LeetCode/0300.Longest-Increasing-Subsequence.cpp
@@ -2,6 +2,31 @@
 
 class Solution {
 public:
+
+    int solve(int i, int& n, int prev, vector<int>& nums, vector<vector<int>>& dp) {
+        if(i == n) return 0;
+
+        if(dp[i+1][prev+1] != -1) return dp[i+1][prev+1];
+
+        int nottake = solve(i+1, n, prev, nums, dp);
+        int take = 0;
+        if(prev == -1 || nums[i] > nums[prev]) {
+            take = 1 + solve(i+1, n, i, nums, dp);
+        }
+
+        return dp[i+1][prev+1] = max(take,nottake);
+    }
+
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+        vector<vector<int>> dp(n+1, vector<int>(n+1, -1));
+        return solve(0, n, -1, nums, dp);
+    }
+};
+
+
+class Solution {
+public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
         vector<int> dp(n,1);

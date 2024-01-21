d13c205dd9fba91a485cdd88cc5d988c46a5b1a1
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2024-01-21 16:33:17 +0530

    Update 0198.House-Robber.cpp

--- LeetCode/0198.House-Robber.cpp
+++ LeetCode/0198.House-Robber.cpp
@@ -3,12 +3,12 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        int prev2 = 0;
-        int prev = nums[0];
-        for(int i=1; i<nums.size(); i++) {
-            int pick = nums[i] + prev2;
-            int notpick = 0 + prev;
-            int curr = max(pick,notpick);
+        int ans = 0, n = nums.size();
+        if(n == 1) return nums[0];
+        if(n == 2) return max(nums[0], nums[1]);
+        int prev2 = nums[0], prev = max(nums[1], nums[0]);
+        for(int i=2; i<n; i++) {
+            int curr = max(nums[i] + prev2, prev);
             prev2 = prev;
             prev = curr;
         }

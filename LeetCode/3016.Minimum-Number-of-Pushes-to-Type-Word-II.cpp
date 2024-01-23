7b0cebe0f82f290ddb2f524bf8fae871a33e86cc
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2024-01-23 18:55:47 +0530

    Create 3016.Minimum-Number-of-Pushes-to-Type-Word-II.cpp

---
+++ LeetCode/3016.Minimum-Number-of-Pushes-to-Type-Word-II.cpp
@@ -0,0 +1,17 @@
+// 3016. Minimum Number of Pushes to Type Word II
+
+class Solution {
+public:
+    int minimumPushes(string word) {
+        vector<int> freq(26, 0);
+        int ans = 0;
+        for(auto i : word) freq[i-'a']++;
+        sort(freq.rbegin(),freq.rend());
+        int p = 0;
+        for(int i=0; i<8; i++) ans += freq[i];
+        for(int i=8; i<16; i++) ans += 2*freq[i];
+        for(int i=16; i<24; i++) ans += 3*freq[i];
+        for(int i=24; i<26; i++) ans += 4*freq[i];
+        return ans;
+    }
+};

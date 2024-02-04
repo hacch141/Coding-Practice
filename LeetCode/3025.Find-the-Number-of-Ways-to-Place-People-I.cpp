6189b6ef9b0f7202fbf1526f85665be4bd4c4104
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2024-02-04 11:39:41 +0530

    Create 3025.Find-the-Number-of-Ways-to-Place-People-I.cpp

---
+++ LeetCode/3025.Find-the-Number-of-Ways-to-Place-People-I.cpp
@@ -0,0 +1,25 @@
+// 3025. Find the Number of Ways to Place People I
+
+class Solution {
+public:
+
+    static bool cmp(vector<int> a, vector<int> b) {
+        if(a[0] == b[0]) return a[1] >= b[1];
+        return a[0] < b[0];
+    }
+
+    int numberOfPairs(vector<vector<int>>& points) {
+        sort(points.begin(), points.end(), cmp);
+        int mn, ans = 0, n = points.size();
+        for(int i = 0; i < n; i++) {
+            mn = 51;
+            for(int j = i - 1; j >= 0; j--) {
+                if(points[j][1] >= points[i][1] && points[j][1] < mn) {
+                    ans++;
+                    mn = points[j][1];
+                }
+            }
+        }
+        return ans;
+    }
+};

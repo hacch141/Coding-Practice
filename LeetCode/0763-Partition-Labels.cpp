2fdbf5cf80cbd58804e24d93a4e7bb504f08bb21
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2024-05-02 19:12:33 +0530

    Update 0763-Partition-Labels.cpp

--- LeetCode/0763-Partition-Labels.cpp
+++ LeetCode/0763-Partition-Labels.cpp
@@ -22,3 +22,35 @@ public:
         return ans;
     }
 };
+
+
+class Solution {
+public:
+
+    int help(map<char,int>& mp, map<char,int>& mp2) {
+        int cnt = 0;
+        for(auto i : mp2) {
+            char ch = i.first;
+            int f = i.second;
+            if(mp[ch] != f) return -1;
+            cnt += f;
+        }
+        return cnt;
+    }
+
+    vector<int> partitionLabels(string s) {
+        map<char,int> mp, mp2;
+        for(auto ch : s) mp[ch]++;
+        int n = s.length();
+        vector<int> ans;
+        for(int i = 0; i < n; i++) {
+            mp2[s[i]]++;
+            int curr = help(mp, mp2);
+            if(curr != -1) {
+                ans.push_back(curr);
+                mp2.clear();
+            }
+        }
+        return ans;
+    }
+};

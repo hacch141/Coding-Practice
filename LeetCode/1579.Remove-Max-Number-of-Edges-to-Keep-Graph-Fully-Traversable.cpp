f30d4d221684d9d4d998d9c0903e502d00fb7c44
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2024-06-30 21:50:08 +0530

    Update 1579.Remove-Max-Number-of-Edges-to-Keep-Graph-Fully-Traversable.cpp

--- LeetCode/1579.Remove-Max-Number-of-Edges-to-Keep-Graph-Fully-Traversable.cpp
+++ LeetCode/1579.Remove-Max-Number-of-Edges-to-Keep-Graph-Fully-Traversable.cpp
@@ -1,74 +1,72 @@
 // 1579. Remove Max Number of Edges to Keep Graph Fully Traversable
 
-class DSU {
-    public:
-    vector<int> Parent, Rank;
-    DSU(int n) {
-        Parent.resize(n);
-        Rank.resize(n, 0);
-        for (int i = 0; i < n; i++) Parent[i] = i;
+// ======================================== DISJOINT SET ==============================================
+class DisjointSet {
+public:
+    vector<int> parent,rank;
+    DisjointSet(int n) {
+        parent.resize(n+1);
+        rank.resize(n+1,0);
+        for(int i=0; i<=n; i++) parent[i] = i;
     }
-    int Find(int x) {
-        return Parent[x] = Parent[x] == x ? x : Find(Parent[x]);
+    
+    int par(int node) {
+        return parent[node] = ((node == parent[node]) ? node : par(parent[node]));
     }
-    bool Union(int x, int y) {
-        int xset = Find(x), yset = Find(y);
-        if (xset != yset) {
-            Rank[xset] < Rank[yset] ? Parent[xset] = yset : Parent[yset] = xset;
-            Rank[xset] += Rank[xset] == Rank[yset];
-            return true;
+    
+    void add(int u, int v) {
+        int ulp_u = par(u), ulp_v = par(v);
+        if(ulp_u != ulp_v) {
+            if(rank[ulp_u] < rank[ulp_v]) {
+                parent[ulp_u] = ulp_v;
+                rank[ulp_v]++;
+            }
+            else {
+                parent[ulp_v] = ulp_u;
+                rank[ulp_u]++;
+            }
         }
-        return false;
+    }
+
+    void remove(int u) {
+        parent[u] = u;
     }
 };
+// ====================================================================================================
 
 class Solution {
 public:
     int maxNumEdgesToRemove(int n, vector<vector<int>>& edges) {
-        DSU dsu1(n+1);
-        DSU dsu2(n+1);
-        int ans = 0;
-        int edgecnt1 = 0;
-        int edgecnt2 = 0;
-
-        sort(edges.begin(), edges.end(), [&](auto const &a, auto const &b){
-            return a[0] > b[0];
-        });
-
-        for(auto edge : edges) {
-            if(edge[0]==3) {
-                if((dsu1.Find(edge[1]) == dsu1.Find(edge[2])) &&  dsu2.Find(edge[1]) == dsu2.Find(edge[2])) {
-                    ans++;
-                }
-                if(dsu1.Find(edge[1]) != dsu1.Find(edge[2])) {
-                    dsu1.Union(edge[1],edge[2]);
-                    edgecnt1++;
-                }
-                if(dsu2.Find(edge[1]) != dsu2.Find(edge[2])) {
-                    dsu2.Union(edge[1],edge[2]);
-                    edgecnt2++;
+        DisjointSet ds1(n), ds2(n);
+        sort(edges.rbegin(),edges.rend());
+        int edgsUsed = 0;
+        for(auto i : edges) {
+            int type = i[0], u = i[1], v = i[2];
+            if(type == 3) {
+                if(ds1.par(u) != ds1.par(v)) {
+                    ds1.add(u, v);
+                    ds2.add(u, v);
+                    edgsUsed++;
                 }
             }
-            else if(edge[0]==1) {
-                if(dsu1.Find(edge[1]) == dsu1.Find(edge[2])) {
-                    ans++;
-                }
-                else {
-                    dsu1.Union(edge[1],edge[2]);
-                    edgecnt1++;
+            else if(type == 1) {
+                if(ds1.par(u) != ds1.par(v)) {
+                    ds1.add(u, v);
+                    edgsUsed++;
                 }
             }
-            else if(edge[0]==2) {
-                if(dsu2.Find(edge[1]) == dsu2.Find(edge[2])) {
-                    ans++;
-                }
-                else {
-                    dsu2.Union(edge[1],edge[2]);
-                    edgecnt2++;
+            else if(type == 2) {
+                if(ds2.par(u) != ds2.par(v)) {
+                    ds2.add(u, v);
+                    edgsUsed++;
                 }
             }
         }
-        if(edgecnt1!=n-1 || edgecnt2!=n-1) return -1;
-        return ans;
+
+        for(int i = 1; i <= n; i++) {
+            if(ds1.par(1) != ds1.par(i) || ds2.par(1) != ds2.par(i)) return -1;
+        }
+        
+        return edges.size() - edgsUsed;
     }
 };

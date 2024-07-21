e05762e07a80178eac3224f332781505703b4169
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2024-07-21 15:23:21 +0530

    Create 0511-Game-Play-Analysis-I.sql

---
+++ LeetCode/SQL/01.Easy/0511-Game-Play-Analysis-I.sql
@@ -0,0 +1,6 @@
+-- 511. Game Play Analysis I
+-- Write a solution to find the first login date for each player.
+-- Return the result table in any order.
+SELECT player_id, MIN(event_date) as first_login
+FROM Activity
+GROUP BY player_id;

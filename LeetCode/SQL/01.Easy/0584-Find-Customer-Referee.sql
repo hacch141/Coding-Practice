c1f09df31b33304cd1010b913edb7082402511ec
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2024-07-21 15:00:15 +0530

    Create 0584-Find-Customer-Referee.sql

---
+++ LeetCode/SQL/01.Easy/0584-Find-Customer-Referee.sql
@@ -0,0 +1,9 @@
+-- 584. Find Customer Referee
+-- Find the names of the customer that are not referred by the customer with id = 2.
+SELECT name 
+FROM Customer
+WHERE referee_id IS NULL OR referee_id != 2;
+
+SELECT name 
+FROM Customer
+WHERE referee_id IS NULL OR referee_id <> 2;

53be47658d882ec278b892786bc7f36140bd9249
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2024-07-21 21:24:43 +0530

    Create 0596-Classes-More-Than-5-Students.sql

---
+++ LeetCode/SQL/01.Easy/0596-Classes-More-Than-5-Students.sql
@@ -0,0 +1,6 @@
+-- 596. Classes More Than 5 Students
+-- Write a solution to find all the classes that have at least five students.
+SELECT class
+FROM Courses
+GROUP BY class
+HAVING COUNT(student) >= 5;

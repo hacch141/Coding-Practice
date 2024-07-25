83f5c542668a4ee4a4ccd434c91e8a8ed4a0a0cb
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2024-07-26 01:24:38 +0530

    Create 1667-Fix-Names-in-a-Table.sql

---
+++ LeetCode/SQL/01.Easy/1667-Fix-Names-in-a-Table.sql
@@ -0,0 +1,6 @@
+-- 1667. Fix Names in a Table
+-- Write a solution to fix the names so that only the first character is uppercase and the rest are lowercase.
+-- Return the result table ordered by user_id.
+SELECT user_id, CONCAT(UPPER(SUBSTR(name, 1, 1)), LOWER(SUBSTR(name, 2)) ) as name
+FROM Users
+ORDER BY user_id

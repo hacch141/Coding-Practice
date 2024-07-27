2010357158de049a76d2c2c5742586ba5bbcecf4
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2024-07-27 19:52:44 +0530

    Update 1334.Find-the-City-With-the-Smallest-Number-of-Neighbors-at-a-Threshold-Distance.cpp

--- LeetCode/1334.Find-the-City-With-the-Smallest-Number-of-Neighbors-at-a-Threshold-Distance.cpp
+++ LeetCode/1334.Find-the-City-With-the-Smallest-Number-of-Neighbors-at-a-Threshold-Distance.cpp
@@ -2,48 +2,51 @@
 
 class Solution {
 public:
+
+    int bfs(int start, int& n, vector<vector<pair<int,int>>>& adj, int distanceThreshold) {
+
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        pq.push({0, start});
+
+        vector<int> dist(n, 1e9);
+        dist[start] = 0;
+
+        while(!pq.empty()) {
+            int du = pq.top().first;
+            int u = pq.top().second;
+            pq.pop();
+            for(auto it : adj[u]) {
+                int v = it.first;
+                int dv = it.second;
+                if(du + dv < dist[v] && du + dv <= distanceThreshold) {
+                    pq.push({du + dv, v});
+                    dist[v] = du + dv;
+                }
+            }
+        }
+
+        int cnt = 0;
+        for(int i = 0; i < n; i++) {
+            if(dist[i] != 1e9) cnt++;
+        }
+        return cnt - 1;
+    }
+
     int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
-        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>> > pq;
-        vector<pair<int,int>> adj[n];
-        for(auto it : edges) {
-            adj[it[0]].push_back({it[1],it[2]});
-            adj[it[1]].push_back({it[0],it[2]});
+        vector<vector<pair<int,int>>> adj(n);
+        for(auto i : edges) {
+            adj[i[0]].push_back({i[1], i[2]});
+            adj[i[1]].push_back({i[0], i[2]});
         }
-        vector<vector<int>> dist;
         
-        for(int i=0; i<n; i++) {
-            vector<int> dis(n,1e9);
-            dis[i] = 0;
-            pq.push({0,i});
-            while(!pq.empty()) {
-                int d = pq.top().first;
-                int u = pq.top().second;
-                pq.pop();
-                for(auto it : adj[u]) {
-                    int v = it.first;
-                    int weight = it.second;
-                    if(d+weight < dis[v]) {
-                        dis[v] = d + weight;
-                        pq.push({dis[v],v});
-                    }
-                }
-            }
-            dist.push_back(dis);
-        }
-        int cityCnt = n;
-        int cityNo = -1;
-        for(int u=0; u<n; u++) {
-            int cnt = 0;
-            for(int v=0; v<n; v++) {
-                if(dist[u][v] <= distanceThreshold) {
-                    cnt++;
-                }
-            }
-            if(cnt <= cityCnt) {
-                cityCnt = cnt;
-                cityNo = u;
+        pair<int,int> p = {1e9, 1e9};
+        for(int i = 0; i < n; i++) {
+            int nodes = bfs(i, n, adj, distanceThreshold);
+            if(nodes <= p.second) {
+                p = {i, nodes};
             }
         }
-        return cityNo;
+
+        return p.first;
     }
 };

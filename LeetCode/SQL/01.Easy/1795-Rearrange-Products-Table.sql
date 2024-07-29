93ac6fa88be3a5c1678fa94d4bdc923a5680b642
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2024-07-29 14:39:40 +0530

    Create 1795-Rearrange-Products-Table.sql

---
+++ LeetCode/SQL/01.Easy/1795-Rearrange-Products-Table.sql
@@ -0,0 +1,13 @@
+-- 1795. Rearrange Products Table
+-- Write a solution to rearrange the Products table so that each row has (product_id, store, price). If a product is not available in a store, do not include a row with that product_id and store combination in the result table.
+(SELECT product_id, 'store1' as store, store1 as price
+FROM Products
+WHERE store1 IS NOT NULL)
+UNION
+(SELECT product_id, 'store2' as store, store2 as price
+FROM Products
+WHERE store2 IS NOT NULL)
+UNION
+(SELECT product_id, 'store3' as store, store3 as price
+FROM Products
+WHERE store3 IS NOT NULL)

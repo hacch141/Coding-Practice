c8377797f13a27f68203960f399a05c995d61652
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2024-11-26 00:38:40 +0530

    Update 1652-Defuse-the-Bomb.cpp

--- LeetCode/1652-Defuse-the-Bomb.cpp
+++ LeetCode/1652-Defuse-the-Bomb.cpp
@@ -1,5 +1,37 @@
 // 1652. Defuse the Bomb
 
+
+class Solution {
+public:
+    vector<int> decrypt(vector<int>& code, int k) {
+        int n = code.size(), sum = 0;
+        vector<int> ans(n, 0);
+        if(k == 0) return ans;
+        for(int i = 1; i <= abs(k); i++) {
+            if(k < 0) {
+                sum += code[(-i + n) % n];
+            }
+            else {
+                sum += code[i];
+            }
+        }
+        for(int i = 0; i < n; i++) {
+            ans[i] = sum;
+            if(k < 0) {
+                sum += code[i];
+                sum -= code[(i + k + n) % n];
+            }
+            else {
+                sum -= code[(i + 1) % n];
+                sum += code[(i + k + 1) % n];
+            }
+        }
+        return ans;
+    }
+};
+
+
+
 class Solution {
 public:
     vector<int> decrypt(vector<int>& code, int k) {

755589d498d5701fea047f2be711d5956cb5c02a
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2024-11-27 00:14:51 +0530

    Update 3254-Find-the-Power-of-K-Size-Subarrays-I.cpp

--- LeetCode/3254-Find-the-Power-of-K-Size-Subarrays-I.cpp
+++ LeetCode/3254-Find-the-Power-of-K-Size-Subarrays-I.cpp
@@ -3,6 +3,27 @@
 class Solution {
 public:
     vector<int> resultsArray(vector<int>& nums, int k) {
+        if(k == 1) return nums;
+        int n = nums.size();
+        vector<int> ans(n - k + 1);
+        int len = 1;
+        for(int i = 1; i < n; i++) {
+            if(nums[i] == nums[i - 1] + 1) {
+                len++;
+            }
+            else {
+                len = 1;
+            }
+            if(i >= k - 1) ans[i - (k - 1)] = (len >= k) ? nums[i] : -1;
+        }
+        return ans;
+    }
+};
+
+
+class Solution {
+public:
+    vector<int> resultsArray(vector<int>& nums, int k) {
         int n = nums.size(), l = 0;
         vector<int> ans;
         for(int r = 0; r < n; r++) {

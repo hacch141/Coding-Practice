e480b2aeb5d78713cde1f21b24732b4b49d072e5
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2024-11-27 00:14:00 +0530

    Create 2924-Find-Champion-II.cpp

---
+++ LeetCode/2924-Find-Champion-II.cpp
@@ -0,0 +1,19 @@
+// 2924. Find Champion II
+
+class Solution {
+public:
+    int findChampion(int n, vector<vector<int>>& edges) {
+        vector<int> indegree(n, 0);
+        for(auto i : edges) {
+            indegree[i[1]]++;
+        }
+        int ans = -1;
+        for(int i = 0; i < n; i++) {
+            if(indegree[i] == 0) {
+                if(ans != - 1) return -1;
+                ans = i;
+            }
+        }
+        return ans;
+    }
+};

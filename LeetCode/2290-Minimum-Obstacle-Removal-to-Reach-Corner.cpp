c1c704155bf320341b6abb9d06d95cee25a9b441
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2024-11-28 23:11:51 +0530

    Update 2290-Minimum-Obstacle-Removal-to-Reach-Corner.cpp

--- LeetCode/2290-Minimum-Obstacle-Removal-to-Reach-Corner.cpp
+++ LeetCode/2290-Minimum-Obstacle-Removal-to-Reach-Corner.cpp
@@ -2,33 +2,30 @@
 
 class Solution {
 public:
-    void dijkstra(vector<vector<int>>& grid, vector<vector<int>>& dp, vector<vector<bool>>& vis) {
+    int dijkstra(vector<vector<int>>& grid, vector<vector<bool>>& vis) {
         int n = grid.size(), m = grid[0].size();
         priority_queue<pair<int, vector<int>>, vector<pair<int, vector<int>>>, greater<pair<int, vector<int>>>> pq;
         vis[0][0] = true;
-        dp[0][0] = grid[0][0];
         pq.push({grid[0][0], {0, 0}});
-        vector<int> dx = {-1, 0, +1, 0};
-        vector<int> dy = {0, +1, 0, -1};
+        vector<int> dx = {-1, 0, +1, 0}, dy = {0, +1, 0, -1};
         while(!pq.empty()) {
             int cnt_obs = pq.top().first, x = pq.top().second[0], y = pq.top().second[1];
+            if(x == n - 1 && y == m - 1) return cnt_obs;
             pq.pop();
             for(int i = 0; i < 4; i++) {
                 int nx = x + dx[i], ny = y + dy[i];
-                if(nx >= 0 && nx < n && ny >= 0 && ny < m && (!vis[nx][ny] || dp[x][y] + grid[nx][ny] < dp[nx][ny])) {
+                if(nx >= 0 && nx < n && ny >= 0 && ny < m && !vis[nx][ny]) {
                     vis[nx][ny] = true;
-                    dp[nx][ny] = dp[x][y] + grid[nx][ny];
-                    pq.push({dp[nx][ny], {nx, ny}});
+                    pq.push({cnt_obs + grid[nx][ny], {nx, ny}});
                 }
             }
         }
+        return -1;
     }
 
     int minimumObstacles(vector<vector<int>>& grid) {
         int n = grid.size(), m = grid[0].size();
-        vector<vector<int>> dp(n, vector<int>(m, 0));
         vector<vector<bool>> vis(n, vector<bool>(m, false));
-        dijkstra(grid, dp, vis);
-        return dp[n - 1][m - 1];
+        return dijkstra(grid, vis);
     }
 };

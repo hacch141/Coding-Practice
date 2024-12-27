7bdc4738928cbbdb2d3782ae02614e08d7a67fe9
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2024-12-27 13:26:15 +0530

    Create 1014-Best-Sightseeing-Pair.cpp

---
+++ LeetCode/1014-Best-Sightseeing-Pair.cpp
@@ -0,0 +1,16 @@
+// 1014. Best Sightseeing Pair
+
+class Solution {
+public:
+    int maxScoreSightseeingPair(vector<int>& values) {
+        int maxScore = 0;
+        int maxLeft = values[0] + 0;
+
+        for (int j = 1; j < values.size(); j++) {
+            maxScore = max(maxScore, maxLeft + values[j] - j);
+            maxLeft = max(maxLeft, values[j] + j);
+        }
+
+        return maxScore;
+    }
+};

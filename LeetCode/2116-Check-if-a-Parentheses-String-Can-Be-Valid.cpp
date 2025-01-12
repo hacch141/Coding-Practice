6080201f223b5af8ef42d0adc0bcc4f56f83af52
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-01-13 00:53:46 +0530

    Create 2116-Check-if-a-Parentheses-String-Can-Be-Valid.cpp

---
+++ LeetCode/2116-Check-if-a-Parentheses-String-Can-Be-Valid.cpp
@@ -0,0 +1,32 @@
+// 2116. Check if a Parentheses String Can Be Valid
+
+class Solution {
+public:
+    bool canBeValid(string s, string locked) {
+        int n = s.length();
+        if(n % 2 == 1) return false;
+        stack<int> open, openClose;
+        for(int i = 0; i < n; i++) {
+            if(locked[i] == '0') {
+                openClose.push(i);
+            }
+            else if(s[i] == '(') {
+                open.push(i);
+            }
+            else if(!open.empty()) {
+                open.pop();
+            }
+            else if(!openClose.empty()) {
+                openClose.pop();
+            }
+            else {
+                return false;
+            }
+        }
+        while(!open.empty() && !openClose.empty() && open.top() < openClose.top()) {
+            open.pop();
+            openClose.pop();
+        }
+        return open.empty();
+    }
+};

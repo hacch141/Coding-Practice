e5570cb086ce461b0f486d319cf7d155ecad34ae
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-01-21 01:20:41 +0530

    Update 2316.Count-Unreachable-Pairs-of-Nodes-in-an-Undirected-Graph.cpp

--- LeetCode/2316.Count-Unreachable-Pairs-of-Nodes-in-an-Undirected-Graph.cpp
+++ LeetCode/2316.Count-Unreachable-Pairs-of-Nodes-in-an-Undirected-Graph.cpp
@@ -1,36 +1,34 @@
 // 2316. Count Unreachable Pairs of Nodes in an Undirected Graph
 
+// DFS
 class Solution {
 public:
 
-    void dfs(int start, vector<int>& vis, vector<vector<int>>& adj, int& nodes) {
-        vis[start] = 1;
-        nodes++;
-        for(auto i : adj[start]) {
-            if(!vis[i]) {
-                dfs(i,vis,adj,nodes);
+    long long dfs(int u, vector<vector<int>> &adj, vector<bool> &vis) {
+        vis[u] = 1;
+        long long comp_nodes = 1;
+        for(auto &v : adj[u]) {
+            if(!vis[v]) {
+                comp_nodes += dfs(v, adj, vis);
             }
         }
+        return comp_nodes;
     }
 
     long long countPairs(int n, vector<vector<int>>& edges) {
         vector<vector<int>> adj(n);
-        for(auto i : edges) {
-            adj[i[0]].push_back(i[1]);
-            adj[i[1]].push_back(i[0]);
+        for(auto &it : edges) {
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
         }
 
-        vector<int> vis(n,0);
-        long long ans = 0;
-        int curr_total = 0;
-
-        for(int i=0; i<n; i++) {
-            if(!vis[i]) {
-                vis[i] = 1;
-                int nodes = 0;
-                dfs(i,vis,adj,nodes);
-                ans += (long long)nodes*curr_total;
-                curr_total += nodes;
+        long long ans = 0, total_nodes = 0;
+        vector<bool> vis(n, false);
+        for(int u = 0; u < n; u++) {
+            if(!vis[u]) {
+                int comp_nodes = dfs(u, adj, vis);
+                ans += comp_nodes * total_nodes;
+                total_nodes += comp_nodes;
             }
         }
 
@@ -38,55 +36,106 @@ public:
     }
 };
 
-
-
-
-// ======================================================================
-
-
+// BFS
 class Solution {
 public:
-
-    void bfs(int start, vector<int>& vis, vector<vector<int>>& adj, int& nodes) {
-        vis[start] = 1;
-        nodes++;
-        
+    long long bfs(int st, vector<vector<int>> &adj, vector<bool> &vis) {
         queue<int> q;
-        q.push(start);
+        q.push(st);
+        vis[st] = 1;
+        long long comp_nodes = 1;
         while(!q.empty()) {
-            int curr = q.front();
+            int u = q.front();
             q.pop();
-            for(auto i : adj[curr]) {
-                if(!vis[i]) {
-                    vis[i] = 1;
-                    nodes++;
-                    q.push(i);
+            for(auto &v : adj[u]) {
+                if(!vis[v]) {
+                    vis[v] = true;
+                    comp_nodes++;
+                    q.push(v);
                 }
             }
         }
+        return comp_nodes;
     }
 
     long long countPairs(int n, vector<vector<int>>& edges) {
         vector<vector<int>> adj(n);
-        for(auto i : edges) {
-            adj[i[0]].push_back(i[1]);
-            adj[i[1]].push_back(i[0]);
+        for(auto &it : edges) {
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
         }
 
-        vector<int> vis(n,0);
-        long long ans = 0;
-        int curr_total = 0;
-
-        for(int i=0; i<n; i++) {
-            if(!vis[i]) {
-                vis[i] = 1;
-                int nodes = 0;
-                bfs(i,vis,adj,nodes);
-                ans += (long long)nodes*curr_total;
-                curr_total += nodes;
+        long long ans = 0, total_nodes = 0;
+        vector<bool> vis(n, false);
+        for(int u = 0; u < n; u++) {
+            if(!vis[u]) {
+                int comp_nodes = bfs(u, adj, vis);
+                ans += comp_nodes * total_nodes;
+                total_nodes += comp_nodes;
             }
         }
 
         return ans;
+    }
+};
+
+// DSU
+class DisjointSet {
+private:
+    vector<int> parent, rank;
+public:
+    DisjointSet(int n) {
+        parent.resize(n + 1);
+        rank.resize(n + 1);
+        for(int i = 0; i <= n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    int find_par(int u) {
+        if(u == parent[u]) return u;
+        return parent[u] = find_par(parent[u]);
+    }
+
+    void union_by_rank(int u, int v) {
+        int par_u = find_par(u);
+        int par_v = find_par(v);
+        if(par_u == par_v) {
+            return;
+        }
+        else if(rank[par_u] > rank[par_v]) {
+            parent[par_v] = par_u;
+        }
+        else if(rank[par_u] < rank[par_v]) {
+            parent[par_u] = par_v;
+        }
+        else {
+            parent[par_v] = par_u;
+            rank[par_u]++;
+        }
+    }
+};
+
+class Solution {
+public:
+    long long countPairs(int n, vector<vector<int>>& edges) {
+        DisjointSet ds(n);
+        for(auto &it : edges) {
+            ds.union_by_rank(it[0], it[1]);
+        }
+
+        vector<int> comp_nodes(n, 0);
+        for(int u = 0; u < n; u++) {
+            comp_nodes[ds.find_par(u)]++;
+        }
+
+        long long total_pairs = 0;
+        int total_nodes = 0;
+        for(int u = 0; u < n; u++) {
+            total_pairs += 1LL * total_nodes * comp_nodes[u];
+            total_nodes += comp_nodes[u];
+        }
+
+        return total_pairs;
     }
 };

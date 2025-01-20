4b7c83d272b3350ebb06e6eb847f9770cdde54e5
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-01-21 00:47:41 +0530

    Update 1319.Number-of-Operations-to-Make-Network-Connected.cpp

--- LeetCode/1319.Number-of-Operations-to-Make-Network-Connected.cpp
+++ LeetCode/1319.Number-of-Operations-to-Make-Network-Connected.cpp
@@ -1,5 +1,49 @@
 // 1319. Number of Operations to Make Network Connected
 
+// BFS
+class Solution {
+public:
+
+    void bfs(int start, vector<int>& vis, vector<vector<int>>& adj) {
+        vis[start] = 1;
+        queue<int> q;
+        q.push(start);
+        while(!q.empty()) {
+            int curr = q.front();
+            q.pop();
+            for(auto i : adj[curr]) {
+                if(!vis[i]) {
+                    vis[i] = 1;
+                    q.push(i);
+                }
+            }
+        }
+    }
+
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        if(connections.size() < n-1) return -1;
+
+        vector<vector<int>> adj(n);
+        for(auto i : connections) {
+            adj[i[0]].push_back(i[1]);
+            adj[i[1]].push_back(i[0]);
+        }
+
+        vector<int> vis(n,0);
+        int cnt = 0;
+
+        for(int i=0; i<n; i++) {
+            if(!vis[i]) {
+                cnt++;
+                bfs(i,vis,adj);
+            }
+        }
+        
+        return cnt-1;
+    }
+};
+
+// DFS
 class Solution {
 public:
 
@@ -30,5 +74,67 @@ public:
 
         if(connections.size() < n-1) return -1;
         return cnt-1;
+    }
+};
+
+// DSU
+class DisjointSet {
+private:
+    vector<int> parent, rank;
+public:
+    DisjointSet(int n ) {
+        parent.resize(n + 1);
+        rank.resize(n + 1, 0);
+        for(int i = 0; i <= n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    int find_par(int u) {
+        if(u == parent[u]) return u;
+        return parent[u] = find_par(parent[u]);
+    }
+
+    void union_by_rank(int u, int v) {
+        int par_u = find_par(u);
+        int par_v = find_par(v);
+        if(par_u == par_v) {
+            return;
+        }
+        else if(rank[par_u] > rank[par_v]) {
+            parent[par_v] = par_u;
+        }
+        else if(rank[par_u] < rank[par_v]) {
+            parent[par_u] = par_v;
+        }
+        else {
+            parent[par_v] = par_u;
+            rank[par_u]++;
+        }
+    }
+};
+
+class Solution {
+public:
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        DisjointSet ds(n);
+        int extra_con = 0;
+        for(auto &con : connections) {
+            int u = con[0];
+            int v = con[1];
+            if(ds.find_par(u) == ds.find_par(v)) {
+                extra_con++;
+            }
+            else {
+                ds.union_by_rank(u, v);
+            }
+        }
+
+        set<int> st;
+        for(int u = 0; u < n; u++) {
+            st.insert(ds.find_par(u));
+        }
+
+        return extra_con >= st.size() - 1 ? st.size() - 1 : -1;
     }
 };

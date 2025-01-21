37de101b707ee2a16771e92e333f34dc5f7fd6d4
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-01-21 23:38:32 +0530

    Update 0743.Network-Delay-Time.cpp

--- LeetCode/0743.Network-Delay-Time.cpp
+++ LeetCode/0743.Network-Delay-Time.cpp
@@ -3,40 +3,35 @@
 class Solution {
 public:
     int networkDelayTime(vector<vector<int>>& times, int n, int k) {
-
-        vector<vector<pair<int,int>>> adj(n+1);
-        for(auto i : times) {
-            adj[i[0]].push_back({i[1],i[2]});
+        vector<vector<pair<int,int>>> adj(n + 1);
+        for(auto &it : times) {
+            adj[it[0]].push_back({it[1], it[2]});
         }
 
         priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
-        pq.push({0,k});
-        vector<int> time(n+1,1e9);
-        time[k] = 0;
+        pq.push({0, k});
+
+        vector<int> dist(n + 1, INT_MAX);
+        dist[k] = 0;
 
         while(!pq.empty()) {
-            int u = pq.top().second;
-            int ut = pq.top().first;
+            auto curr = pq.top();
             pq.pop();
-
+            int u = curr.second, w = curr.first;
             for(auto it : adj[u]) {
-                int v = it.first;
-                int vt = it.second;
-                if(ut + vt < time[v]) {
-                    time[v] = ut + vt;
-                    pq.push({time[v],v});
+                int v = it.first, nw = it.second;
+                if(w + nw < dist[v]) {
+                    dist[v] = w + nw;
+                    pq.push({dist[v], v});
                 }
             }
         }
 
-        int ans = 0;
-        for(int i=1; i<=n; i++) {
-            if(time[i] == 1e9) {
-                return -1;
-            }
-            ans = max(ans,time[i]);
+        int mn_time = 0;
+        for(int u = 1; u <= n; u++) {
+            mn_time = max(mn_time, dist[u]);
         }
 
-        return ans;
+        return mn_time != INT_MAX ? mn_time : -1;
     }
 };

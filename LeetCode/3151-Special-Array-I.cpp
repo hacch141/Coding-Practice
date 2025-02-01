fc00a13ccbca71f3aacf41fbb7f2cb1ebbd609f7
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-02-01 16:09:51 +0530

    Create 3151-Special-Array-I.cpp

---
+++ LeetCode/3151-Special-Array-I.cpp
@@ -0,0 +1,16 @@
+// 3151. Special Array I
+
+class Solution {
+public:
+    bool isArraySpecial(vector<int>& nums) {
+        int n = nums.size();
+        bool f = true;
+        for(int i = 0; i < n - 1; i++) {
+            if((nums[i] & 1) == (nums[i + 1] & 1)) {
+                f = false;
+                break;
+            }
+        }
+        return f;
+    }
+};

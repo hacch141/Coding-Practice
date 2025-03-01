db29e318048f6ce9e63f93e7d484ae6d8a8fa72c
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-03-02 02:14:56 +0530

    Update 0841-Keys-and-Rooms.cpp

--- LeetCode/0841-Keys-and-Rooms.cpp
+++ LeetCode/0841-Keys-and-Rooms.cpp
@@ -28,3 +28,30 @@ public:
         return f;
     }
 };
+
+// ==============================================================
+
+class Solution {
+public:
+    void dfs(int u, vector<vector<int>>& rooms, vector<bool>& vis) {
+        vis[u] = true;
+        for(auto &v : rooms[u]) {
+            if(!vis[v]) dfs(v, rooms, vis);
+        }
+    }
+
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        int n = rooms.size();
+
+        vector<bool> vis(n, false);
+        vis[0] = true;
+        for(auto &i : rooms[0]) {
+            dfs(i, rooms, vis);
+        }
+
+        bool f = true;
+        for(auto i : vis) f &= i;
+
+        return f;
+    }
+};

f27f6af1d4db05008606223b8110d9ea03705926
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-03-02 20:07:58 +0530

    Create 1061-Lexicographically-Smallest-Equivalent-String.cpp

---
+++ LeetCode/1061-Lexicographically-Smallest-Equivalent-String.cpp
@@ -0,0 +1,43 @@
+// 1061. Lexicographically Smallest Equivalent String
+
+class Solution {
+public:
+    void dfs(int u, vector<vector<int>>& adj, vector<bool>& vis, vector<int>& cmp, int& mn) {
+        vis[u] = true;
+        cmp.push_back(u);
+        mn = min(mn, u);
+        for(auto &v : adj[u]) {
+            if(!vis[v]) {
+                dfs(v, adj, vis, cmp, mn);
+            }
+        }
+    }
+
+    string smallestEquivalentString(string s1, string s2, string baseStr) {
+        vector<vector<int>> adj(26);
+        int n = s1.length();
+        for(int i = 0; i < n; i++) {
+            adj[s1[i] - 'a'].push_back(s2[i] - 'a');
+            adj[s2[i] - 'a'].push_back(s1[i] - 'a');
+        }
+
+        vector<int> lex(26);
+        for(int i = 0; i < 26; i++) lex[i] = i;
+
+        vector<bool> vis(26, false);
+        for(int i = 0; i < 26; i++) {
+            int mn = i;
+            vector<int> cmp;
+            if(!vis[i]) dfs(i, adj, vis, cmp, mn);
+            for(auto &i : cmp) lex[i] = mn;
+        }
+
+        string ans;
+        for(auto &ch : baseStr) {
+            if(lex[ch - 'a'] == -1) ans += ch;
+            else ans += (lex[ch - 'a'] + 'a');
+        }
+
+        return ans;
+    }
+};

c479cdce38464e5f0b146ff3dd34bb2ec35253cd
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-03-03 02:05:49 +0530

    Update 0834-Sum-of-Distances-in-Tree.cpp

--- LeetCode/0834-Sum-of-Distances-in-Tree.cpp
+++ LeetCode/0834-Sum-of-Distances-in-Tree.cpp
@@ -2,36 +2,34 @@
 
 class Solution {
 public:
-
-    int sum;
-
-    void dfs(int u, int p, vector<vector<int>>& tree, vector<int>& cnt, vector<int>& ans) {
-        for(auto v : tree[u]) {
-            if(v == p) continue;
-            dfs(v, u, tree, cnt, ans);
+    void dfs(int u, int par, vector<vector<int>>& adj, vector<int>& cnt, vector<int>& ans) {
+        for(auto &v : adj[u]) {
+            if(v == par) continue;
+            dfs(v, u, adj, cnt, ans);
             cnt[u] += cnt[v];
-            ans[u] += ans[v] + cnt[v];
+            ans[u] += cnt[v] + ans[v];
         }
     }
 
-    void dfs1(int u, int p, vector<vector<int>>& tree, vector<int>& cnt, vector<int>& ans, int& n) {
-        for(auto v : tree[u]) {
-            if(v == p) continue;
+    void dfs2(int u, int par, vector<vector<int>>& adj, vector<int>& cnt, vector<int>& ans, int& n) {
+        for(auto &v : adj[u]) {
+            if(v == par) continue;
             ans[v] = ans[u] - cnt[v] + (n - cnt[v]);
-            dfs1(v, u, tree, cnt, ans, n);
+            dfs2(v, u, adj, cnt, ans, n);
         }
     }
 
     vector<int> sumOfDistancesInTree(int n, vector<vector<int>>& edges) {
-        vector<vector<int>> tree(n);
-        for(auto i : edges) {
-            tree[i[0]].push_back(i[1]);
-            tree[i[1]].push_back(i[0]);
+        vector<vector<int>> adj(n);
+        for(auto &it : edges) {
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
         }
 
         vector<int> cnt(n, 1), ans(n, 0);
-        dfs(0, 0, tree, cnt, ans);
-        dfs1(0, 0, tree, cnt, ans, n);
+        dfs(0, -1, adj, cnt, ans);
+        dfs2(0, 0, adj, cnt, ans, n);
+
         return ans;
     }
 };

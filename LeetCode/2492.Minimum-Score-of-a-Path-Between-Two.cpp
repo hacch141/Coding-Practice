c3c88f92550cf071abc6cda5b17a73e2bf9bbdb5
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-03-04 23:00:15 +0530

    Update 2492.Minimum-Score-of-a-Path-Between-Two.cpp

--- LeetCode/2492.Minimum-Score-of-a-Path-Between-Two.cpp
+++ LeetCode/2492.Minimum-Score-of-a-Path-Between-Two.cpp
@@ -2,37 +2,60 @@
 
 class Solution {
 public:
-
-    void dfs(int start, vector<int>& vis, vector<vector<pair<int,int>>>& adj, vector<int>& ans) {
-        vis[start] = 1;
-        for(auto it : adj[start]) {
-            if(!vis[it.first]) {
-                ans.push_back(it.first);
-                dfs(it.first,vis,adj,ans);
-            }
+    void dfs(int u, vector<vector<pair<int,int>>>& adj, vector<bool>& vis, int& ans) {
+        vis[u] = true;
+        for(auto &it : adj[u]) {
+            int v = it.first;
+            ans = min(ans, it.second);
+            if(!vis[v]) dfs(v, adj, vis, ans);
         }
     }
 
     int minScore(int n, vector<vector<int>>& roads) {
-        vector<vector<pair<int,int>>> adj(n);
-        for(auto it : roads) {
-            int u = it[0];
-            int v = it[1];
-            int wt = it[2];
-            adj[u-1].push_back({v-1,wt});
-            adj[v-1].push_back({u-1,wt});
+        vector<vector<pair<int,int>>> adj(n + 1);
+        for(auto &it : roads) {
+            adj[it[0]].push_back({it[1], it[2]});
+            adj[it[1]].push_back({it[0], it[2]});
         }
 
-        vector<int> vis(n,0);
-        vector<int>ans;
-        dfs(0,vis,adj,ans);
+        int ans = INT_MAX;
+        vector<bool> vis(n + 1, false);
+        dfs(1, adj, vis, ans);
 
-        int res = INT_MAX;
-        for(int i=0 ; i<ans.size() ; i++){
-            for(auto it : adj[ans[i]]){
-                res = min(res,it.second);
+        return ans;
+    }
+};
+
+// =====================================================================
+
+class Solution {
+public:
+    int minScore(int n, vector<vector<int>>& roads) {
+        vector<vector<pair<int,int>>> adj(n + 1);
+        for(auto &it : roads) {
+            adj[it[0]].push_back({it[1], it[2]});
+            adj[it[1]].push_back({it[0], it[2]});
+        }
+
+        int ans = INT_MAX;
+        vector<bool> vis(n + 1, false);
+        queue<int> q;
+        q.push(1);
+
+        while(!q.empty()) {
+            int sz = q.size();
+            while(sz--) {
+                int u = q.front();
+                q.pop();
+                vis[u] = true;
+                for(auto &it : adj[u]) {
+                    int v = it.first;
+                    ans = min(ans, it.second);
+                    if(!vis[v]) q.push(v);
+                }
             }
         }
-        return res;
+
+        return ans;
     }
 };

815a71ec0027a432c2302b96e1ea0d91919c5639
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-03-04 23:16:04 +0530

    Update 1466.Reorder-Routes-to-Make-All-Paths-Lead-to-the-City-Zero.cpp

--- LeetCode/1466.Reorder-Routes-to-Make-All-Paths-Lead-to-the-City-Zero.cpp
+++ LeetCode/1466.Reorder-Routes-to-Make-All-Paths-Lead-to-the-City-Zero.cpp
@@ -1,8 +1,76 @@
 // 1466. Reorder Routes to Make All Paths Lead to the City Zero
 
+// dfs
 class Solution {
 public:
+    void dfs(int u, vector<vector<int>>& adj, vector<set<int>>& can_go, vector<bool>& vis, int& cnt) {
+        vis[u] = true;
+        for(auto &v : adj[u]) {
+            if(!vis[v]) {
+                if(can_go[u].count(v)) cnt++;
+                dfs(v, adj, can_go, vis, cnt);
+            }
+        }
+    }
 
+    int minReorder(int n, vector<vector<int>>& connections) {
+        vector<vector<int>> adj(n);
+        vector<set<int>> can_go(n);
+        for(auto &it : connections) {
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
+            can_go[it[0]].insert(it[1]);
+        }
+
+        int cnt = 0;
+        vector<bool> vis(n, false);
+
+        dfs(0, adj, can_go, vis, cnt);
+
+        return cnt;
+    }
+};
+
+// bfs
+class Solution {
+public:
+    int minReorder(int n, vector<vector<int>>& connections) {
+        vector<vector<int>> adj(n);
+        vector<set<int>> can_go(n);
+        for(auto &it : connections) {
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
+            can_go[it[0]].insert(it[1]);
+        }
+
+        queue<int> q;
+        q.push(0);
+        int cnt = 0;
+        vector<bool> vis(n, false);
+
+        while(!q.empty()) {
+            int sz = q.size();
+            while(sz--) {
+                int u = q.front();
+                q.pop();
+                vis[u] = true;
+                for(auto &v : adj[u]) {
+                    if(!vis[v]) {
+                        if(can_go[u].count(v)) cnt++;
+                        q.push(v);
+                    }
+                }
+            }
+        }
+
+        return cnt;
+    }
+};
+
+//================================================================================
+
+class Solution {
+public:
     void bfs(int& cnt, vector<vector<int>>& adj, int& n) {
         queue<int> q;
         q.push(0);
@@ -33,51 +101,6 @@ public:
 
         int cnt = 0;
         bfs(cnt,adj,n);
-        return cnt;
-    }
-};
-
-
-// =================================================================================================
-
-
-
-class Solution {
-public:
-
-    void bfs(int& cnt, unordered_map<string,int>& mp, vector<vector<int>>& adj, int& n) {
-        queue<int> q;
-        q.push(0);
-        vector<int> vis(n,0);
-        vis[0] = 1;
-
-        while(!q.empty()) {
-            int curr = q.front();
-            q.pop();
-            for(auto i : adj[curr]) {
-                if(!vis[i]) {
-                    vis[i] = 1;
-                    if(mp.find(to_string(curr)+"#"+to_string(i)) != mp.end()) {
-                        cnt++;
-                    }
-                    q.push(i);
-                }
-            }
-        }
-    }
-
-    int minReorder(int n, vector<vector<int>>& connections) {
-        unordered_map<string,int> mp;
-        vector<vector<int>> adj(n);
-
-        for(auto i : connections) {
-            mp[to_string(i[0])+"#"+to_string(i[1])]++;
-            adj[i[0]].push_back(i[1]);
-            adj[i[1]].push_back(i[0]);
-        }
-
-        int cnt = 0;
-        bfs(cnt,mp,adj,n);
         return cnt;
     }
 };

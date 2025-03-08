ffd71aa8f839e130119905edc83eb1aa6c60c1ed
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-03-08 17:16:35 +0530

    Update 1514-Path-with-Maximum-Probability.cpp

--- LeetCode/1514-Path-with-Maximum-Probability.cpp
+++ LeetCode/1514-Path-with-Maximum-Probability.cpp
@@ -3,32 +3,28 @@
 class Solution {
 public:
     double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
-
-        int len = edges.size();
         vector<vector<pair<int,double>>> adj(n);
-        for(int i = 0; i < len; i++) {
-            int u = edges[i][0], v = edges[i][1];
-            adj[u].push_back({v, succProb[i]});
-            adj[v].push_back({u, succProb[i]});
+        for(int i = 0; i < edges.size(); i++) {
+            adj[edges[i][0]].push_back({edges[i][1], succProb[i]});
+            adj[edges[i][1]].push_back({edges[i][0], succProb[i]});
         }
 
         vector<double> p(n, 0);
+        priority_queue<pair<double, int>> pq;
+        pq.push({1, start_node});
         p[start_node] = 1;
 
-        priority_queue<pair<double,int>> pq;
-        pq.push({1, start_node});
-
         while(!pq.empty()) {
-            int u = pq.top().second;
-            double pu = pq.top().first;
-            if(u == end_node) return pu;
+            auto curr = pq.top();
             pq.pop();
-            for(auto it : adj[u]) {
+            int u = curr.second;
+            double prob = curr.first;
+            for(auto &it : adj[u]) {
                 int v = it.first;
-                double pv = it.second;
-                if(pu * pv > p[v]) {
-                    p[v] = pu * pv;
-                    pq.push({p[v], v});
+                double new_prob = prob * it.second;
+                if(new_prob > p[v]) {
+                    p[v] = new_prob;
+                    pq.push({new_prob, v});
                 }
             }
         }

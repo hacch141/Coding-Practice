5f75e2a7d120f187e9716b96278ca72a5cd8c9a0
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-03-08 15:46:59 +0530

    Update 1557.Minimum-Number-of-Vertices-to-Reach-All-Nodes.cpp

--- LeetCode/1557.Minimum-Number-of-Vertices-to-Reach-All-Nodes.cpp
+++ LeetCode/1557.Minimum-Number-of-Vertices-to-Reach-All-Nodes.cpp
@@ -3,16 +3,16 @@
 class Solution {
 public:
     vector<int> findSmallestSetOfVertices(int n, vector<vector<int>>& edges) {
-        vector<int> v(n,0);
-        for(auto i : edges) {
-            v[i[1]]++;
+        vector<int> indegree(n, 0);
+        for(auto &it : edges) {
+            indegree[it[1]]++;
         }
+
         vector<int> ans;
-        for(int i=0; i<n; i++) {
-            if(v[i] == 0) {
-                ans.push_back(i);
-            }
+        for(int i = 0; i < n; i++) {
+            if(indegree[i] == 0) ans.push_back(i);
         }
+
         return ans;
     }
 };

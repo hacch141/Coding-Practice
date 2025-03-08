756b8abcfdd39bc7e458b5d947a13c7ea4ec658a
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-03-08 15:35:56 +0530

    Update 1579.Remove-Max-Number-of-Edges-to-Keep-Graph-Fully-Traversable.cpp

--- LeetCode/1579.Remove-Max-Number-of-Edges-to-Keep-Graph-Fully-Traversable.cpp
+++ LeetCode/1579.Remove-Max-Number-of-Edges-to-Keep-Graph-Fully-Traversable.cpp
@@ -1,72 +1,76 @@
 // 1579. Remove Max Number of Edges to Keep Graph Fully Traversable
 
-// ======================================== DISJOINT SET ==============================================
 class DisjointSet {
+private:
+    vector<int> parent, sz;
 public:
-    vector<int> parent,rank;
-    DisjointSet(int n) {
-        parent.resize(n+1);
-        rank.resize(n+1,0);
-        for(int i=0; i<=n; i++) parent[i] = i;
-    }
-    
-    int par(int node) {
-        return parent[node] = ((node == parent[node]) ? node : par(parent[node]));
-    }
-    
-    void add(int u, int v) {
-        int ulp_u = par(u), ulp_v = par(v);
-        if(ulp_u != ulp_v) {
-            if(rank[ulp_u] < rank[ulp_v]) {
-                parent[ulp_u] = ulp_v;
-                rank[ulp_v]++;
-            }
-            else {
-                parent[ulp_v] = ulp_u;
-                rank[ulp_u]++;
-            }
-        }
+    DisjointSet(int n ) {
+        parent.resize(n + 1);
+        sz.resize(n + 1, 1);
+        for(int i = 0; i <= n; i++) parent[i] = i;
     }
 
-    void remove(int u) {
-        parent[u] = u;
+    int find_par(int u) {
+        if(parent[u] == u) return u;
+        return parent[u] = find_par(parent[u]);
+    }
+
+    void union_by_size(int u, int v) {
+        int par_u = find_par(u), par_v = find_par(v);
+        if(par_u == par_v) {
+            return;
+        }
+        else if(sz[par_u] > sz[par_v]) {
+            sz[par_u] += sz[par_v];
+            parent[par_v] = par_u;
+        }
+        else {
+            sz[par_v] += sz[par_u];
+            parent[par_u] = par_v;
+        }
     }
 };
-// ====================================================================================================
 
 class Solution {
 public:
     int maxNumEdgesToRemove(int n, vector<vector<int>>& edges) {
+        sort(edges.rbegin(), edges.rend());
+
+        int cnt = 0;
         DisjointSet ds1(n), ds2(n);
-        sort(edges.rbegin(),edges.rend());
-        int edgsUsed = 0;
-        for(auto i : edges) {
-            int type = i[0], u = i[1], v = i[2];
+        for(auto &it : edges) {
+            int type = it[0], u = it[1], v = it[2];
             if(type == 3) {
-                if(ds1.par(u) != ds1.par(v)) {
-                    ds1.add(u, v);
-                    ds2.add(u, v);
-                    edgsUsed++;
+                if(ds1.find_par(u) != ds1.find_par(v)) {
+                    ds1.union_by_size(u, v);
+                    ds2.union_by_size(u, v);
+                }
+                else {
+                    cnt++;
                 }
             }
             else if(type == 1) {
-                if(ds1.par(u) != ds1.par(v)) {
-                    ds1.add(u, v);
-                    edgsUsed++;
+                if(ds1.find_par(u) != ds1.find_par(v)) {
+                    ds1.union_by_size(u, v);
+                }
+                else {
+                    cnt++;
                 }
             }
             else if(type == 2) {
-                if(ds2.par(u) != ds2.par(v)) {
-                    ds2.add(u, v);
-                    edgsUsed++;
+                if(ds2.find_par(u) != ds2.find_par(v)) {
+                    ds2.union_by_size(u, v);
+                }
+                else {
+                    cnt++;
                 }
             }
         }
 
         for(int i = 1; i <= n; i++) {
-            if(ds1.par(1) != ds1.par(i) || ds2.par(1) != ds2.par(i)) return -1;
+            if(ds1.find_par(i) != ds1.find_par(1) || ds2.find_par(i) != ds2.find_par(1)) return -1;
         }
-        
-        return edges.size() - edgsUsed;
+
+        return cnt;
     }
 };

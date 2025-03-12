870fc1a7cd0d89abdeb434c6775853a33db77158
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-03-13 01:26:19 +0530

    Update 2529-Maximum-Count-of-Positive-Integer-and-Negative-Integer.cpp

--- LeetCode/2529-Maximum-Count-of-Positive-Integer-and-Negative-Integer.cpp
+++ LeetCode/2529-Maximum-Count-of-Positive-Integer-and-Negative-Integer.cpp
@@ -11,3 +11,65 @@ public:
         return max(pos, neg);
     }
 };
+
+// ======================================
+
+class Solution {
+public:
+    int find_neg(vector<int>& nums) {
+        int n = nums.size();
+        int left = 0, right = n - 1, ans = -1;
+        while(left <= right) {
+            int mid = (right - left) / 2 + left;
+            if(nums[mid] >= 0) {
+                right = mid - 1;
+            }
+            else {
+                ans = mid;
+                left = mid + 1;
+            }
+        }
+        return ans + 1;
+    }
+
+    int find_pos(vector<int>& nums) {
+        int n = nums.size();
+        int left = 0, right = n - 1, ans = n;
+        while(left <= right) {
+            int mid = (right - left) / 2 + left;
+            if(nums[mid] <= 0) {
+                left = mid + 1;
+            }
+            else {
+                ans = mid;
+                right = mid - 1;
+            }
+        }
+        return n - ans;
+    }
+
+    int maximumCount(vector<int>& nums) {
+        int neg = find_neg(nums), pos = find_pos(nums);
+        return max(neg, pos);
+    }
+};
+
+// =====================
+
+class Solution {
+public:
+    int find_neg(vector<int>& nums) {
+        int ind = upper_bound(nums.begin(), nums.end(), -1) - nums.begin();
+        return ind;
+    }
+
+    int find_pos(vector<int>& nums) {
+        int ind = upper_bound(nums.begin(), nums.end(), 0) - nums.begin();
+        return nums.size() - ind;
+    }
+
+    int maximumCount(vector<int>& nums) {
+        int neg = find_neg(nums), pos = find_pos(nums);
+        return max(neg, pos);
+    }
+};

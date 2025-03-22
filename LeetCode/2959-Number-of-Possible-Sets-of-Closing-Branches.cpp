fd1b1df8278a3c6e781cb8826a2015a4b213930b
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-03-23 01:39:49 +0530

    Update 2959-Number-of-Possible-Sets-of-Closing-Branches.cpp

--- LeetCode/2959-Number-of-Possible-Sets-of-Closing-Branches.cpp
+++ LeetCode/2959-Number-of-Possible-Sets-of-Closing-Branches.cpp
@@ -3,33 +3,28 @@
 class Solution {
 public:
     bool is_ok(int mask, int n, int maxDistance, vector<vector<int>>& roads) {
-        vector<int> v;
-        // choose only those nodes whose bits are set and go via only through them 
-        for(int i = 0; i < n; i++) {
-            if((mask >> i) & 1) v.push_back(i);
-        }
-
         vector<vector<int>> adj(n, vector<int>(n, 1e9));
         for(int i = 0; i < n; i++) adj[i][i] = 0;
 
         for(auto &it : roads) {
+            if(!((mask >> it[0]) & 1) || !((mask >> it[1]) & 1)) continue;
             adj[it[0]][it[1]] = min(adj[it[0]][it[1]],it[2]);
             adj[it[1]][it[0]] = min(adj[it[1]][it[0]],it[2]);
         }
 
         // Floyd Warshall to find shortest distance from any node to any other node
-        for(int via = 0; via < v.size(); via++) {
+        for(int via = 0; via < n; via++) {
             for(int i = 0; i < n; i++) {
                 for(int j = 0; j < n; j++) {
-                    int via_val = v[via];
-                    adj[i][j] = min(adj[i][j], adj[i][via_val] + adj[via_val][j]);
+                    adj[i][j] = min(adj[i][j], adj[i][via] + adj[via][j]);
                 }
             }
         }
 
-        for(int i = 0; i < v.size(); i++) {
-            for(int j = i + 1; j < v.size(); j++) {
-                if(adj[v[i]][v[j]] > maxDistance) return false;
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+                if(!((mask >> i) & 1) || !((mask >> j) & 1)) continue;
+                if(adj[i][j] > maxDistance) return false;
             }
         }
 

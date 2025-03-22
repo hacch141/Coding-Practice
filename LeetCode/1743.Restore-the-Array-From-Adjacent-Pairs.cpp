812d52b2ff7ea625d191eee0e1f4cbe2eb368ee1
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-03-22 21:58:41 +0530

    Update 1743.Restore-the-Array-From-Adjacent-Pairs.cpp

--- LeetCode/1743.Restore-the-Array-From-Adjacent-Pairs.cpp
+++ LeetCode/1743.Restore-the-Array-From-Adjacent-Pairs.cpp
@@ -3,44 +3,37 @@
 class Solution {
 public:
     vector<int> restoreArray(vector<vector<int>>& adjacentPairs) {
-        unordered_map<int, vector<int>> mp;
-        for (auto i : adjacentPairs) {
-            mp[i[0]].push_back(i[1]);
-            mp[i[1]].push_back(i[0]);
+        int n = adjacentPairs.size() + 1;
+        map<int,vector<int>> adj;
+        for(auto &it : adjacentPairs) {
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
         }
 
-        int first, second;
+        vector<int> ans;
         set<int> vis;
-        
-        for (auto i : mp) {
-            if (i.second.size() == 1) {
-                first = i.first;
-                second = i.second[0];
+        for(auto &it : adjacentPairs) {
+            int u = it[0], v = it[1];
+            if(adj[u].size() == 1) {
+                ans.push_back(u);
+                vis.insert(u);
+                break;
+            }
+            if(adj[v].size() == 1) {
+                ans.push_back(v);
+                vis.insert(v);
                 break;
             }
         }
 
-        vector<int> ans;
-        ans.push_back(first);
-        ans.push_back(second); 
-        vis.insert(first);
-        vis.insert(second);
-
-        while (ans.size() < adjacentPairs.size()+1) {
-            vector<int> v = mp[second];
-            int next;
-
-            // Find the next element.
-            for (int neighbor : v) {
-                if (vis.find(neighbor) == vis.end()) {
-                    next = neighbor;
-                    break;
+        while(ans.size() < n) {
+            int u = ans.back();
+            for(auto &v : adj[u]) {
+                if(!vis.count(v)) {
+                    vis.insert(v);
+                    ans.push_back(v);
                 }
             }
-
-            ans.push_back(next);
-            vis.insert(next);
-            second = next;
         }
 
         return ans;

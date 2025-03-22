35e22c63394b25df68066e05e28f38f1da41d76c
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-03-23 02:33:39 +0530

    Update 3015.Count-the-Number-of-Houses-at-a-Certain-Distance-I.cpp

--- LeetCode/3015.Count-the-Number-of-Houses-at-a-Certain-Distance-I.cpp
+++ LeetCode/3015.Count-the-Number-of-Houses-at-a-Certain-Distance-I.cpp
@@ -46,3 +46,39 @@ public:
         return ans;
     }
 };
+
+// ================================================================
+
+class Solution {
+public:
+    vector<int> countOfPairs(int n, int x, int y) {
+        vector<vector<int>> adj(n, vector<int>(n, 1e9));
+        for(int i = 0; i < n - 1; i++) {
+            adj[i][i + 1] = 1;
+            adj[i + 1][i] = 1;
+        }
+
+        for(int i = 0; i < n; i++) adj[i][i] = 0;
+
+        adj[x - 1][y - 1] = 1;
+        adj[y - 1][x - 1] = 1;
+
+        for(int via = 0; via < n; via++) {
+            for(int i = 0; i < n; i++) {
+                for(int j = 0; j < n; j++) {
+                    adj[i][j] = min(adj[i][j], adj[i][via] + adj[via][j]);
+                }
+            }
+        }
+
+        vector<int> ans(n, 0);
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i == j || adj[i][j] == 1e9) continue;
+                ans[adj[i][j] - 1]++;
+            }
+        }
+
+        return ans;
+    }
+};

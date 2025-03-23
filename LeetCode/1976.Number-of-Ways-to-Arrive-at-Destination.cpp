e4281a7a157019ca4861c8919b5edf1206584644
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-03-23 16:49:05 +0530

    Update 1976.Number-of-Ways-to-Arrive-at-Destination.cpp

--- LeetCode/1976.Number-of-Ways-to-Arrive-at-Destination.cpp
+++ LeetCode/1976.Number-of-Ways-to-Arrive-at-Destination.cpp
@@ -3,40 +3,41 @@
 class Solution {
 public:
     int countPaths(int n, vector<vector<int>>& roads) {
-        vector<pair<long long,long long>> adj[n];
-        for(auto it : roads) {
-            adj[it[0]].push_back({it[1],it[2]});
-            adj[it[1]].push_back({it[0],it[2]});
+        vector<vector<pair<int,int>>> adj(n);
+        for(auto &it : roads) {
+            adj[it[0]].push_back({it[1], it[2]});
+            adj[it[1]].push_back({it[0], it[2]});
         }
-        
-        priority_queue< pair<long long,long long>, vector< pair<long long,long long> >, greater< pair<long long,long long> > > pq;
-        pq.push({0,0});
-        vector<long long> dist(n,LONG_MAX);
-        vector<long long> ways(n, 0);
+
+        priority_queue<pair<long long,int>, vector<pair<long long,int>>, greater<pair<long long,int>>> pq;
+        pq.push({0, 0});
+
+        vector<long long> dist(n, LLONG_MAX), cnt(n, 0);
         dist[0] = 0;
-        ways[0] = 1;
-        int mod = (int)(1e9+7);
+        cnt[0] = 1;
+
+        long long MOD = 1e9 + 7;
 
         while(!pq.empty()) {
-            long long d = pq.top().first;
-            long long u = pq.top().second;
+            auto curr = pq.top();
             pq.pop();
-
-            if(d > dist[u]) continue;
-
-            for(auto it : adj[u]) {
-                long long v = it.first;
-                long long weight = it.second;
-                if(d+weight<dist[v]) {
-                    dist[v] = d + weight;
-                    ways[v] = ways[u];
-                    pq.push({dist[v],v});
+            long long d = curr.first;
+            long long u = curr.second;
+            for(auto &it : adj[u]) {
+                int v = it.first;
+                long long new_d = it.second;
+                if(d + new_d < dist[v]) {
+                    dist[v] = d + new_d;
+                    cnt[v] = cnt[u];
+                    pq.push({dist[v], v});
                 }
-                else if(d+weight == dist[v]) {
-                    ways[v] = (ways[v]+ways[u])%mod;
+                else if(d + new_d == dist[v]) {
+                    cnt[v] += cnt[u];
+                    cnt[v] %= MOD;
                 }
             }
         }
-        return (ways[n-1])%mod;
+
+        return cnt[n - 1];
     }
 };

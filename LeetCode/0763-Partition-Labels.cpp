f3b7bceb42f9f450d561c546673717a327243831
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-03-31 00:40:30 +0530

    Update 0763-Partition-Labels.cpp

--- LeetCode/0763-Partition-Labels.cpp
+++ LeetCode/0763-Partition-Labels.cpp
@@ -1,56 +1,67 @@
 // 763. Partition Labels
 
+// Merge Intervals
 class Solution {
 public:
     vector<int> partitionLabels(string s) {
-        unordered_map<char,int> mp;
-        for(int i=0; i<s.length(); i++) {
+        int n = s.length();
+        map<char,int> mp;
+        for(int i = 0; i < n; i++) {
             mp[s[i]] = i;
         }
-        
-        int prev = -1;
-        int maximum = 0;
-        vector<int> ans;
-        
-        for(int i=0; i<s.length(); i++) {
-            maximum = max(maximum,mp[s[i]]);
-            if(i == maximum) {
-                ans.push_back(maximum-prev);
-                prev = maximum;
+
+        vector<vector<int>> v;
+        for(int i = 0; i < n; i++) {
+            if(mp.find(s[i]) != mp.end()) {
+                v.push_back({i, mp[s[i]]});
+                mp.erase(mp.find(s[i]));
             }
         }
+
+        vector<vector<int>> v_merge;
+        int st = v[0][0], end = v[0][1];
+        for(int i = 1; i < v.size(); i++) {
+            if(v[i][0] < end) {
+                end = max(end, v[i][1]);
+            }
+            else {
+                v_merge.push_back({st, end});
+                st = v[i][0], end = v[i][1];
+            }
+        }
+        v_merge.push_back({st, end});
+
+        vector<int> ans;
+        for(auto &it : v_merge) {
+            ans.push_back(it[1] - it[0] + 1);
+        }
+
         return ans;
     }
 };
 
+// ==================================================
 
 class Solution {
 public:
-
-    int help(map<char,int>& mp, map<char,int>& mp2) {
-        int cnt = 0;
-        for(auto i : mp2) {
-            char ch = i.first;
-            int f = i.second;
-            if(mp[ch] != f) return -1;
-            cnt += f;
-        }
-        return cnt;
-    }
-
     vector<int> partitionLabels(string s) {
-        map<char,int> mp, mp2;
-        for(auto ch : s) mp[ch]++;
         int n = s.length();
-        vector<int> ans;
+        map<char,int> mp;
         for(int i = 0; i < n; i++) {
-            mp2[s[i]]++;
-            int curr = help(mp, mp2);
-            if(curr != -1) {
-                ans.push_back(curr);
-                mp2.clear();
+            mp[s[i]] = i;
+        }
+
+        vector<int> ans;
+        int st = 0, end = mp[s[0]];
+
+        for(int i = 0; i < n; i++) {
+            end = max(end, mp[s[i]]);
+            if(i == end) {
+                ans.push_back(end - st + 1);
+                st = i + 1;
             }
         }
+
         return ans;
     }
 };

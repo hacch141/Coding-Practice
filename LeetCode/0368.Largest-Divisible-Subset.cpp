977d00709ac3f468ff975bd1d4168e0850c02e8d
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-04-06 15:55:38 +0530

    Update 0368.Largest-Divisible-Subset.cpp

--- LeetCode/0368.Largest-Divisible-Subset.cpp
+++ LeetCode/0368.Largest-Divisible-Subset.cpp
@@ -2,30 +2,36 @@
 
 class Solution {
 public:
-
     vector<int> largestDivisibleSubset(vector<int>& nums) {
         int n = nums.size();
         sort(nums.begin(), nums.end());
-        vector<int> lis(n, 1), prev(n, -1), ans;
-        int mxLen = 0, st = -1;
-        
+
+        vector<int> cnt(n, 1), prev(n);
+        for(int i = 0; i < n; i++) prev[i] = i;
+
+        int mx = 0, mx_ind = -1;
+
         for(int i = 0; i < n; i++) {
-            for(int j = 0; j < i; j++) {
-                if(nums[i] % nums[j] == 0 && 1 + lis[j] > lis[i]) {
-                    lis[i] = 1 + lis[j];
-                    prev[i] = j;
+            for(int j = i + 1; j < n; j++) {
+                if(nums[j] % nums[i] == 0 && cnt[j] < 1 + cnt[i]) {
+                    cnt[j] = 1 + cnt[i];
+                    prev[j] = i;
                 }
             }
-            if(lis[i] > mxLen) {
-                mxLen = lis[i];
-                st = i;
+            if(cnt[i] > mx) {
+                mx = cnt[i];
+                mx_ind = i;
             }
         }
 
-        while(st != -1) {
-            ans.push_back(nums[st]);
-            st = prev[st];
+        vector<int> ans;
+        while(prev[mx_ind] != mx_ind) {
+            ans.push_back(nums[mx_ind]);
+            mx_ind = prev[mx_ind];
         }
+        ans.push_back(nums[mx_ind]);
+
+        reverse(ans.begin(), ans.end());
 
         return ans;
     }

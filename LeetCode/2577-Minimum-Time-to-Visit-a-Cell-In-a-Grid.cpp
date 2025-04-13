be3a2ebb12b1fb321c1826e854eb8ab1819364be
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-04-13 17:46:30 +0530

    Update 2577-Minimum-Time-to-Visit-a-Cell-In-a-Grid.cpp

--- LeetCode/2577-Minimum-Time-to-Visit-a-Cell-In-a-Grid.cpp
+++ LeetCode/2577-Minimum-Time-to-Visit-a-Cell-In-a-Grid.cpp
@@ -3,28 +3,37 @@
 class Solution {
 public:
     int minimumTime(vector<vector<int>>& grid) {
-        int n = grid.size(), m = grid[0].size();
         if(grid[0][1] > 1 && grid[1][0] > 1) return -1;
-        vector<vector<bool>> vis(n, vector<bool>(m, false));
+
+        int n = grid.size(), m = grid[0].size();
+        vector<vector<int>> time(n, vector<int>(m, INT_MAX));
+        time[0][0] = 0;
+
         priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
         pq.push({0, 0, 0});
-        vector<int> dx = {0, -1, 0, +1}, dy = {-1, 0, +1, 0};
-        while(!pq.empty()) {
-            auto curr = pq.top();
-            pq.pop();
-            int time = curr[0], x = curr[1], y = curr[2];
-            if(x == n - 1 && y == m - 1) return time;
-            for(int i = 0; i < 4; i++) {
-                int nx = x + dx[i], ny = y + dy[i];
-                if(nx >= 0 && nx < n && ny >= 0 && ny < m && !vis[nx][ny]) {
-                    int waitTime = ((grid[nx][ny] - time) % 2 == 0) ? 1 : 0;
-                    int new_time = max(time + 1, grid[nx][ny] + waitTime);
-                    vis[nx][ny] = true;
-                    pq.push({new_time, nx, ny});
 
+        vector<int> dx = {-1, 0, +1, 0};
+        vector<int> dy = {0, +1, 0, -1};
+
+        while(!pq.empty()) {
+            auto it = pq.top();
+            pq.pop();
+            int t1 = it[0];
+            int x1 = it[1];
+            int y1 = it[2];
+            for(int k = 0; k < 4; k++) {
+                int x2 = x1 + dx[k];
+                int y2 = y1 + dy[k];
+                if(x2 >= 0 && x2 < n && y2 >= 0 && y2 < m) {
+                    int t2 = grid[x2][y2] <= time[x1][y1] ? 1 + time[x1][y1] : grid[x2][y2] + (time[x1][y1] % 2 == grid[x2][y2] % 2);
+                    if(t2 < time[x2][y2]) {
+                        time[x2][y2] = t2;
+                        pq.push({t2, x2, y2});
+                    }
                 }
             }
         }
-        return -1;
+
+        return time[n - 1][m - 1];
     }
 };

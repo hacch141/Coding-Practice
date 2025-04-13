ba484646995d3da89f0fd1afe779ee6197b3f02f
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-04-13 17:16:01 +0530

    Update 2290-Minimum-Obstacle-Removal-to-Reach-Corner.cpp

--- LeetCode/2290-Minimum-Obstacle-Removal-to-Reach-Corner.cpp
+++ LeetCode/2290-Minimum-Obstacle-Removal-to-Reach-Corner.cpp
@@ -2,30 +2,35 @@
 
 class Solution {
 public:
-    int dijkstra(vector<vector<int>>& grid, vector<vector<bool>>& vis) {
+    int minimumObstacles(vector<vector<int>>& grid) {
         int n = grid.size(), m = grid[0].size();
-        priority_queue<pair<int, vector<int>>, vector<pair<int, vector<int>>>, greater<pair<int, vector<int>>>> pq;
-        vis[0][0] = true;
-        pq.push({grid[0][0], {0, 0}});
-        vector<int> dx = {-1, 0, +1, 0}, dy = {0, +1, 0, -1};
+        vector<vector<int>> dp(n, vector<int>(m, INT_MAX));
+        dp[0][0] = 0;
+
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+        pq.push({0, 0, 0});
+
+        vector<int> dx = {-1, 0, +1, 0};
+        vector<int> dy = {0, +1, 0, -1};
         while(!pq.empty()) {
-            int cnt_obs = pq.top().first, x = pq.top().second[0], y = pq.top().second[1];
-            if(x == n - 1 && y == m - 1) return cnt_obs;
+            auto it = pq.top();
             pq.pop();
-            for(int i = 0; i < 4; i++) {
-                int nx = x + dx[i], ny = y + dy[i];
-                if(nx >= 0 && nx < n && ny >= 0 && ny < m && !vis[nx][ny]) {
-                    vis[nx][ny] = true;
-                    pq.push({cnt_obs + grid[nx][ny], {nx, ny}});
+            int d1 = it[0];
+            int x1 = it[1];
+            int y1 = it[2];
+            for(int k = 0; k < 4; k++) {
+                int x2 = x1 + dx[k];
+                int y2 = y1 + dy[k];
+                if(x2 >= 0 && x2 < n && y2 >= 0 && y2 < m) {
+                    int d2 = d1 + (grid[x2][y2] == 1);
+                    if(d2 < dp[x2][y2]) {
+                        dp[x2][y2] = d2;
+                        pq.push({d2, x2, y2});
+                    }
                 }
             }
         }
-        return -1;
-    }
 
-    int minimumObstacles(vector<vector<int>>& grid) {
-        int n = grid.size(), m = grid[0].size();
-        vector<vector<bool>> vis(n, vector<bool>(m, false));
-        return dijkstra(grid, vis);
+        return dp[n - 1][m - 1];
     }
 };

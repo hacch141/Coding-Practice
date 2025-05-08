c27e49b223b0efb4a3e20ac5566c29bf14f7423e
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-05-09 00:32:52 +0530

    Create 3342-Find-Minimum-Time-to-Reach-Last-Room-II.cpp

---
+++ LeetCode/3342-Find-Minimum-Time-to-Reach-Last-Room-II.cpp
@@ -0,0 +1,33 @@
+// 3342. Find Minimum Time to Reach Last Room II
+
+class Solution {
+public:
+    int minTimeToReach(vector<vector<int>>& moveTime) {
+        int n = moveTime.size(), m = moveTime[0].size();
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+        pq.push({0, 1, 0, 0});
+        moveTime[0][0] = -1;
+
+        vector<int> dx = {-1, 0, +1, 0};
+        vector<int> dy = {0, +1, 0, -1};
+
+        while(!pq.empty()) {
+            auto v = pq.top();
+            pq.pop();
+            int t1 = v[0], add1 = v[1], x1 = v[2], y1 = v[3];
+            if(x1 == n - 1 && y1 == m - 1) return t1;
+            for(int x = 0; x < 4; x++) {
+                int x2 = x1 + dx[x];
+                int y2 = y1 + dy[x];
+                int add2 = add1 == 1 ? 2 : 1;
+                if(x2 >= 0 && x2 < n && y2 >= 0 && y2 < m && moveTime[x2][y2] != -1) {
+                    int t2 = add1 + max(t1, moveTime[x2][y2]);
+                    pq.push({t2, add2, x2, y2});
+                    moveTime[x2][y2] = -1;
+                }
+            }
+        }
+
+        return -1;
+    }
+};

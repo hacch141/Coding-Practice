4c10d883e02d06a298a73d023e6b07f008d05b8e
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-05-18 01:39:13 +0530

    Update 0958.Check-Completeness-of-a-Binary-Tree.cpp

--- LeetCode/0958.Check-Completeness-of-a-Binary-Tree.cpp
+++ LeetCode/0958.Check-Completeness-of-a-Binary-Tree.cpp
@@ -1,5 +1,6 @@
 // 958. Check Completeness of a Binary Tree
 
+// BFS
 class Solution {
 public:
     bool isCompleteTree(TreeNode* root) {
@@ -31,27 +32,23 @@ public:
     }
 };
 
+// DFS
 class Solution {
 public:
+    int get_count(TreeNode* root) {
+        if(!root) return 0;
+        return 1 + get_count(root->left) + get_count(root->right);
+    }
+
+    bool dfs(int ind, int& total_nodes, TreeNode* root) {
+        if(!root) return true;
+        if(ind >= total_nodes) return false;
+        return dfs(2 * ind + 1, total_nodes, root->left) && dfs(2 * ind + 2, total_nodes, root->right);
+    }
+
     bool isCompleteTree(TreeNode* root) {
-        queue<TreeNode*> q;
-        q.push(root);
-        bool stop = false;
-        while(!q.empty()) {
-            int sz = q.size();
-            while(sz--) {
-                auto curr = q.front();
-                q.pop();
-                if(!curr) {
-                    stop = true;
-                }
-                else {
-                    if(stop) return false;
-                    q.push(curr->left);
-                    q.push(curr->right);
-                }
-            }
-        }
-        return true;
+        int total_nodes = get_count(root);
+        int ind = 0;
+        return dfs(ind, total_nodes, root);
     }
 };

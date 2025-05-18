57d7d8ec6b3d2800fb0234c08f5ec30477950a44
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-05-18 16:14:46 +0530

    Update and rename 222.Count-Complete-Tree-Nodes.cpp to 0222.Count-Complete-Tree-Nodes.cpp

---
+++ LeetCode/0222.Count-Complete-Tree-Nodes.cpp
@@ -0,0 +1,26 @@
+// 222. Count Complete Tree Nodes\
+
+class Solution {
+public:
+    int countNodes(TreeNode* root) {
+        int lh = 0, rh = 0;
+        
+        TreeNode* curr = root;
+        while(curr != NULL) {
+            lh++;
+            curr = curr->left;
+        }
+        
+        curr = root;
+        while(curr != NULL) {
+            rh++;
+            curr = curr->right;
+        }
+        
+        if(lh == rh) {
+            return pow(2, lh) - 1;
+        }
+        
+        return 1 + countNodes(root->left) + countNodes(root->right);
+    }
+};

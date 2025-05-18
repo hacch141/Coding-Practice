38f1c2c276b81aba0ec83ed3170d2f50b53b4cd9
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-05-18 17:19:51 +0530

    Update 1026.Maximum-Difference-Between-Node-and-Ancestor.cpp

--- LeetCode/1026.Maximum-Difference-Between-Node-and-Ancestor.cpp
+++ LeetCode/1026.Maximum-Difference-Between-Node-and-Ancestor.cpp
@@ -2,6 +2,51 @@
 
 class Solution {
 public:
+    int dfs(TreeNode* root, int mn, int mx) {
+        if(!root) return mx - mn;
+        mn = min(mn, root->val);
+        mx = max(mx, root->val);
+        int l = dfs(root->left, mn, mx);
+        int r = dfs(root->right, mn, mx);
+        return max(l, r);
+    }
+
+    int maxAncestorDiff(TreeNode* root) {
+        return dfs(root, 1e6, -1);
+    }
+};
+
+// down to top
+class Solution {
+public:
+    pair<int,int> dfs(TreeNode* root, int& mx_diff) {
+        if(!root) return {1e6, -1};
+        if(!root->left && !root->right) return {root->val, root->val};
+
+        auto l = dfs(root->left, mx_diff);
+        auto r = dfs(root->right, mx_diff);
+
+        int mn = min(l.first, r.first);
+        int mx = max(l.second, r.second);
+
+        mx_diff = max(mx_diff, abs(root->val - mn));
+        mx_diff = max(mx_diff, abs(root->val - mx));
+
+        mn = min(mn, root->val);
+        mx = max(mx, root->val);
+        return {mn, mx};
+    }
+
+    int maxAncestorDiff(TreeNode* root) {
+        int mx_diff = 0;
+        dfs(root, mx_diff);
+        return mx_diff;
+    }
+};
+
+// top to down
+class Solution {
+public:
 
     void solve(TreeNode* root, int mx, int mn, int& ans) {
         if(!root) return;

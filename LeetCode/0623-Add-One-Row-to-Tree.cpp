eb70d804591f1e6ea08b41aaaa848e4797c807c3
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-05-18 15:41:41 +0530

    Update 0623-Add-One-Row-to-Tree.cpp

--- LeetCode/0623-Add-One-Row-to-Tree.cpp
+++ LeetCode/0623-Add-One-Row-to-Tree.cpp
@@ -1,41 +1,72 @@
 // 623. Add One Row to Tree
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+// DFS
 class Solution {
 public:
-
-    void solve(TreeNode* root, int val, int depth) {
+    void dfs(TreeNode* root, int val, int depth) {
         if(!root) return;
         if(depth == 2) {
-            TreeNode* l = root->left;
-            root->left = new TreeNode(val);
-            root->left->left = l;
-            TreeNode* r = root->right;
-            root->right = new TreeNode(val);
-            root->right->right = r;
+            TreeNode* new_node_l = new TreeNode(val);
+            new_node_l->left = root->left;
+            root->left = new_node_l;
+            TreeNode* new_node_r = new TreeNode(val);
+            new_node_r->right = root->right;
+            root->right = new_node_r;
             return;
         }
-        solve(root->left, val, depth - 1);
-        solve(root->right, val, depth - 1);
+        dfs(root->left, val, depth - 1);
+        dfs(root->right, val, depth - 1);
     }
 
     TreeNode* addOneRow(TreeNode* root, int val, int depth) {
         if(depth == 1) {
-            TreeNode* newRoot = new TreeNode(val);
-            newRoot->left = root;
-            return newRoot;
+            TreeNode* new_root = new TreeNode(val);
+            new_root->left = root;
+            return new_root;
         }
-        solve(root, val, depth);
+        
+        dfs(root, val, depth);
+        return root;
+    }
+};
+
+
+// BFS
+class Solution {
+public:
+    TreeNode* addOneRow(TreeNode* root, int val, int depth) {
+        if(depth == 1) {
+            TreeNode* new_root = new TreeNode(val);
+            new_root->left = root;
+            return new_root;
+        }
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        int lvl = 1;
+        while(!q.empty()) {
+            if(lvl == depth - 1) break;
+            int sz = q.size();
+            while(sz--) {
+                auto curr = q.front();
+                q.pop();
+                if(curr->left) q.push(curr->left);
+                if(curr->right) q.push(curr->right);
+            }
+            lvl++;
+        }
+
+        while(!q.empty()) {
+            auto curr = q.front();
+            q.pop();
+            TreeNode* new_node_l = new TreeNode(val);
+            new_node_l->left = curr->left;
+            curr->left = new_node_l;
+            TreeNode* new_node_r = new TreeNode(val);
+            new_node_r->right = curr->right;
+            curr->right = new_node_r;
+        }
+
         return root;
     }
 };

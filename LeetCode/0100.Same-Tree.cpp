c18904a8a7744a613a6e70253628b3502d84769f
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-05-18 23:12:03 +0530

    Update 0100.Same-Tree.cpp

--- LeetCode/0100.Same-Tree.cpp
+++ LeetCode/0100.Same-Tree.cpp
@@ -1,5 +1,6 @@
 // 100. Same Tree
 
+// DFS
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
@@ -7,5 +8,41 @@ public:
         if(p==NULL || q==NULL) return false;
         if(p->val != q->val) return false;
         return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+    }
+};
+
+// BFS
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p == NULL && q == NULL)
+            return true;
+        if(p == NULL || q == NULL)
+            return false;
+        queue<TreeNode*> q1;
+        queue<TreeNode*> q2;
+        q1.push(p);
+        q2.push(q);
+        while(!q1.empty() && !q2.empty()) {
+            TreeNode* first = q1.front();
+            TreeNode* second = q2.front();
+            q1.pop();
+            q2.pop();
+            if(first->val != second->val)
+                return false;
+          
+            if(first->left && second->left) {
+                q1.push(first->left);
+                q2.push(second->left);
+            } else if(first->left || second->left)
+                return false;
+          
+            if(first->right && second->right) {
+                q1.push(first->right);
+                q2.push(second->right);
+            } else if(first->right || second->right)
+                return false;
+        }
+        return true; 
     }
 };

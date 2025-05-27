ab9de3435764f3ed736af32a26d8fc01646be639
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-05-27 22:09:14 +0530

    Create 2894-Divisible-and-Non-divisible-Sums-Difference.cpp

---
+++ LeetCode/2894-Divisible-and-Non-divisible-Sums-Difference.cpp
@@ -0,0 +1,12 @@
+// 2894. Divisible and Non-divisible Sums Difference
+
+class Solution {
+public:
+    int differenceOfSums(int n, int m) {
+        int ans = 0;
+        for(int i = 1; i <= n; i++) {
+            ans += ((i % m) != 0) ? i : -i;
+        }
+        return ans;
+    }
+};

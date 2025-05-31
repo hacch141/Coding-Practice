f6eb5df6832ae6eb2e345a7393c14e47754cefb1
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-06-01 03:27:37 +0530

    Update 1361-Validate-Binary-Tree-Nodes.cpp

--- LeetCode/1361-Validate-Binary-Tree-Nodes.cpp
+++ LeetCode/1361-Validate-Binary-Tree-Nodes.cpp
@@ -1,16 +1,66 @@
 // 1361. Validate Binary Tree Nodes
 
+// BFS
+class Solution {
+public:
+    bool validateBinaryTreeNodes(int n, vector<int>& leftChild, vector<int>& rightChild) {
+        vector<int> indegree(n, 0);
+        for(int i = 0; i < n; i++) {
+            if(leftChild[i] != -1) indegree[leftChild[i]]++;
+            if(rightChild[i] != -1) indegree[rightChild[i]]++;
+        }
+        
+        int st = -1;
+        for(int i = 0; i < n; i++) {
+            if(indegree[i] == 0) {
+                if(st == -1) st = i;
+                else return false;
+            }
+            if(indegree[i] > 1) return false;
+        }
+        if(st == -1) return false;
+
+        vector<bool> vis(n, false);
+        vis[st] = true;
+
+        queue<int> q;
+        q.push(st);
+
+        while(!q.empty()) {
+            int sz = q.size();
+            while(sz--) {
+                int curr = q.front();
+                q.pop();
+                if(leftChild[curr] != -1 && !vis[leftChild[curr]]) {
+                    q.push(leftChild[curr]);
+                    vis[leftChild[curr]] = true;
+                }
+                if(rightChild[curr] != -1 && !vis[rightChild[curr]]) {
+                    q.push(rightChild[curr]);
+                    vis[rightChild[curr]] = true;
+                }
+            }
+        }
+
+        for(int i = 0; i < n; i++) {
+            if(!vis[i]) return false;
+        }
+
+        return true;
+    }
+};
+
+// DFS
 class Solution {
 public:
 
-    bool dfs(int u, int n, vector<int>& leftChild, vector<int>& rightChild, vector<bool>& vis, int& cnt) {
+    bool dfs(int u, vector<int>& leftChild, vector<int>& rightChild, vector<bool>& vis) {
         vis[u] = true;
-        cnt++;
         if(leftChild[u] != -1) {
-            if(vis[leftChild[u]] || !dfs(leftChild[u], n, leftChild, rightChild, vis, cnt)) return false;
+            if(vis[leftChild[u]] || !dfs(leftChild[u], leftChild, rightChild, vis)) return false;
         }
         if(rightChild[u] != -1) {
-            if(vis[rightChild[u]] || !dfs(rightChild[u], n, leftChild, rightChild, vis, cnt)) return false;
+            if(vis[rightChild[u]] || !dfs(rightChild[u], leftChild, rightChild, vis)) return false;
         }
         return true;
     }
@@ -25,21 +75,20 @@ public:
         int st = -1;
         for(int i = 0; i < n; i++) {
             if(indegree[i] == 0) {
-                if(st == -1) {
-                    st = i;
-                }
-                else {
-                    return false;
-                }
+                if(st == -1) st = i;
+                else return false;
             }
         }
-
         if(st == -1) return false;
 
         vector<bool> vis(n, false);
-        int cnt = 0;
-        if(!dfs(st, n, leftChild, rightChild, vis, cnt)) return false;
-        return cnt == n;
+        if(!dfs(st, leftChild, rightChild, vis)) return false;
+
+        for(int i = 0; i < n; i++) {
+            if(!vis[i]) return false;
+        }
+
+        return true;
     }
 };
 
@@ -51,25 +100,16 @@ public:
     int components;
 
     int find(int x) {
-        if(parent[x] == x)
-            return x;
-        
+        if(parent[x] == x) return x;
         return parent[x] = find(parent[x]);
     }
 
     bool Union(int par, int child) {
         int child_ka_parent  = find(child);
-    
-        //child_ka_parent != child --> Child had already some other parent
-        if(child_ka_parent != child)
-            return false;
-        
         int parent_ka_parent = find(par);
-        //parent_ka_parent == child_ka_parent ---> Parallel edge (It means, already they were connected by another edge)
-        if (parent_ka_parent == child_ka_parent) {
-            return false;
-        }
         
+        if(child_ka_parent != child || parent_ka_parent == child) return false;
+
         parent[child] = par;
         components--;
         return true;
@@ -78,19 +118,13 @@ public:
     bool validateBinaryTreeNodes(int n, vector<int>& leftChild, vector<int>& rightChild) {
         components = n;
         parent.resize(n);
-        for(int i = 0; i<n; i++) {
-            parent[i] = i;
-        }
+        for(int i = 0; i<n; i++) parent[i] = i;
 
         for (int i = 0; i < n; i++) {
-            if (leftChild[i] >= 0 && !Union(i, leftChild[i])) {
-                return false;
-            }
-            if (rightChild[i] >= 0 && !Union(i, rightChild[i])) {
-                return false;
-            }
+            if (leftChild[i] >= 0 && !Union(i, leftChild[i])) return false;
+            if (rightChild[i] >= 0 && !Union(i, rightChild[i])) return false;
         }
-        
+
         return components == 1;
     }
 };

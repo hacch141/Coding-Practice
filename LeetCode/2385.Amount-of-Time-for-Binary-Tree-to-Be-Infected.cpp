4381f52f8f5eef535a4c3d196df08b870701f439
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-06-01 17:15:06 +0530

    Update 2385.Amount-of-Time-for-Binary-Tree-to-Be-Infected.cpp

--- LeetCode/2385.Amount-of-Time-for-Binary-Tree-to-Be-Infected.cpp
+++ LeetCode/2385.Amount-of-Time-for-Binary-Tree-to-Be-Infected.cpp
@@ -1,68 +1,53 @@
 // 2385. Amount of Time for Binary Tree to Be Infected
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-
-    void findParents(TreeNode*& root, unordered_map<int,TreeNode*>& mp, int& start, TreeNode*& st) {
+    void find_par(TreeNode* root, unordered_map<TreeNode*,TreeNode*>& par, int& start, TreeNode*& st) {
         if(!root) return;
-        if(root->val == start) {
-            st = root;
-        }
+        if(root->val == start) st = root;
         if(root->left) {
-            mp[root->left->val] = root;
-            findParents(root->left, mp, start, st);
+            par[root->left] = root;
+            find_par(root->left, par, start, st);
         }
         if(root->right) {
-            mp[root->right->val] = root;
-            findParents(root->right, mp, start, st);
+            par[root->right] = root;
+            find_par(root->right, par, start, st);
         }
-    }
-
-    int bfs(TreeNode*& st, unordered_map<int,TreeNode*>& mp, unordered_map<int,bool>& vis) {
-        queue<TreeNode*> q;
-        q.push(st);
-        vis[st->val] = true;
-        int time = -1;
-        while(!q.empty()) {
-            int size = q.size();
-            for(int i=0; i<size; i++) {
-                TreeNode* curr = q.front();
-                q.pop();
-                if(curr->left && !vis[curr->left->val]) {
-                    q.push(curr->left);
-                    vis[curr->left->val] = true;
-                }
-                if(curr->right && !vis[curr->right->val]) {
-                    q.push(curr->right);
-                    vis[curr->right->val] = true;
-                }
-                TreeNode* par = mp[curr->val];
-                if(par && !vis[par->val]) {
-                    q.push(par);
-                    vis[par->val] = true;
-                }
-            }
-            time++;
-        }
-        return time;
     }
 
     int amountOfTime(TreeNode* root, int start) {
+        unordered_map<TreeNode*,TreeNode*> par;
         TreeNode* st;
-        unordered_map<int,TreeNode*> mp;
-        unordered_map<int,bool> vis;
-        findParents(root, mp, start, st);
-        return bfs(st, mp, vis);
+        find_par(root, par, start, st);
+
+        queue<TreeNode*> q;
+        q.push(st);
+
+        set<int> vis;
+        vis.insert(start);
+
+        int min = -1;
+        while(!q.empty()) {
+            int sz = q.size();
+            while(sz--) {
+                auto curr = q.front();
+                q.pop();
+                if(curr->left && !vis.count(curr->left->val)) {
+                    q.push(curr->left);
+                    vis.insert(curr->left->val);
+                }
+                if(curr->right && !vis.count(curr->right->val)) {
+                    q.push(curr->right);
+                    vis.insert(curr->right->val);
+                }
+                if(par.count(curr) && !vis.count(par[curr]->val)) {
+                    q.push(par[curr]);
+                    vis.insert(par[curr]->val);
+                }
+            }
+            min++;
+        }
+
+        return min;
     }
 };

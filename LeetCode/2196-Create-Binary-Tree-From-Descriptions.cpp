e9c023491b2a4f8cf3193da6290d2fd738893582
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-06-03 01:13:24 +0530

    Update 2196-Create-Binary-Tree-From-Descriptions.cpp

--- LeetCode/2196-Create-Binary-Tree-From-Descriptions.cpp
+++ LeetCode/2196-Create-Binary-Tree-From-Descriptions.cpp
@@ -1,16 +1,36 @@
 // 2196. Create Binary Tree From Descriptions
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
+class Solution {
+public:
+    TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
+        unordered_map<int,TreeNode*> mp;
+        set<int> childs;
+
+        for(auto it : descriptions) {
+            TreeNode* par;
+            TreeNode* child;
+
+            if(mp.count(it[0])) par = mp[it[0]];
+            else mp[it[0]] = par = new TreeNode(it[0]);
+
+            if(mp.count(it[1])) child = mp[it[1]];
+            else mp[it[1]] = child = new TreeNode(it[1]);
+
+            if(it[2]) par->left = child;
+            else par->right = child;
+
+            childs.insert(it[1]);
+        }
+
+        for(auto it : descriptions) {
+            if(!childs.count(it[0])) return mp[it[0]];
+        }
+
+        return NULL;
+    }
+};
+
+// Kind of BFS
 class Solution {
 public:
     TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {

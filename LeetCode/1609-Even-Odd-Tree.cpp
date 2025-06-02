f1bd030c633ba43ac37540117ffa2f0cf985dff3
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-06-03 00:24:16 +0530

    Update 1609-Even-Odd-Tree.cpp

--- LeetCode/1609-Even-Odd-Tree.cpp
+++ LeetCode/1609-Even-Odd-Tree.cpp
@@ -1,30 +1,38 @@
 // 1609. Even Odd Tree
 
+// BFS
 class Solution {
 public:
     bool isEvenOddTree(TreeNode* root) {
         queue<TreeNode*> q;
         q.push(root);
+
         int lvl = 0;
         while(!q.empty()) {
-            int len = q.size();
-            TreeNode* prev = q.front();
-            q.pop();
-            if(prev->left) q.push(prev->left);
-            if(prev->right) q.push(prev->right);
-            if((lvl % 2 == 0 && prev->val % 2 == 0) || (lvl % 2 == 1 && prev->val % 2 == 1)) return false;
-            for(int i = 1; i < len; i++) {
-                TreeNode* cur = q.front();
-                if(cur->left) q.push(cur->left);
-                if(cur->right) q.push(cur->right);
+            int sz = q.size();
+            vector<int> v;
+            while(sz--) {
+                auto curr = q.front();
                 q.pop();
-                if(lvl % 2 == 0) {
-                    if(cur->val <= prev->val || cur->val % 2 == 0) return false;
+                v.push_back(curr->val);
+                if(curr->left) {
+                    q.push(curr->left);
                 }
-                else {
-                    if(cur->val >= prev->val || cur->val % 2 == 1) return false;
+                if(curr->right) {
+                    q.push(curr->right);
                 }
-                prev = cur;
+            }
+
+            int n = v.size();
+            if(lvl % 2 == 0) {
+                for(int i = 0; i < n; i++) {
+                    if(v[i] % 2 == 0 || (i + 1 < n && v[i] >= v[i + 1])) return false;
+                }
+            }
+            else {
+                for(int i = n - 1; i >= 0; i--) {
+                    if(v[i] % 2 != 0 || (i - 1 >= 0 && v[i] >= v[i - 1])) return false;
+                }
             }
             lvl++;
         }

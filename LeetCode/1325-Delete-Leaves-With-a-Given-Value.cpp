ef6d62f55864dd67e9f0c59ddefcb9b778fcb1a3
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-06-03 00:57:22 +0530

    Update 1325-Delete-Leaves-With-a-Given-Value.cpp

--- LeetCode/1325-Delete-Leaves-With-a-Given-Value.cpp
+++ LeetCode/1325-Delete-Leaves-With-a-Given-Value.cpp
@@ -1,33 +1,19 @@
 // 1325. Delete Leaves With a Given Value
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
+    TreeNode* dfs(TreeNode* root, int target) {
+        if(!root) return NULL;
 
-    bool solve(TreeNode* root, int target) {
-        if(!root) return true;
+        root->left = dfs(root->left, target);
+        root->right = dfs(root->right, target);
 
-        bool l = solve(root->left, target);
-        bool r = solve(root->right, target);
+        if(!root->left && !root->right && root->val == target) return NULL;
 
-        if(l) root->left = NULL;
-        if(r) root->right = NULL;
-
-        return (l && r && root->val == target);
+        return root;
     }
 
     TreeNode* removeLeafNodes(TreeNode* root, int target) {
-        bool isRootDeleted = solve(root, target);
-        return isRootDeleted ? NULL : root;
+        return dfs(root,target);
     }
 };

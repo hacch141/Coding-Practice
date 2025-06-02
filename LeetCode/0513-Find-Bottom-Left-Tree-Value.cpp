dac61f4e63328f2634336d2e37c039d17295a108
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-06-03 00:10:59 +0530

    Update 0513-Find-Bottom-Left-Tree-Value.cpp

--- LeetCode/0513-Find-Bottom-Left-Tree-Value.cpp
+++ LeetCode/0513-Find-Bottom-Left-Tree-Value.cpp
@@ -1,25 +1,42 @@
 // 513. Find Bottom Left Tree Value
 
+// BFS
 class Solution {
 public:
     int findBottomLeftValue(TreeNode* root) {
         queue<TreeNode*> q;
         q.push(root);
-        int ans = -1;
+        int ans;
         while(!q.empty()) {
-            int len = q.size();
-            bool f = true;
-            while(len--) {
-                TreeNode* node = q.front();
+            int sz = q.size();
+            ans = q.front()->val;
+            while(sz--) {
+                auto curr = q.front();
                 q.pop();
-                if(f) {
-                    ans = node->val;
-                    f = false;
-                }
-                if(node->left) q.push(node->left);
-                if(node->right) q.push(node->right);
+                if(curr->left) q.push(curr->left);
+                if(curr->right) q.push(curr->right);
             }
         }
+        return ans;
+    }
+};
+
+// DFS
+class Solution {
+public:
+    void dfs(TreeNode* root, int depth, int& mx_depth, int& ans) {
+        if(!root) return;
+        if(depth > mx_depth) {
+            ans = root->val;
+            mx_depth = depth;
+        }
+        dfs(root->left, depth + 1, mx_depth, ans);
+        dfs(root->right, depth + 1, mx_depth, ans);
+    }
+
+    int findBottomLeftValue(TreeNode* root) {
+        int ans, mx_depth = 0;
+        dfs(root, 1, mx_depth, ans);
         return ans;
     }
 };

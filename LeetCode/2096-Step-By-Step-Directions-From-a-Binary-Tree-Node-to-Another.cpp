d490f52488f6cab4250c5ea214a858fee284682b
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-06-05 02:06:18 +0530

    Update 2096-Step-By-Step-Directions-From-a-Binary-Tree-Node-to-Another.cpp

--- LeetCode/2096-Step-By-Step-Directions-From-a-Binary-Tree-Node-to-Another.cpp
+++ LeetCode/2096-Step-By-Step-Directions-From-a-Binary-Tree-Node-to-Another.cpp
@@ -1,53 +1,41 @@
 // 2096. Step-By-Step Directions From a Binary Tree Node to Another
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-
-    TreeNode* getLCA(TreeNode* root, int u, int v) {
+    TreeNode* get_lca(TreeNode* root, int startValue, int destValue) {
         if(!root) return NULL;
-        if(root->val == u || root->val == v) return root;
-        TreeNode* l = getLCA(root->left, u, v);
-        TreeNode* r = getLCA(root->right, u, v);
+        if(root->val == startValue || root->val == destValue) return root;
+        TreeNode* l = get_lca(root->left, startValue, destValue);
+        TreeNode* r = get_lca(root->right, startValue, destValue);
         if(l && r) return root;
         if(l) return l;
         if(r) return r;
         return NULL;
     }
 
-    void getPath(TreeNode* root, int val, string& cur, string& ans) {
-        if(!root || ans.length() > 0) return;
+    void get_root_to_val_path(TreeNode* root, int val, string& curr, string& path) {
+        if(!root) return;
         if(root->val == val) {
-            ans = cur;
+            path = curr;
             return;
         }
 
-        cur.push_back('L');
-        getPath(root->left, val, cur, ans);
-        cur.pop_back();
+        curr.push_back('L');
+        get_root_to_val_path(root->left, val, curr, path);
+        curr.pop_back();
 
-        cur.push_back('R');
-        getPath(root->right, val, cur, ans);
-        cur.pop_back();
+        curr.push_back('R');
+        get_root_to_val_path(root->right, val, curr, path);
+        curr.pop_back();
     }
 
     string getDirections(TreeNode* root, int startValue, int destValue) {
-        TreeNode* lca = getLCA(root, startValue, destValue);
-        string cur, l, r;
-        getPath(lca, startValue, cur, l);
-        getPath(lca, destValue, cur, r);
-        string path(l.length(), 'U');
-        path += r;
-        return path;
+        TreeNode* lca = get_lca(root, startValue, destValue);
+        string up, down, curr = "";
+        get_root_to_val_path(lca, startValue, curr, up);
+        get_root_to_val_path(lca, destValue, curr, down);
+        string ans(up.length(), 'U');
+        ans += down;
+        return ans;
     }
 };

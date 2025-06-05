6ecd6f5899f4aee94a52c2f9bc2a4cda98aa580d
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-06-06 01:14:30 +0530

    Update 2415.Reverse-Odd-Levels-of-Binary-Tree.cpp

--- LeetCode/2415.Reverse-Odd-Levels-of-Binary-Tree.cpp
+++ LeetCode/2415.Reverse-Odd-Levels-of-Binary-Tree.cpp
@@ -9,27 +9,24 @@ public:
         int lvl = 0;
         while(!q.empty()) {
             int sz = q.size();
-
-            vector<int> val;
             vector<TreeNode*> nodes;
 
             while(sz--) {
                 TreeNode* curr = q.front();
                 q.pop();
 
-                if(lvl & 1) {
-                    val.push_back(curr->val);
-                    nodes.push_back(curr);
-                }
+                if(lvl & 1) nodes.push_back(curr);
 
                 if(curr->left) q.push(curr->left);
                 if(curr->right) q.push(curr->right);
             }
 
             if(lvl & 1) {
-                reverse(val.begin(), val.end());
-                for(int i = 0; i < nodes.size(); i++) {
-                    nodes[i]->val = val[i];
+                int l = 0, r = nodes.size() - 1;
+                while(l < r) {
+                    swap(nodes[l]->val, nodes[r]->val);
+                    l++;
+                    r--;
                 }
             }
 

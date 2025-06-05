f5465542b19012479f3cafe27b02cd36ff1b5885
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-06-06 01:01:05 +0530

    Update 0951-Flip-Equivalent-Binary-Trees.cpp

--- LeetCode/0951-Flip-Equivalent-Binary-Trees.cpp
+++ LeetCode/0951-Flip-Equivalent-Binary-Trees.cpp
@@ -1,29 +1,18 @@
 // 951. Flip Equivalent Binary Trees
 
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-
-    bool solve(TreeNode* root1, TreeNode* root2) {
+    bool dfs(TreeNode* root1, TreeNode* root2) {
         if(!root1 && !root2) return true;
-        if(!root1 || !root2) return false;
-        if(root1->val != root2->val) return false;
-        bool noswap = solve(root1->left, root2->left) && solve(root1->right, root2->right);
-        bool swap = solve(root1->left, root2->right) && solve(root1->right, root2->left);
-        return noswap || swap;
+        if(!root1 || !root2 || (root1->val != root2->val)) return false;
+
+        bool flip = dfs(root1->left, root2->right) && dfs(root1->right, root2->left);
+        bool no_flip = dfs(root1->left, root2->left) && dfs(root1->right, root2->right);
+
+        return flip || no_flip;
     }
 
     bool flipEquiv(TreeNode* root1, TreeNode* root2) {
-        return solve(root1, root2);
+        return dfs(root1, root2);
     }
 };

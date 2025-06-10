f88516628bc2f378148ac8cfdde9410270b38fc5
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-06-11 00:09:45 +0530

    Update 0889-Construct-Binary-Tree-from-Preorder-and-Postorder-Traversal.cpp

--- LeetCode/0889-Construct-Binary-Tree-from-Preorder-and-Postorder-Traversal.cpp
+++ LeetCode/0889-Construct-Binary-Tree-from-Preorder-and-Postorder-Traversal.cpp
@@ -2,28 +2,25 @@
 
 class Solution {
 public:
-    TreeNode* rec(int l, int r, int& pre_ind, vector<int>& preorder, vector<int>& postorder, vector<int>& get_ind) {
-        if (l > r || pre_ind >= preorder.size()) return nullptr;
-        
+    TreeNode* dfs(int l, int r, int& pre_ind, int& n, vector<int>& preorder, vector<int>& postorder, unordered_map<int,int>& mp) {
+        if(l > r || pre_ind >= n) return NULL;
+
         TreeNode* root = new TreeNode(preorder[pre_ind]);
         pre_ind++;
 
-        if (l == r) return root;
+        if(pre_ind >= n || l == r) return root;
+        int mid_ind = mp[preorder[pre_ind]];
 
-        int post_ind = get_ind[preorder[pre_ind]];
-        
-        root->left = rec(l, post_ind, pre_ind, preorder, postorder, get_ind);
-        root->right = rec(post_ind + 1, r - 1, pre_ind, preorder, postorder, get_ind);
-        
+        root->left = dfs(l, mid_ind, pre_ind, n, preorder, postorder, mp);
+        root->right = dfs(mid_ind + 1, r - 1, pre_ind, n, preorder, postorder, mp);
+
         return root;
     }
 
     TreeNode* constructFromPrePost(vector<int>& preorder, vector<int>& postorder) {
         int n = preorder.size(), pre_ind = 0;
-        vector<int> get_ind(31, -1);
-        for (int i = 0; i < n; i++) {
-            get_ind[postorder[i]] = i;
-        }
-        return rec(0, n - 1, pre_ind, preorder, postorder, get_ind);
+        unordered_map<int,int> mp;
+        for(int i = 0; i < n; i++) mp[postorder[i]] = i;
+        return dfs(0, n - 1, pre_ind, n, preorder, postorder, mp);
     }
 };

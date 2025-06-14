b6c603c454bd247f7e05d9adf81fbb1b2f641d64
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-06-14 20:32:54 +0530

    Update 1235.Maximum-Profit-in-Job-Scheduling.cpp

--- LeetCode/1235.Maximum-Profit-in-Job-Scheduling.cpp
+++ LeetCode/1235.Maximum-Profit-in-Job-Scheduling.cpp
@@ -2,33 +2,41 @@
 
 class Solution {
 public:
+    int dp[50001];
 
-    int solve(int ind, int& n, vector<vector<int>>& v, vector<int>& dp) {
-        if(ind == n) return 0;
+    int get_ind(int val, vector<vector<int>>& v) {
+        int n = v.size();
+        int l = 0, h = n - 1, ind = n;
+        while(l <= h) {
+            int m = (h - l) / 2 + l;
+            if(val <= v[m][0]) {
+                ind = m;
+                h = m - 1;
+            }
+            else {
+                l = m + 1;
+            }
+        }
+        return ind;
+    }
 
+    int solve(int ind, int& n, vector<vector<int>>& v) {
+        if(ind >= n) return 0;
         if(dp[ind] != -1) return dp[ind];
-
-        auto it = lower_bound(v.begin(), v.end(), v[ind][1], [](const vector<int>& a, int value) {
-            return a[0] < value;
-        });
-        int nextIndex = it - v.begin();
-
-        int take = v[ind][2] + solve(nextIndex, n, v, dp);
-        int nottake = solve(ind+1, n, v, dp);
-
+        int nxt_ind = get_ind(v[ind][1], v);
+        int take = v[ind][2] + solve(nxt_ind, n, v);
+        int nottake = solve(ind + 1, n, v);
         return dp[ind] = max(take, nottake);
     }
 
     int jobScheduling(vector<int>& startTime, vector<int>& endTime, vector<int>& profit) {
         int n = startTime.size();
-        vector<vector<int>> v(n, vector<int>(3));
-        for(int i=0; i<n; i++) {
-            v[i][0] = startTime[i];
-            v[i][1] = endTime[i];
-            v[i][2] = profit[i];
+        memset(dp, -1, sizeof(dp));
+        vector<vector<int>> v;
+        for(int i = 0; i < n; i++) {
+            v.push_back({startTime[i], endTime[i], profit[i]});
         }
-        sort(v.begin(),v.end());
-        vector<int> dp(n+1, -1);
-        return solve(0, n, v, dp);
+        sort(v.begin(), v.end());
+        return solve(0, n, v);
     }
 };

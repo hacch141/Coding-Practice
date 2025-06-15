9087d074c186e5737093a07493e70ad632dc572c
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-06-16 00:28:24 +0530

    Update 0055.Jump-Game.cpp

--- LeetCode/0055.Jump-Game.cpp
+++ LeetCode/0055.Jump-Game.cpp
@@ -1,44 +1,42 @@
 // 55. Jump Game
 
-
-
+// Rec
 class Solution {
-
 public:
+    int dp[10001];
+
+    bool solve(int ind, int& n, vector<int>& nums) {
+        if(ind >= n - 1) return true;
+
+        if(dp[ind] != -1) return dp[ind];
+
+        bool f = false;
+        for(int i = 1; i <= nums[ind]; i++) {
+            if(solve(ind + i, n, nums)) {
+                f = true;
+                break;
+            }
+        }
+
+        return dp[ind] = f;
+    }
+
     bool canJump(vector<int>& nums) {
         int n = nums.size();
-        int maxi = nums[0];
-        int i;
-        for(i=1; i<n && i<=maxi; i++) {
-            maxi = max(maxi,i+nums[i]);
-        }
-        return i==n;
+        memset(dp, -1, sizeof(dp));
+        return solve(0, n, nums);
     }
 };
 
-
-// =======================================================
-
+// DP
 class Solution {
-
-private:
-    bool solve(int i, int n, vector<int>& nums, vector<int>& dp) {
-        if(i==n-1) return true;
-        if(nums[i]==0) return false;
-        if(dp[i] != -1) return dp[i];
-        bool isJump = false;
-        for(int jump=1; jump<=nums[i] && i+jump<n; jump++) {
-            isJump = solve(i+jump,n,nums,dp);
-            if(isJump) return true;
-        }
-        return dp[i] = isJump;
-    }
-
 public:
     bool canJump(vector<int>& nums) {
-        int n = nums.size();
-        if(nums.size()==1) return true;
-        vector<int> dp(n+1,-1);
-        return solve(0,n,nums,dp);
+        int n = nums.size(), mx = nums[0];
+        for(int i = 0; i < n; i++) {
+            if(i > mx) return false;
+            mx = max(mx, i + nums[i]);
+        }
+        return true;
     }
 };

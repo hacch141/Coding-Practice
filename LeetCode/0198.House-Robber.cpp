de69cb6a6e283c55b6f9bd7b554a33ecb2a773c7
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-06-15 18:43:59 +0530

    Update 0198.House-Robber.cpp

--- LeetCode/0198.House-Robber.cpp
+++ LeetCode/0198.House-Robber.cpp
@@ -1,5 +1,6 @@
 // 198. House Robber
 
+// DP
 class Solution {
 public:
     int rob(vector<int>& nums) {
@@ -13,5 +14,27 @@ public:
             prev = curr;
         }
         return prev;
+    }
+};
+
+// Rec
+class Solution {
+public:
+    int dp[101];
+
+    int solve(int ind, int& n, vector<int>& nums) {
+        if(ind >= n) return 0;
+
+        if(dp[ind] != -1) return dp[ind];
+
+        int take = nums[ind] + solve(ind + 2, n, nums);
+        int nottake = solve(ind + 1, n, nums);
+        return dp[ind] = max(take, nottake);
+    }
+
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        memset(dp, -1, sizeof(dp));
+        return solve(0, n, nums);
     }
 };

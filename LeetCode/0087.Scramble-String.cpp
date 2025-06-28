b98433709434d468a77668113c56fccedaed2adb
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-06-28 20:59:01 +0530

    Update 0087.Scramble-String.cpp

--- LeetCode/0087.Scramble-String.cpp
+++ LeetCode/0087.Scramble-String.cpp
@@ -2,36 +2,30 @@
 
 class Solution {
 public:
-
-    bool solve(string s1, string s2, unordered_map<string,bool>& mp) {
-
-        if(mp.find(s1+s2) != mp.end()) return mp[s1+s2];
-
-        if(s1.length() != s2.length()) return false;
-        if(s1.compare(s2)==0) return true;
-
-        if(s1.length()<=1 || s2.length()<=1) return false;
+    bool solve(string s1, string s2, unordered_map<string, bool>& mp) {
+        if(mp.count(s1 + "#" + s2)) return mp[s1 + "#" + s2];
         int n = s1.length();
+        if(s1 == s2) return true;
+        if(n == 1) return false;
+        for(int i = 1; i < n; i++) {
+            // swap(x, y) --> len(x1) == len(y2) && len(y1) == len(x2)
+            string x1 = s1.substr(0, i);
+            string y1 = s1.substr(i);
+            string x2 = s2.substr(0, n - i);
+            string y2 = s2.substr(n - i);
+            bool swap = solve(x1, y2, mp) && solve(x2, y1, mp);
 
-        bool swap, noswap;
-        bool flag = false;
-
-        for(int i=1; i<n; i++) {
-
-            swap = solve(s1.substr(0,i),s2.substr(n-i,i),mp) && solve(s1.substr(i,n-i),s2.substr(0,n-i),mp);
-            noswap = solve(s1.substr(0,i),s2.substr(0,i),mp) && solve(s1.substr(i,n-i),s2.substr(i,n-i),mp);
-            flag = swap||noswap;
-            if(flag) {
-                mp[s1+s2] = true;
-                return true;
-            }
+            // no_swap(x, y) --> len(x1) == len(x2) && len(y1) == len(y2)
+            x2 = s2.substr(0, i);
+            y2 = s2.substr(i);
+            bool no_swap = solve(x1, x2, mp) && solve(y1, y2, mp);
+            if(swap || no_swap) return mp[s1 + "#" + s2] = true;
         }
-        mp[s1+s2] = false;
-        return false;
+        return mp[s1 + "#" + s2] = false;
     }
 
     bool isScramble(string s1, string s2) {
-        unordered_map<string,bool> mp;
-        return solve(s1,s2,mp);
+        unordered_map<string, bool> mp;
+        return solve(s1, s2, mp);
     }
 };

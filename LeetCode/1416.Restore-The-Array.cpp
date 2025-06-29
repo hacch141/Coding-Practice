98e3915f12d3d2829b002c837a56a9c4d0a3906a
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-06-29 17:49:41 +0530

    Update 1416.Restore-The-Array.cpp

--- LeetCode/1416.Restore-The-Array.cpp
+++ LeetCode/1416.Restore-The-Array.cpp
@@ -2,30 +2,27 @@
 
 class Solution {
 public:
+    int MOD = 1e9 + 7;
 
-    int mod = 1e9+7;
+    int solve(int ind, int n, string& s, int k, vector<int>& dp) {
+        if(ind == n) return 1;
+        if(s[ind] == '0') return 0;
 
-    int helper(string& s, int& k, int i, vector<int>& dp) {
-        if(s[i]=='0') return 0;
-        if(i==s.size()) return 1;
+        if(dp[ind] != -1) return dp[ind];
 
-        if(dp[i] != -1) return dp[i];
-
-        int n = s.length();
-        int ans = 0;
-        long num = 0;
-        for(int j=i; j<n; j++) {
-            num = num*10 + s[j]-'0';
-            if(num>k) break;
-            ans = (ans + helper(s,k,j+1,dp))%mod;
+        int cnt = 0, curr_ind = ind;
+        long long curr = 0;
+        for(int i = ind; i < n; i++) {
+            curr = (curr * 10) + (s[i] - '0');
+            if(curr > k) break;
+            cnt = (cnt + solve(i + 1, n, s, k, dp)) % MOD;
         }
-
-        return dp[i] = ans;
+        return dp[ind] = cnt;
     }
 
     int numberOfArrays(string s, int k) {
         int n = s.length();
-        vector<int> dp(n,-1);
-        return helper(s,k,0,dp);
+        vector<int> dp(n, -1);
+        return solve(0, n, s, k, dp);
     }
 };

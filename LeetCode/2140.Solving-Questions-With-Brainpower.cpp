edb607c898d89a5a3598358150e877263e6b7471
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-06-29 19:12:05 +0530

    Update 2140.Solving-Questions-With-Brainpower.cpp

--- LeetCode/2140.Solving-Questions-With-Brainpower.cpp
+++ LeetCode/2140.Solving-Questions-With-Brainpower.cpp
@@ -2,20 +2,35 @@
 
 class Solution {
 public:
+    long long solve(int ind, int n, vector<vector<int>>& questions, vector<long long>& dp) {
+        if(ind >= n) return 0;
 
-    long long solve(int i, vector<vector<int>>& questions, int ans, vector<long long>& dp, int &n) {
-        if(i >= n) {
-            return 0;
-        }
-        if(dp[i] != -1) return dp[i];
-        long long take = ans + questions[i][0] + solve(i+questions[i][1]+1,questions,ans,dp,n);
-        long long nottake = ans + solve(i+1,questions,ans,dp,n);
-        return dp[i] = max(take,nottake);
+        if(dp[ind] != -1) return dp[ind];
+
+        long long not_take = solve(ind + 1, n, questions, dp);
+        long long take = questions[ind][0] + solve(ind + questions[ind][1] + 1, n, questions, dp);
+
+        return dp[ind] = max(take, not_take);
     }
 
     long long mostPoints(vector<vector<int>>& questions) {
         int n = questions.size();
-        vector<long long> dp(n,-1);
-        return solve(0,questions,0,dp,n);
+        vector<long long> dp(n, -1);
+        return solve(0, n, questions, dp);
+    }
+};
+
+class Solution {
+public:
+    long long mostPoints(vector<vector<int>>& questions) {
+        int n = questions.size();
+        vector<long long> dp(n + 1, 0);
+        for(int i = n - 1; i >= 0; i--) {
+            int nxt = i + questions[i][1] + 1;
+            long long take = questions[i][0] + (nxt < n ? dp[nxt] : 0);
+            long long not_take = dp[i + 1];
+            dp[i] = max(take, not_take);
+        }
+        return dp[0];
     }
 };

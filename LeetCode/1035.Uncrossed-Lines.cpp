c1b9788f43e77126b087f8f86708ef8e200bf4a7
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-06-29 18:48:02 +0530

    Update 1035.Uncrossed-Lines.cpp

--- LeetCode/1035.Uncrossed-Lines.cpp
+++ LeetCode/1035.Uncrossed-Lines.cpp
@@ -2,21 +2,22 @@
 
 class Solution {
 public:
-    int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
-        int n1 = nums1.size();
-        int n2 = nums2.size();
-        vector<int> dp(n2+1,0), curr(n2+1,0);
+    int solve(int ind1, int n1, int ind2, int n2, vector<int>& nums1, vector<int>& nums2, vector<vector<int>>& dp) {
+        if(ind1 == n1 || ind2 == n2) return 0;
 
-        for(int i=1; i<=n1; i++) {
-            for(int j=1; j<=n2; j++) {
-                if(nums1[i-1] == nums2[j-1]) {
-                    curr[j] = 1 + dp[j-1];
-                } else {
-                    curr[j] = max(dp[j],curr[j-1]);
-                }
-            }
-            dp = curr;
-        }
-        return dp[n2];
+        if(dp[ind1][ind2] != -1) return dp[ind1][ind2];
+
+        if(nums1[ind1] == nums2[ind2]) return dp[ind1][ind2] = 1 + solve(ind1 + 1, n1, ind2 + 1, n2, nums1, nums2, dp);
+
+        int left = solve(ind1 + 1, n1, ind2, n2, nums1, nums2, dp);
+        int right = solve(ind1, n1, ind2 + 1, n2, nums1, nums2, dp);
+
+        return dp[ind1][ind2] = max(left, right);
+    }
+
+    int maxUncrossedLines(vector<int>& nums1, vector<int>& nums2) {
+        int n1 = nums1.size(), n2 = nums2.size();
+        vector<vector<int>> dp(n1, vector<int>(n2, -1));
+        return solve(0, n1, 0, n2, nums1, nums2, dp);
     }
 };

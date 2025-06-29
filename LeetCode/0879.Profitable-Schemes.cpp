75512262affaa7218a127f72a6462b24c091f5c9
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-06-29 17:20:42 +0530

    Update 0879.Profitable-Schemes.cpp

--- LeetCode/0879.Profitable-Schemes.cpp
+++ LeetCode/0879.Profitable-Schemes.cpp
@@ -22,31 +22,3 @@ public:
         return solve(0, group, profit, n, 0, minProfit, dp);
     }
 };
-
-// 
-
-class Solution {
-public:
-
-    int mod = 1e9+7;
-    int dp[102][102][102];
-
-    int helper(int i, int n, int currProfit, int& minProfit, vector<int>& group, vector<int>& profit) {
-
-        if(i==group.size() && n>=0 && currProfit == minProfit) return 1;
-        if(n<0 || i==group.size()) return 0;
-
-        if(dp[i][n][currProfit] != -1) return dp[i][n][currProfit];
-
-        int take=0, notTake=0;
-        notTake = helper(i+1,n,currProfit,minProfit,group,profit)%mod;
-        take = helper(i+1,n-group[i],min(currProfit+profit[i],minProfit),minProfit,group,profit)%mod;
-
-        return dp[i][n][currProfit] = (take+notTake)%mod;
-    }
-
-    int profitableSchemes(int n, int minProfit, vector<int>& group, vector<int>& profit) {
-        memset(dp,-1,sizeof(dp));
-        return helper(0,n,0,minProfit,group,profit);
-    }
-};

95e49daa137cda401ceeb4adc286130b81901a0b
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-07-02 03:11:01 +0530

    Update 1140-Stone-Game-II.cpp

--- LeetCode/1140-Stone-Game-II.cpp
+++ LeetCode/1140-Stone-Game-II.cpp
@@ -1,27 +1,31 @@
 // 1140. Stone Game II
+
 class Solution {
 public:
-
-    int solve(int ind, int& n, int person, int m, vector<int>& piles, vector<vector<vector<int>>>& dp) {
+    int solve(int ind, int M, bool alice, int n, vector<int>& piles, vector<vector<vector<int>>>& dp) {
         if(ind >= n) return 0;
-        if(dp[ind][m][person] != -1) return dp[ind][m][person];
-        int sum = 0, ans = (person == 1 ? 0 : 1e9);
-        for(int i = 0; i < 2 * m; i++) {
-            if(i + ind >= n) break;
-            sum += piles[i + ind];
-            if(person == 1) {
-                ans = max(ans, sum + solve(ind + i + 1, n, 0, max(m, i + 1), piles, dp));
+
+        if(dp[ind][M][alice] != -1) return dp[ind][M][alice];
+
+        int curr_stones = 0, ans = alice ? 0 : 1e9;
+        for(int i = 0; i < 2 * M; i++) {
+            if(ind + i >= n) break;
+            curr_stones += piles[ind + i];
+            if(alice) {
+                // take max bcz alice play optimally
+                ans = max(ans, curr_stones + solve(ind + i + 1, max(i + 1, M), !alice, n, piles, dp));
             }
             else {
-                ans = min(ans, solve(ind + i + 1, n, 1, max(m, i + 1), piles, dp));
+                // take min bcz bob play optimally
+                ans = min(ans, solve(ind + i + 1, max(i + 1, M), !alice, n, piles, dp));
             }
         }
-        return dp[ind][m][person] = ans;
+        return dp[ind][M][alice] = ans;
     }
 
     int stoneGameII(vector<int>& piles) {
         int n = piles.size();
-        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(n + 1, vector<int>(2, -1)));
-        return solve(0, n, 1, 1, piles, dp);
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>((2 * n) + 1, vector<int>(2, -1)));
+        return solve(0, 1, true, n, piles, dp);
     }
 };

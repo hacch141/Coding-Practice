f19c09d9d7cd8e767e7a318f501db25bdfac7242
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-07-02 04:16:57 +0530

    Update 1547.Minimum-Cost-to-Cut-a-Stick.cpp

--- LeetCode/1547.Minimum-Cost-to-Cut-a-Stick.cpp
+++ LeetCode/1547.Minimum-Cost-to-Cut-a-Stick.cpp
@@ -18,3 +18,29 @@ public:
         return solve(0, n, cuts, mp);
     }
 };
+
+// OR
+
+class Solution {Add commentMore actions
+private:
+    int solve(int i, int j, vector<int>& cuts, vector<vector<int>>& dp) {
+        if(i > j) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+        int mini = INT_MAX;
+        for(int k=i; k<=j; k++) {
+            int cost = cuts[j+1]-cuts[i-1] + solve(i,k-1,cuts,dp) + solve(k+1,j,cuts,dp);
+            mini = min(mini,cost);
+        }
+        return dp[i][j] = mini;
+    }
+
+public:
+    int minCost(int n, vector<int>& cuts) {
+        int c = cuts.size();
+        cuts.push_back(n);
+        cuts.push_back(0);
+        sort(cuts.begin(),cuts.end());
+        vector<vector<int>> dp(c+1, vector<int> (c+1,-1));
+        return solve(1,c,cuts,dp);
+    }
+};

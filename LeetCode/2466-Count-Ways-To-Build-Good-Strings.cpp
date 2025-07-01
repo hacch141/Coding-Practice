4292ea6cb7d83a87e38cfca4d6be2aa5e090bfdb
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-07-02 01:50:27 +0530

    Update 2466-Count-Ways-To-Build-Good-Strings.cpp

--- LeetCode/2466-Count-Ways-To-Build-Good-Strings.cpp
+++ LeetCode/2466-Count-Ways-To-Build-Good-Strings.cpp
@@ -24,3 +24,21 @@ public:
         return solve(0, low, high, zero, one, dp);
     }
 };
+
+// Iterative
+class Solution {
+public:
+    int MOD = 1e9 + 7;
+
+    int countGoodStrings(int low, int high, int zero, int one) {
+        vector<int> dp(high + 1, 0);
+        dp[0] = 1;
+        int ans = 0;
+        for(int i = 1; i <= high; i++) {
+            if(i - zero >= 0) dp[i] = (dp[i] + dp[i - zero]) % MOD;
+            if(i - one >= 0) dp[i] = (dp[i] + dp[i - one]) % MOD;
+            if(i >= low) ans = (ans + dp[i]) % MOD;
+        }
+        return ans;
+    }
+};

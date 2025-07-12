c4dc961c46207ea7a6426e1540e903898baaafd9
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-07-12 18:44:17 +0530

    Update 0139-Word-Break.cpp

--- LeetCode/0139-Word-Break.cpp
+++ LeetCode/0139-Word-Break.cpp
@@ -2,27 +2,24 @@
 
 class Solution {
 public:
-    bool solve(int ind, int n, string curr, string& s, unordered_set<string>& st, map<pair<int,string>, int>& mp) {
-        if(ind == n) {
-            return curr == "";
-        }
+    bool solve(int ind, int n, string& s, unordered_set<string>& st, vector<int>& dp) {
+        if(ind == n) return true;
 
-        if(mp.count({ind, curr})) return mp[{ind, curr}];
+        if(dp[ind] != -1) return dp[ind];
 
-        bool cut = false;
-        curr += s[ind];
-        if(st.count(curr)) {
-            cut = solve(ind + 1, n, "", s, st, mp);
+        string curr = "";
+        for(int i = ind; i < n; i++) {
+            curr += s[i];
+            if(st.count(curr) && solve(i + 1, n, s, st, dp)) return dp[ind] = true;
         }
-        bool notcut = solve(ind + 1, n, curr, s, st, mp);
-        return mp[{ind, curr}] = cut || notcut;
+        return dp[ind] = false;
     }
 
     bool wordBreak(string s, vector<string>& wordDict) {
         int n = s.length();
         unordered_set<string> st;
-        map<pair<int,string>, int> mp;
         for(auto s : wordDict) st.insert(s);
-        return solve(0, n, "", s, st, mp);
+        vector<int> dp(n, -1);
+        return solve(0, n, s, st, dp);
     }
 };

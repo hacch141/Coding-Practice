71e6df43faafb3603808aecea979e8cbc17376b1
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-07-13 17:11:35 +0530

    Update 0343.Integer-Break.cpp

--- LeetCode/0343.Integer-Break.cpp
+++ LeetCode/0343.Integer-Break.cpp
@@ -2,34 +2,42 @@
 
 class Solution {
 public:
+    int solve(int n, bool first, vector<vector<int>>& dp) {
+        if(n <= 1) return 1;
 
-    int solve(int ind, int sum, int n, vector<int>& nums, unordered_map<string,int>& mp) {
-        if(ind == n-1 || sum == n) {
-            return 1;
+        if(dp[n][first] != -1) return dp[n][first];
+
+        int mx = 0;
+        for(int i = 1; i < n; i++) {
+            mx = max(mx, i * (solve(n - i, false, dp)));
         }
+        if(!first) mx = max(mx, n);
 
-        string s = to_string(ind) + "#" + to_string(sum);
-        if(mp.find(s) != mp.end()) {
-            return mp[s];
-        }
-
-        int take = INT_MIN;
-        if(sum+nums[ind] <= n) {
-            take = nums[ind] * solve(ind , sum+nums[ind] , n , nums , mp);
-        }
-
-        int nottake = solve(ind+1 , sum , n , nums , mp);
-
-        return mp[s] = max(take , nottake);
+        return dp[n][first] = mx;
     }
 
     int integerBreak(int n) {
-        vector<int> nums(n-1,0);
-        for(int i=0; i<n-1; i++) {
-            nums[i] = i+1;
-        }
+        vector<vector<int>> dp(n + 1, vector<int>(2, -1));
+        return solve(n, true, dp);
+    }
+};
 
-        unordered_map<string,int> mp;
-        return solve(0,0,n,nums,mp);
+// Math
+class Solution {
+public:
+    int integerBreak(int n) {
+        if(n == 2) return 1;
+        if(n == 3) return 2;
+
+        int cnt = n / 3;
+        if(n % 3 == 1) {
+            // use all (cnt-1) 3s and one 4
+            return pow(3, cnt - 1) * 4;
+        }
+        else if(n % 3 == 2) {
+            // use all cnt 3s and one 2
+            return pow(3, cnt) * 2;
+        }
+        return pow(3, cnt);
     }
 };

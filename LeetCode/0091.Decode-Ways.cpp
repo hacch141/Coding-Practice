eab2256909ce5d63e428286a5ca66bc85c52103e
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-07-14 01:52:13 +0530

    Update 0091.Decode-Ways.cpp

--- LeetCode/0091.Decode-Ways.cpp
+++ LeetCode/0091.Decode-Ways.cpp
@@ -2,23 +2,29 @@
 
 class Solution {
 public:
-
-    int solve(int ind, int& n, string& s, vector<int>& dp) {
-        if(ind == s.length()) return 1;
+    int solve(int ind, int n, string& s, vector<int>& dp) {
         if(s[ind] == '0') return 0;
+        if(ind >= n) return 1;
 
         if(dp[ind] != -1) return dp[ind];
-        
-        int one = solve(ind+1, n, s, dp);
-        int two = 0;
-        if(ind < s.length() - 1 && s.substr(ind,2) <= "26") two = solve(ind+2, n, s, dp);
 
-        return dp[ind] = one + two;
+        int cnt = 0;
+        // for single digit
+        int curr = s[ind] - '0';
+        cnt = (cnt + solve(ind + 1, n, s, dp));
+
+        // for double digit
+        if(ind + 1 < n) {
+            curr = (10 * curr) + (s[ind + 1] - '0');
+            if(curr <= 26) cnt = (cnt + solve(ind + 2, n, s, dp));
+        }
+
+        return dp[ind] = cnt;
     }
 
     int numDecodings(string s) {
         int n = s.length();
-        vector<int> dp(n,-1);
+        vector<int> dp(n, -1);
         return solve(0, n, s, dp);
     }
 };

0f2e83b501adcc6319ec3c52fb96451987bc7351
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-07-13 16:49:43 +0530

    Update 0799-Champagne-Tower.cpp

--- LeetCode/0799-Champagne-Tower.cpp
+++ LeetCode/0799-Champagne-Tower.cpp
@@ -16,3 +16,23 @@ public:
         return min(1.0, dp[query_row][query_glass]);
     }
 };
+
+class Solution {
+public:
+    double solve(int poured, int i, int j, vector<vector<double>>& dp) {
+        if(i < 0 || j < 0) return 0;
+        if(i == 0 && j == 0) return poured;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        double l = (solve(poured, i - 1, j - 1, dp) - 1) / 2;
+        double r = (solve(poured, i - 1, j, dp) - 1) / 2;
+
+        return dp[i][j] = max(0.0, l) + max(0.0, r);
+    }
+
+    double champagneTower(int poured, int query_row, int query_glass) {
+        vector<vector<double>> dp(query_row + 1, vector<double>(query_glass + 1, -1));
+        return min(1.0, solve(poured, query_row, query_glass, dp));
+    }
+};

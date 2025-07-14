9b569406f0bd87593f619376e8caae7a44e73ddc
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-07-14 21:06:48 +0530

    Update 1290.Convert-Binary-Number-in-a-Linked-List-to-Integer.cpp

--- LeetCode/1290.Convert-Binary-Number-in-a-Linked-List-to-Integer.cpp
+++ LeetCode/1290.Convert-Binary-Number-in-a-Linked-List-to-Integer.cpp
@@ -1,39 +1,26 @@
 // 1290. Convert Binary Number in a Linked List to Integer
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
-
-    void reverse(ListNode*& head) {
-        ListNode* prev = NULL;
+    ListNode* get_reverse(ListNode* head) {
         ListNode* curr = head;
-        ListNode* next;
+        ListNode* prev = NULL;
         while(curr) {
-            next = curr->next;
+            ListNode* next = curr->next;
             curr->next = prev;
             prev = curr;
             curr = next;
         }
-        head = prev;
+        return prev;
     }
 
     int getDecimalValue(ListNode* head) {
-        reverse(head);
-        int bit = 1;
-        int num = 0;
-        while(head) {
-            if(head->val) num += bit;
-            bit = bit << 1;
-            head = head->next;
+        ListNode* rev = get_reverse(head);
+        int num = 0, bit = 0;
+        while(rev) {
+            if(rev->val) num |= (1 << bit);
+            rev = rev->next;
+            bit++;
         }
         return num;
     }

39ed19ea0baba88a3f088f7c21a056425cd5fe57
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-07-17 02:21:14 +0530

    Update 0647.Palindromic-Substrings.cpp

--- LeetCode/0647.Palindromic-Substrings.cpp
+++ LeetCode/0647.Palindromic-Substrings.cpp
@@ -1,7 +1,6 @@
 // 647. Palindromic Substrings
 class Solution {
 public:
-
     int check(int l, int r, string& s) {
         int cnt = 0;
         while(l >= 0 && r < s.length() && s[l] == s[r]) {
@@ -22,61 +21,44 @@ public:
     }
 };
 
-
-
+// Rec
 class Solution {
 public:
+    bool solve(int i, int j, int n, string& s, vector<vector<int>>& dp) {
+        if(i >= j) return true;
+        if(s[i] != s[j]) return false;
+        if(dp[i][j] != -1) return dp[i][j];
+        return dp[i][j] = solve(i + 1, j - 1, n, s, dp);
+    }
 
     int countSubstrings(string s) {
-        int n = s.length(), ans = 0;
-        vector<vector<bool>> dp(n, vector<bool>(n, -1));
-        for(int L = 1; L <= n; L++) {
-            for(int l = 0; l + L - 1 < n; l++) {
-                int r = l + L - 1;
-                if(l == r) {
-                    dp[l][r] = true;
-                }
-                else if(l + 1 == r) {
-                    dp[l][r] = s[l] == s[r];
-                }
-                else {
-                    dp[l][r] = (s[l] == s[r] && dp[l + 1][r - 1]);
-                }
-                ans += dp[l][r];
+        int n = s.length();
+        int ans = 0;
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+        for(int i = 0; i < n; i++) {
+            for(int j = i; j < n; j++) {
+                ans += solve(i, j, n, s, dp);
             }
         }
         return ans;
     }
 };
 
-
-
-
+// Bottom Up
 class Solution {
 public:
-
-    vector<vector<int>> dp;
-
-    Solution() {
-        dp.resize(1001, vector<int>(1001, -1));
-    }
-
-    bool ok(int l, int r, string& s) {
-        if(l >= r) return true;
-        if(dp[l][r] != -1) return dp[l][r];
-        if(s[l] != s[r]) return dp[l][r] = false;
-        return dp[l][r] = ok(l + 1, r - 1, s);
-    }
-
     int countSubstrings(string s) {
-        int n = s.length(), ans = 0;
-
-        for(int i = 0; i < n; i++) {
-            for(int j = i; j < n; j++) {
-                if(ok(i, j, s)) ans++;
+        int n = s.length();
+        int ans = 0;
+        vector<vector<bool>> dp(n, vector<bool>(n, false));
+        for(int r = 0; r < n; r++) {
+            for(int l = 0; l <= r; l++) {
+                if(l == r) dp[l][r] = true;
+                else if(l + 1 == r) dp[l][r] = (s[l] == s[r]);
+                else dp[l][r] = (s[l] == s[r] && dp[l + 1][r - 1]);
+                ans += dp[l][r];
             }
         }
-
         return ans;
     }
 };

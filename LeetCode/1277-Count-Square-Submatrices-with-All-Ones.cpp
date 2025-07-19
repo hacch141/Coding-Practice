7473f19f76f064ce1a5983d3409e711fec6880f0
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-07-20 03:30:17 +0530

    Update 1277-Count-Square-Submatrices-with-All-Ones.cpp

--- LeetCode/1277-Count-Square-Submatrices-with-All-Ones.cpp
+++ LeetCode/1277-Count-Square-Submatrices-with-All-Ones.cpp
@@ -1,5 +1,27 @@
 // 1277. Count Square Submatrices with All Ones
 
+// DP
+class Solution {
+public:
+    int countSquares(vector<vector<int>>& matrix) {
+        int n = matrix.size(), m = matrix[0].size();
+        int ans = 0;
+        vector<vector<int>> dp(n, vector<int>(m, 0));
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(i == 0 || j == 0) {
+                    dp[i][j] = matrix[i][j];
+                }
+                else if(matrix[i][j]){
+                    dp[i][j] = 1 + min({dp[i - 1][j], dp[i - 1][j - 1], dp[i][j - 1]});
+                }
+                ans += dp[i][j];
+            }
+        }
+        return ans;
+    }
+};
+
 // Rec
 class Solution {
 public:

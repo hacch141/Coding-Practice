c6f632880e773893deb24699198a67424be35e2d
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-07-20 02:22:52 +0530

    Update 1937-Maximum-Number-of-Points-with-Cost.cpp

--- LeetCode/1937-Maximum-Number-of-Points-with-Cost.cpp
+++ LeetCode/1937-Maximum-Number-of-Points-with-Cost.cpp
@@ -1,65 +1,28 @@
 // 1937. Maximum Number of Points with Cost
+
 class Solution {
 public:
     long long maxPoints(vector<vector<int>>& points) {
         int n = points.size(), m = points[0].size();
-
-        vector<long long> prev(m);
-        for(int i = 0; i < m; i++) prev[i] = (long long)points[0][i];
-
-        long long ans = 0;
+        vector<vector<long long>> dp(n, vector<long long>(m, INT_MIN));
+        for(int j = 0; j < m; j++) dp[0][j] = points[0][j];
         for(int i = 1; i < n; i++) {
-            vector<long long> curr(m, 0);
-            for(long j = 0; j < m; j++) {
-                for(int k = 0; k < m; k++) {
-                    curr[j] = max(curr[j], points[i][j] + prev[k] - abs(j - k));
-                }
-            }
-            prev = curr;
-        }
-
-        for(int i = 0; i < m; i++) ans = max(ans, prev[i]);
-        
-        return ans;
-    }
-};
-// TC : (n * m * m)
-
-
-// Optimised
-class Solution {
-public:
-    long long maxPoints(vector<vector<int>>& points) {
-        int n = points.size(), m = points[0].size();
-
-        vector<long long> prev(m);
-        for(int i = 0; i < m; i++) prev[i] = (long long)points[0][i];
-
-        long long ans = 0;
-        for(int i = 1; i < n; i++) {
-            vector<long long> curr(m, 0), leftMax(m, 0), rightMax(m, 0);
-
-            leftMax[0] = prev[0];
+            vector<long long> l_mx(m);
+            l_mx[0] = dp[i - 1][0];
             for(int j = 1; j < m; j++) {
-                leftMax[j] = max(leftMax[j - 1] - 1 , prev[j]);
+                l_mx[j] = max(l_mx[j - 1] - 1, dp[i - 1][j]);
             }
 
-            rightMax[m - 1] = prev[m - 1];
+            vector<long long> r_mx(m);
+            r_mx[m - 1] = dp[i - 1][m - 1];
             for(int j = m - 2; j >= 0; j--) {
-                rightMax[j] = max(rightMax[j + 1] - 1 , prev[j]);
+                r_mx[j] = max(r_mx[j + 1] - 1, dp[i - 1][j]);
             }
 
-            for(long j = 0; j < m; j++) {
-                curr[j] = points[i][j] + max(leftMax[j], rightMax[j]);
+            for(int j = 0; j < m; j++) {
+                dp[i][j] = points[i][j] + max(l_mx[j], r_mx[j]);
             }
-            prev = curr;
         }
-
-        for(int i = 0; i < m; i++) ans = max(ans, prev[i]);
-
-        return ans;
+        return *max_element(dp[n - 1].begin(), dp[n - 1].end());
     }
 };
-// TC : (n * m)
-
-

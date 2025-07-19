4ede9f55a18ddc7a2ffd512608153aa6ceff708d
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-07-20 01:25:17 +0530

    Update 1105-Filling-Bookcase-Shelves.cpp

--- LeetCode/1105-Filling-Bookcase-Shelves.cpp
+++ LeetCode/1105-Filling-Bookcase-Shelves.cpp
@@ -1,16 +1,19 @@
 // 1105. Filling Bookcase Shelves
+
 class Solution {
 public:
-
-    int solve(int ind, int& n, vector<vector<int>>& books, int shelfWidth, vector<int>& dp) {
+    int solve(int ind, int n, vector<vector<int>>& books, int shelfWidth, vector<int>& dp) {
         if(ind == n) return 0;
+
         if(dp[ind] != -1) return dp[ind];
-        int currwidth = 0, mx = 0, ans = INT_MAX;
+
+        int mx_height = 0, curr_width = 0;
+        int ans = INT_MAX;
         for(int i = ind; i < n; i++) {
-            currwidth += books[i][0];
-            mx = max(mx, books[i][1]);
-            if(currwidth > shelfWidth) break;
-            ans = min(ans, mx + solve(i + 1, n, books, shelfWidth, dp));
+            mx_height = max(mx_height, books[i][1]);
+            curr_width += books[i][0];
+            if(curr_width > shelfWidth) break;
+            ans = min(ans, mx_height + solve(i + 1, n, books, shelfWidth, dp));
         }
         return dp[ind] = ans;
     }

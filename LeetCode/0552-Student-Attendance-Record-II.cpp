f2bd121b9698bccbbee9ebd40816d904324e8bc6
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-07-20 00:50:14 +0530

    Update 0552-Student-Attendance-Record-II.cpp

--- LeetCode/0552-Student-Attendance-Record-II.cpp
+++ LeetCode/0552-Student-Attendance-Record-II.cpp
@@ -2,23 +2,31 @@
 
 class Solution {
 public:
+    int MOD = 1e9 + 7;
 
-    int mod = 1e9 + 7;
-
-    int solve(int ind, int& n, bool isAb, int cL, vector<vector<vector<int>>>& dp) {
+    int solve(int ind, int n, bool prev_abs, int cons_late, vector<vector<vector<int>>>& dp) {
         if(ind == n) return 1;
 
-        if(dp[ind][isAb][cL] != -1) return dp[ind][isAb][cL];
+        if(dp[ind][prev_abs][cons_late] != -1) return dp[ind][prev_abs][cons_late];
 
-        int ans = solve(ind + 1, n, isAb, 0, dp) % mod;
-        if(!isAb) ans = (ans + solve(ind + 1, n, true, 0, dp)) % mod;
-        if(cL < 2) ans = (ans + solve(ind + 1, n, isAb, cL + 1, dp)) % mod;
+        // present
+        int cnt = solve(ind + 1, n, prev_abs, 0, dp) % MOD;
 
-        return dp[ind][isAb][cL] = ans;
+        // The student was absent ('A') for strictly fewer than 2 days total
+        if(!prev_abs) {
+            cnt = (cnt + solve(ind + 1, n, true, 0, dp)) % MOD;
+        }
+
+        // The student was never late ('L') for 3 or more consecutive days.
+        if(cons_late < 2) {
+            cnt = (cnt + solve(ind + 1, n, prev_abs, cons_late + 1, dp)) % MOD;
+        }
+
+        return dp[ind][prev_abs][cons_late] = cnt;
     }
 
     int checkRecord(int n) {
-        vector<vector<vector<int>>> dp(n + 1, vector<vector<int>>(2, vector<int>(3, -1)));
+        vector<vector<vector<int>>> dp(n, vector<vector<int>>(2, vector<int>(3, -1)));
         return solve(0, n, false, 0, dp);
     }
 };

e9682c27dca97cefdc44c77c5c19b2178ad9671e
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-07-20 03:25:37 +0530

    Update 1277-Count-Square-Submatrices-with-All-Ones.cpp

--- LeetCode/1277-Count-Square-Submatrices-with-All-Ones.cpp
+++ LeetCode/1277-Count-Square-Submatrices-with-All-Ones.cpp
@@ -1,5 +1,35 @@
 // 1277. Count Square Submatrices with All Ones
 
+// Rec
+class Solution {
+public:
+    int solve(int i, int j, int n, int m, vector<vector<int>>& matrix, vector<vector<int>>& dp) {
+        if(i >= n || j >= m) return 0;
+        if(matrix[i][j] == 0) return 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int right = solve(i, j + 1, n, m, matrix, dp);
+        int diagonal = solve(i + 1, j + 1, n, m, matrix, dp);
+        int bottom = solve(i + 1, j, n, m, matrix, dp);
+
+        return dp[i][j] = 1 + min({right, diagonal, bottom});
+    }
+
+    int countSquares(vector<vector<int>>& matrix) {
+        int n = matrix.size(), m = matrix[0].size();
+        int ans = 0;
+        vector<vector<int>> dp(n, vector<int>(m, -1));
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                ans += solve(i, j, n, m, matrix, dp);
+            }
+        }
+        return ans;
+    }
+};
+
+// Pre calculate the area and use it
 class Solution {
 public:
 

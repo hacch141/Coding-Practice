6bfb951fc969f0c7fe45c574740d70ae07b185ba
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-07-20 02:56:31 +0530

    Update 0650-2-Keys-Keyboard.cpp

--- LeetCode/0650-2-Keys-Keyboard.cpp
+++ LeetCode/0650-2-Keys-Keyboard.cpp
@@ -1,5 +1,29 @@
 // 650. 2 Keys Keyboard
 
+// rec
+class Solution {
+public:
+    int solve(int ind, int n, int last_copy, vector<vector<int>>& dp) {
+        if(ind == n) return 0;
+        if(ind > n) return 1e9;
+
+        if(dp[ind][last_copy] != -1) return dp[ind][last_copy];
+
+        int copy = INT_MAX;
+        if(ind != last_copy) copy = 1 + solve(ind, n, ind, dp);
+        int paste = INT_MAX;
+        if(last_copy > 0) paste = 1 + solve(ind + last_copy, n, last_copy, dp);
+
+        return dp[ind][last_copy] = min(copy, paste);
+    }
+
+    int minSteps(int n) {
+        vector<vector<int>> dp(n, vector<int>(n, -1));
+        return solve(1, n, 0, dp);
+    }
+};
+
+// Math
 class Solution {
 public:
     int minSteps(int n) {

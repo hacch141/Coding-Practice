1a5ced07f86a4d3c5d5223e6f68062c6112b26a0
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-07-24 01:17:53 +0530

    Update 0416.Partition-Equal-Subset-Sum.cpp

--- LeetCode/0416.Partition-Equal-Subset-Sum.cpp
+++ LeetCode/0416.Partition-Equal-Subset-Sum.cpp
@@ -1,27 +1,49 @@
 // 416. Partition Equal Subset Sum
 
+// DP
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
-        int k = 0;
         int n = nums.size();
-        for(auto i : nums) k += i;
-        // if total sum is odd then it's not possible 
-        if(k%2 == 1) return false;
-        vector<bool> dp(k+1,0), curr(k+1,0);
-        dp[0] = curr[0] = true;
-        if(nums[0] <= k) dp[nums[0]] = true;
-        for(int i=1; i<n; i++) {
-            for(int target = 1; target<=k; target++) {
-                bool notpick = dp[target];
-                bool pick = false;
-                if(nums[i] <= target) pick = dp[target-nums[i]];
-                curr[target] = notpick | pick;
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        if(sum & 1) return false;
+        int target = sum / 2;
+
+        vector<vector<bool>> dp(n + 1, vector<bool>(target + 1, false));
+        dp[0][0] = true;
+        for(int i = 1; i <= n; i++) {
+            for(int j = 0; j <= target; j++) {
+                // nottale
+                dp[i][j] = dp[i][j] | dp[i - 1][j];
+                // take
+                if(nums[i - 1] <= j) {
+                    dp[i][j] = dp[i][j] | dp[i - 1][j - nums[i - 1]];
+                }
             }
-            dp = curr;
         }
-        // if k/2 is possible then (k-(k/2)) is also possible 
-        // Hence there are two equal subset sum
-        return dp[k/2];
+        return dp[n][target];
+    }
+};
+
+// Rec
+class Solution {
+public:
+    bool rec(int ind, int& n, int target, vector<int>& nums, vector<vector<int>>& dp) {
+        if(target < 0) return false;
+        if(ind == n) return target == 0;
+        if(dp[ind][target] != -1) return dp[ind][target];
+        bool f = false;
+        f |= rec(ind + 1, n, target - nums[ind], nums, dp);
+        f |= rec(ind + 1, n, target, nums, dp);
+        return dp[ind][target] = f;
+    }
+
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size(), sum = 0;
+        for(auto &i : nums) sum += i;
+        if(sum & 1) return false;
+        int target = sum / 2;
+        vector<vector<int>> dp(n, vector<int>(target + 1, -1));
+        return rec(0, n, target, nums, dp);
     }
 };

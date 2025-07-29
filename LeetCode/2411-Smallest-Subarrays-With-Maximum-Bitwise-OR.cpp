b1944de0cd3d8b5eccf1da6f610345e194dd9216
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-07-29 22:00:09 +0530

    Create 2411-Smallest-Subarrays-With-Maximum-Bitwise-OR.cpp

---
+++ LeetCode/2411-Smallest-Subarrays-With-Maximum-Bitwise-OR.cpp
@@ -0,0 +1,40 @@
+// 2411. Smallest Subarrays With Maximum Bitwise OR
+
+class Solution {
+public:
+    vector<int> smallestSubarrays(vector<int>& nums) {
+        int n = nums.size();
+
+        int curr_xor = 0;
+        vector<int> mx_xor(n);
+        for(int i = n - 1; i >= 0; i--) {
+            curr_xor |= nums[i];
+            mx_xor[i] = curr_xor;
+        }
+
+        int l = 0, curr = 0;
+        vector<int> bits(32, 0), ans;
+        for(int r = 0; r < n; r++) {
+            int num = nums[r];
+            for(int i = 0; i < 32; i++) {
+                if((num >> i) & 1) {
+                    bits[i]++;
+                    if(bits[i] == 1) curr |= (1 << i);
+                }
+            }
+            while(l <= r && curr == mx_xor[l]) {
+                ans.push_back(r - l + 1);
+                int num = nums[l];
+                for(int i = 0; i < 32; i++) {
+                    if((num >> i) & 1) {
+                        bits[i]--;
+                        if(bits[i] == 0) curr -= (1 << i);
+                    }
+                }
+                l++;
+            }
+        }
+
+        return ans;
+    }
+};

a29773e40c542112d3e6fa4abd2f11436e4da2ac
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-07-29 22:47:12 +0530

    Update 2411-Smallest-Subarrays-With-Maximum-Bitwise-OR.cpp

--- LeetCode/2411-Smallest-Subarrays-With-Maximum-Bitwise-OR.cpp
+++ LeetCode/2411-Smallest-Subarrays-With-Maximum-Bitwise-OR.cpp
@@ -38,3 +38,24 @@ public:
         return ans;
     }
 };
+
+// 
+class Solution {
+public:
+    vector<int> smallestSubarrays(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> last(30, -1), ans(n, 1);
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = 0; j < 30; ++j) {
+                if ((nums[i] >> j) & 1) {
+                    last[j] = i;
+                } else if (last[j] >= 0) {
+                    ans[i] = max(ans[i], last[j] - i + 1);
+                }
+                
+            }
+            
+        }
+        return ans;
+    }
+};

2eda4520770fec20a3155f0464560ed2162e0c71
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-09-03 23:18:59 +0530

    Update 3027.Find-the-Number-of-Ways-to-Place-People-II.cpp

--- LeetCode/3027.Find-the-Number-of-Ways-to-Place-People-II.cpp
+++ LeetCode/3027.Find-the-Number-of-Ways-to-Place-People-II.cpp
@@ -2,21 +2,20 @@
 
 class Solution {
 public:
-
-    static bool cmp(vector<int> a, vector<int> b) {
-        if(a[0] == b[0]) return a[1] >= b[1];
-        return a[0] < b[0];
+    static bool cmp(vector<int>& v1, vector<int>& v2) {
+        if(v1[1] == v2[1]) return v1[0] < v2[0];
+        return v1[1] > v2[1];
     }
 
     int numberOfPairs(vector<vector<int>>& points) {
+        int n = points.size(), ans = 0;
         sort(points.begin(), points.end(), cmp);
-        int mn, ans = 0, n = points.size();
         for(int i = 0; i < n; i++) {
-            mn = 1e9 + 1;
+            int mx = INT_MIN;
             for(int j = i - 1; j >= 0; j--) {
-                if(points[j][1] >= points[i][1] && points[j][1] < mn) {
-                    ans++;
-                    mn = points[j][1];
+                if(points[j][0] <= points[i][0]) {
+                    ans += points[j][0] > mx;
+                    mx = max(mx, points[j][0]);
                 }
             }
         }

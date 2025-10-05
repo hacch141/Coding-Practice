f486896b190e322e2af62b8a171f986ae66bbcf6
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2025-10-05 23:08:45 +0530

    Update 0417-Pacific-Atlantic-Water-Flow.java

--- LeetCode/0417-Pacific-Atlantic-Water-Flow.java
+++ LeetCode/0417-Pacific-Atlantic-Water-Flow.java
@@ -1,5 +1,53 @@
 // 417. Pacific Atlantic Water Flow
 
+class Solution {
+    int[] dx = {-1, 0, +1, 0};
+    int[] dy = {0, +1, 0, -1};
+
+    boolean isValid(int nx, int ny, int n, int m, boolean[][] vis) {
+        return nx >= 0 && nx < n && ny >= 0 && ny < m && !vis[nx][ny];
+    }
+
+    public void dfs(int x, int y, int n, int m, boolean[][] vis, int[][] heights) {
+        vis[x][y] = true;
+        for (int dir = 0; dir < 4; dir++) {
+            int nx = x + dx[dir];
+            int ny = y + dy[dir];
+            if (isValid(nx, ny, n, m, vis) && heights[nx][ny] >= heights[x][y]) {
+                dfs(nx, ny, n, m, vis, heights);
+            }
+        }
+    }
+
+    public List<List<Integer>> pacificAtlantic(int[][] heights) {
+        int n = heights.length, m = heights[0].length;
+        boolean[][] atlantic = new boolean[n][m];
+        boolean[][] pacific = new boolean[n][m];
+
+        for (int i = 0; i < n; i++) {
+            dfs(i, 0, n, m, pacific, heights);
+            dfs(i, m - 1, n, m, atlantic, heights);
+        }
+        for (int j = 0; j < m; j++) {
+            dfs(0, j, n, m, pacific, heights);
+            dfs(n - 1, j, n, m, atlantic, heights);
+        }
+
+        List<List<Integer>> ans = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (pacific[i][j] && atlantic[i][j]) {
+                    ans.add(Arrays.asList(i, j));
+                }
+            }
+        }
+
+        return ans;
+    }
+}
+
+// ========================================================================
+
 class Pair {
     int a, b;
     public Pair(int a, int b) {

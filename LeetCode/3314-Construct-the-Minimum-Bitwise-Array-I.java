49bb047dcfb1ac6991c974b971dc9ce048684d9a
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2026-01-21 01:33:06 +0530

    Create 3314-Construct-the-Minimum-Bitwise-Array-I.java

---
+++ LeetCode/3314-Construct-the-Minimum-Bitwise-Array-I.java
@@ -0,0 +1,22 @@
+// 3314. Construct the Minimum Bitwise Array I
+
+class Solution {
+    public int[] minBitwiseArray(List<Integer> nums) {
+        int n = nums.size();
+        int[] ans = new int[n];
+        for (int i = 0; i < n; i++) {
+            int num = nums.get(i);
+            if (num % 2 == 0) {
+                ans[i] = -1;
+            }
+            else {
+                int cnt = 0;
+                while ((num & (1 << cnt)) != 0) {
+                    ans[i] = num - (1 << cnt);
+                    cnt++;
+                }
+            }
+        }
+        return ans;
+    }
+}

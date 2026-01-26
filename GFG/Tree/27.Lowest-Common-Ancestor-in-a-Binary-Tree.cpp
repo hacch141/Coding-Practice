dab62a09d93680ff82015a8cbd78e2ab133ef496
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2026-01-27 02:39:46 +0530

    Update 27.Lowest-Common-Ancestor-in-a-Binary-Tree.cpp

--- GFG/Tree/27.Lowest-Common-Ancestor-in-a-Binary-Tree.cpp
+++ GFG/Tree/27.Lowest-Common-Ancestor-in-a-Binary-Tree.cpp
@@ -1,5 +1,25 @@
 // Lowest Common Ancestor in a Binary Tree
 
+// Java
+class Solution {
+    private Node solve(Node root, int n1, int n2) {
+        if (root == null || root.data == n1 || root.data == n2) {
+            return root;
+        }
+
+        Node left = solve(root.left, n1, n2);
+        Node right = solve(root.right, n1, n2);
+
+        if (left != null && right != null) return root;
+        return (left != null) ? left : right;
+    }
+
+    // Function to return the lowest common ancestor in a Binary Tree.
+    public Node lca(Node root, int n1, int n2) {
+        return solve(root, n1, n2);
+    }
+}
+
 class Solution
 {
     public:

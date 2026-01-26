3d6bed1d3777799768871e8cb94d6c9d468cadda
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2026-01-27 01:51:00 +0530

    Update 25.Symmetric-Tree.cpp

--- GFG/Tree/25.Symmetric-Tree.cpp
+++ GFG/Tree/25.Symmetric-Tree.cpp
@@ -1,5 +1,26 @@
 // Symmetric Tree
 
+// Java
+class Solution {
+    boolean solve(Node n1, Node n2) {
+        if (n1 == null && n2 == null) return true;
+        if (n1 == null || n2 == null) return false;
+        if (n1.data != n2.data) return false;
+
+        boolean left = solve(n1.left, n2.right);
+        boolean right = solve(n1.right, n2.left);
+
+        return left && right;
+    }
+
+    // return true/false denoting whether the tree is Symmetric or not
+    public boolean isSymmetric(Node root) {
+        if (root == null) return true;
+        return solve(root.left, root.right);
+    }
+}
+
+
 class Solution{
     public:
     

2a6acb4a89d81bff7f3a1cc180b9b9078fa99ab5
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2026-01-28 02:13:40 +0530

    Update and rename 11.Preorder-to-PostOrder.cpp to 11.construct-bst-from-preorder.cpp

--- GFG/Binary Search Tree/11.construct-bst-from-preorder.cpp
+++ GFG/Binary Search Tree/11.construct-bst-from-preorder.cpp
@@ -1,4 +1,23 @@
-// Preorder to PostOrder
+// Construct BST from preorder
+
+class Solution {
+    int i = 0;
+
+    public TreeNode solve(int[] preorder, int bound) {
+        if (i == preorder.length || preorder[i] > bound) return null;
+        TreeNode root = new TreeNode(preorder[i++]);
+
+        root.left = solve(preorder, root.val);
+        root.right = solve(preorder, bound);
+        return root;
+    }
+
+    public TreeNode bstFromPreorder(int[] preorder) {
+        return solve(preorder, Integer.MAX_VALUE);
+    }
+}
+
+// ==============================================
 
 class Solution{
 public:

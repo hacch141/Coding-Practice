01150a9dfe5670a8eee82eba483a3571d6344fab
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2026-01-28 02:18:49 +0530

    Update 12.Predecessor-and-Successor.cpp

--- GFG/Binary Search Tree/12.Predecessor-and-Successor.cpp
+++ GFG/Binary Search Tree/12.Predecessor-and-Successor.cpp
@@ -1,5 +1,42 @@
 // Predecessor and Successor
 
+class Solution {
+
+    // pre[0] and suc[0] will store predecessor and successor
+    public void findPreSuc(Node root, Node[] pre, Node[] suc, int key) {
+        Node curr = root;
+        while (curr != null) {
+            if (curr.key == key) {
+                // Predecessor: max value in left subtree
+                if (curr.left != null) {
+                    Node temp = curr.left;
+                    while (temp.right != null)
+                        temp = temp.right;
+                    pre[0] = temp;
+                }
+                // Successor: min value in right subtree
+                if (curr.right != null) {
+                    Node temp = curr.right;
+                    while (temp.left != null)
+                        temp = temp.left;
+                    suc[0] = temp;
+                }
+                return;
+            }
+            else if (key > curr.key) {
+                pre[0] = curr;        // possible predecessor
+                curr = curr.right;
+            }
+            else {
+                suc[0] = curr;        // possible successor
+                curr = curr.left;
+            }
+        }
+    }
+}
+
+// =============================================================
+
 class Solution
 {
     public:

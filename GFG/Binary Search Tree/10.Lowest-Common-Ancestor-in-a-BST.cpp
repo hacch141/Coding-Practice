66ef0d6278cd9ed4d48b5a84ec552db973e3f44e
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2026-01-28 01:40:40 +0530

    Update 10.Lowest-Common-Ancestor-in-a-BST.cpp

--- GFG/Binary Search Tree/10.Lowest-Common-Ancestor-in-a-BST.cpp
+++ GFG/Binary Search Tree/10.Lowest-Common-Ancestor-in-a-BST.cpp
@@ -1,5 +1,21 @@
 // Lowest Common Ancestor in a BST
 
+class Solution {
+    Node LCA(Node root, int n1, int n2) {
+        if (root == null) return null;
+
+        if (n1 > root.data && n2 > root.data) {
+            return LCA(root.right, n1, n2);
+        } 
+        else if (n1 < root.data && n2 < root.data) {
+            return LCA(root.left, n1, n2);
+        }
+        return root;
+    }
+}
+
+// =====================================================
+
 class Solution{
     public:
     Node* LCA(Node *root, int n1, int n2)

060d42d0fa9361e4893dd957959269c38e706212
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2026-01-28 00:25:15 +0530

    Update 06.Insert-a-node-in-a-BST.cpp

--- GFG/Binary Search Tree/06.Insert-a-node-in-a-BST.cpp
+++ GFG/Binary Search Tree/06.Insert-a-node-in-a-BST.cpp
@@ -1,5 +1,38 @@
 // Insert a node in a BST
 
+class Solution {
+    Node insert(Node root, int key) {
+        if (root == null)
+            return new Node(key);
+
+        Node curr = root;
+
+        while (true) {
+            if (key == curr.data) {
+                break; // duplicates not inserted
+            } 
+            else if (key > curr.data) {
+                if (curr.right != null) {
+                    curr = curr.right;
+                } else {
+                    curr.right = new Node(key);
+                    break;
+                }
+            } 
+            else {
+                if (curr.left != null) {
+                    curr = curr.left;
+                } else {
+                    curr.left = new Node(key);
+                    break;
+                }
+            }
+        }
+        return root;
+    }
+}
+
+
 // Function to insert a node in a BST.
 Node* insert(Node* root, int Key) {
     // Your code here

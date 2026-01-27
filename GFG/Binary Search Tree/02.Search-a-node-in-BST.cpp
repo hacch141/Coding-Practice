c4b8b2b29ec0b0b1f544e822a63e3e63e82c90d7
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2026-01-28 00:18:20 +0530

    Update 02.Search-a-node-in-BST.cpp

--- GFG/Binary Search Tree/02.Search-a-node-in-BST.cpp
+++ GFG/Binary Search Tree/02.Search-a-node-in-BST.cpp
@@ -1,5 +1,22 @@
 // Search a node in BST
 
+class Solution {
+    boolean search(Node root, int x) {
+        Node curr = root;
+
+        while (curr != null) {
+            if (x == curr.data) {
+                return true;
+            } else if (x < curr.data) {
+                curr = curr.left;
+            } else {
+                curr = curr.right;
+            }
+        }
+        return false;
+    }
+}
+
 bool search(Node* root, int x) {
     // Your code here
     Node* curr = root;

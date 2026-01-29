4927b73869719b281bf8e4e2964d5dd272736da2
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2026-01-30 01:53:42 +0530

    Update 30.Nodes-at-given-distance-in-binary-tree.cpp

--- GFG/Tree/30.Nodes-at-given-distance-in-binary-tree.cpp
+++ GFG/Tree/30.Nodes-at-given-distance-in-binary-tree.cpp
@@ -1,5 +1,65 @@
 // Nodes at given distance in binary tree
 
+class Solution {
+    public Map<TreeNode, TreeNode> parent;
+
+    public void getParent(TreeNode root) {
+        if (root == null) return;
+        if (root.left != null) {
+            parent.put(root.left, root);
+            getParent(root.left);
+        }
+        if (root.right != null) {
+            parent.put(root.right, root);
+            getParent(root.right);
+        }
+    }
+
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+
+        parent = new HashMap<>();
+        getParent(root);
+
+        List<Integer> ans = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+
+        Set<TreeNode> vis = new HashSet<>();
+        q.add(target);
+        vis.add(target);
+        int lvl = 0;
+
+        while (!q.isEmpty()) {
+            if (lvl == k) {
+                while (!q.isEmpty()) {
+                    ans.add(q.poll().val);
+                }
+            }
+            int sz = q.size();
+            while (sz-- > 0) {
+                TreeNode curr = q.poll();
+                if (curr.left != null && !vis.contains(curr.left)) {
+                    vis.add(curr.left);
+                    q.add(curr.left);
+                }
+                if (curr.right != null && !vis.contains(curr.right)) {
+                    vis.add(curr.right);
+                    q.add(curr.right);
+                }
+                if (parent.get(curr) != null && !vis.contains(parent.get(curr))) {
+                    TreeNode par = parent.get(curr);
+                    vis.add(par);
+                    q.add(par);
+                }
+            }
+            lvl++;
+        }
+
+        return ans;
+    }
+}
+
+// ==========================================================
+
 // ME
 class Solution
 {

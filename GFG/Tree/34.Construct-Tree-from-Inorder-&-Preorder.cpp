ad0cdd40b0f90c3e10e0a2a790a94f527aa108ae
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2026-01-30 02:37:04 +0530

    Update 34.Construct-Tree-from-Inorder-&-Preorder.cpp

--- GFG/Tree/34.Construct-Tree-from-Inorder-&-Preorder.cpp
+++ GFG/Tree/34.Construct-Tree-from-Inorder-&-Preorder.cpp
@@ -1,5 +1,36 @@
 // Construct Tree from Inorder & Preorder
 
+class Solution {
+    int ptr = 0;
+
+    public TreeNode build(int l, int r, int[] inorder, int[] preorder) {
+        if (l > r || ptr >= preorder.length) return null;
+
+        TreeNode root = new TreeNode(preorder[ptr++]);
+        int ind = -1;
+        for (int i = l; i <= r; i++) {
+            if (inorder[i] == root.val) {
+                ind = i;
+                break;
+            }
+        }
+
+        if (ind == -1) return root;
+
+        root.left = build(l, ind - 1, inorder, preorder);
+        root.right = build(ind + 1, r, inorder, preorder);
+
+        return root;
+    }
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        int n = preorder.length;
+        return build(0, n - 1, inorder, preorder);
+    }
+}
+
+// =============================================================
+
 class Solution{
     public:
     Node* build(int in[], int is, int ie, int pre[], int ps, int pe, map<int,queue<int>>& inmp) {

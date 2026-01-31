4cedf7fba29b4babf8efde65dba7438dd302cc76
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2026-01-31 22:40:29 +0530

    Update 14.Detect-cycle-in-a-directed-graph.cpp

--- GFG/Graph/BFS-DFS/14.Detect-cycle-in-a-directed-graph.cpp
+++ GFG/Graph/BFS-DFS/14.Detect-cycle-in-a-directed-graph.cpp
@@ -1,6 +1,38 @@
 // Detect cycle in a directed graph
 
 class Solution {
+    private boolean dfs(int u, List<List<Integer>> adj, boolean[] vis, boolean[] pathVis) {
+        vis[u] = true;
+        pathVis[u] = true;
+
+        for (int v : adj.get(u)) {
+            if (pathVis[v]) return true;
+            if (vis[v]) continue;
+            if (dfs(v, adj, vis, pathVis)) return true;
+        }
+
+        pathVis[u] = false; // backtrack
+        return false;
+    }
+
+    // Function to detect cycle in a directed graph
+    public boolean isCyclic(int V, List<List<Integer>> adj) {
+
+        boolean[] vis = new boolean[V];
+        boolean[] pathVis = new boolean[V];
+
+        for (int u = 0; u < V; u++) {
+            if (!vis[u]) {
+                if (dfs(u, adj, vis, pathVis)) return true;
+            }
+        }
+        return false;
+    }
+}
+
+// ===============================================================
+
+class Solution {
   public:
     bool dfs(int u, vector<vector<int>> adj, vector<bool> &vis, vector<bool> &pathVis) {
         vis[u] = true;

aaec7a54bb354fa3cb28638db4c03549b9610fc8
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2026-01-31 17:34:15 +0530

    Update 09.Number-Of-Enclaves.cpp

--- GFG/Graph/BFS-DFS/09.Number-Of-Enclaves.cpp
+++ GFG/Graph/BFS-DFS/09.Number-Of-Enclaves.cpp
@@ -1,5 +1,68 @@
 // Number Of Enclaves
 
+class Solution {
+
+    private void dfs(int row, int col, int[][] grid, boolean[][] vis, int n, int m) {
+        vis[row][col] = true;
+        int[] delRow = {-1, 0, 1, 0};
+        int[] delCol = {0, -1, 0, 1};
+
+        for (int d = 0; d < 4; d++) {
+            int nRow = row + delRow[d];
+            int nCol = col + delCol[d];
+
+            if (nRow >= 0 && nRow < n &&
+                nCol >= 0 && nCol < m &&
+                !vis[nRow][nCol] &&
+                grid[nRow][nCol] == 1) {
+
+                dfs(nRow, nCol, grid, vis, n, m);
+            }
+        }
+    }
+
+    public int numEnclaves(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+
+        boolean[][] vis = new boolean[n][m];
+
+        // Left & Right boundaries
+        for (int i = 0; i < n; i++) {
+            if (!vis[i][0] && grid[i][0] == 1) {
+                dfs(i, 0, grid, vis, n, m);
+            }
+            if (!vis[i][m - 1] && grid[i][m - 1] == 1) {
+                dfs(i, m - 1, grid, vis, n, m);
+            }
+        }
+
+        // Top & Bottom boundaries
+        for (int j = 0; j < m; j++) {
+            if (!vis[0][j] && grid[0][j] == 1) {
+                dfs(0, j, grid, vis, n, m);
+            }
+            if (!vis[n - 1][j] && grid[n - 1][j] == 1) {
+                dfs(n - 1, j, grid, vis, n, m);
+            }
+        }
+
+        // Count enclave land cells
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 1 && !vis[i][j]) {
+                    ans++;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
+
+// =============================================================
+
 // BFS
 class Solution {
     

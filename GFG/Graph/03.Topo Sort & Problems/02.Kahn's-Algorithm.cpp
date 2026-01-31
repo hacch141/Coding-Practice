a1933f72b305af21d1d07621d015b70c8b66f0d2
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2026-02-01 01:37:56 +0530

    Update 02.Kahn's-Algorithm.cpp

--- GFG/Graph/03.Topo Sort & Problems/02.Kahn's-Algorithm.cpp
+++ GFG/Graph/03.Topo Sort & Problems/02.Kahn's-Algorithm.cpp
@@ -1,5 +1,51 @@
 // Kahn's Algorithm
 
+import java.util.*;
+
+class Solution {
+
+    // Function to return vertices in Topological order
+    public int[] topoSort(int V, List<List<Integer>> adj) {
+
+        int[] indegree = new int[V];
+
+        // Step 1: Calculate indegree
+        for (int u = 0; u < V; u++) {
+            for (int v : adj.get(u)) {
+                indegree[v]++;
+            }
+        }
+
+        // Step 2: Push nodes with indegree 0
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < V; i++) {
+            if (indegree[i] == 0) {
+                q.offer(i);
+            }
+        }
+
+        // Step 3: BFS
+        int[] topo = new int[V];
+        int idx = 0;
+
+        while (!q.isEmpty()) {
+            int u = q.poll();
+            topo[idx++] = u;
+
+            for (int v : adj.get(u)) {
+                indegree[v]--;
+                if (indegree[v] == 0) {
+                    q.offer(v);
+                }
+            }
+        }
+
+        return topo;
+    }
+}
+
+// ===================================================================
+
 class Solution
 {
 	public:

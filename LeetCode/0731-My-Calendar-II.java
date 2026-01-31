c961ae88308cd5a035d1c37a8b9b875f3c873e42
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2026-02-01 03:05:27 +0530

    Create 0731-My-Calendar-II.java

---
+++ LeetCode/0731-My-Calendar-II.java
@@ -0,0 +1,24 @@
+// 731. My Calendar II
+
+class MyCalendarTwo {
+    TreeMap<Integer,Integer> mp;
+
+    public MyCalendarTwo() {
+        mp = new TreeMap<>();
+    }
+    
+    public boolean book(int startTime, int endTime) {
+        int events = 0;
+        mp.put(startTime, mp.getOrDefault(startTime, 0) + 1);
+        mp.put(endTime, mp.getOrDefault(endTime, 0) - 1);
+        for (int del : mp.values()) {
+            events += del;
+            if (events > 2) {
+                mp.put(startTime, mp.get(startTime) - 1);
+                mp.put(endTime, mp.get(endTime) + 1);
+                return false;
+            }
+        }
+        return true;
+    }
+}

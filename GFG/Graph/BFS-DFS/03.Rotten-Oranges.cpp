ea5924a0de12942a3130fc8e38ec6b2a5477fac6
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2026-01-31 16:03:16 +0530

    Update 03.Rotten-Oranges.cpp

--- GFG/Graph/BFS-DFS/03.Rotten-Oranges.cpp
+++ GFG/Graph/BFS-DFS/03.Rotten-Oranges.cpp
@@ -1,5 +1,70 @@
 // Rotten Oranges
 
+import java.util.*;
+
+class Solution {
+
+    // Function to find minimum time required to rot all oranges.
+    public int orangesRotting(int[][] grid) {
+        int n = grid.length;
+        int m = grid[0].length;
+
+        boolean[][] vis = new boolean[n][m];
+        Queue<int[]> q = new LinkedList<>();
+
+        int fresh = 0;
+
+        // Initialize queue with rotten oranges
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] == 2) {
+                    vis[i][j] = true;
+                    q.add(new int[]{i, j});
+                } else if (grid[i][j] == 1) {
+                    fresh++;
+                }
+            }
+        }
+
+        int[] delRow = {-1, 0, 1, 0};
+        int[] delCol = {0, -1, 0, 1};
+
+        int time = 0;
+        int rotten = 0;
+
+        while (!q.isEmpty()) {
+            int size = q.size();
+
+            for (int i = 0; i < size; i++) {
+                int[] cur = q.poll();
+                int r = cur[0];
+                int c = cur[1];
+
+                for (int d = 0; d < 4; d++) {
+                    int nRow = r + delRow[d];
+                    int nCol = c + delCol[d];
+
+                    if (nRow >= 0 && nRow < n &&
+                        nCol >= 0 && nCol < m &&
+                        !vis[nRow][nCol] &&
+                        grid[nRow][nCol] == 1) {
+
+                        rotten++;
+                        vis[nRow][nCol] = true;
+                        q.add(new int[]{nRow, nCol});
+                    }
+                }
+            }
+            time++;
+        }
+
+        if (fresh != rotten) return -1;
+        return time - 1;
+    }
+}
+
+// ========================================================================
+
 // BFS
 class Solution 
 {

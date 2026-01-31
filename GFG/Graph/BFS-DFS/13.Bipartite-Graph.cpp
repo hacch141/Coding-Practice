95798a8cabc2b046ad8e0e99a9ec9a10734c670c
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2026-01-31 22:17:41 +0530

    Update 13.Bipartite-Graph.cpp

--- GFG/Graph/BFS-DFS/13.Bipartite-Graph.cpp
+++ GFG/Graph/BFS-DFS/13.Bipartite-Graph.cpp
@@ -1,5 +1,34 @@
 // Bipartite Graph
 
+class Solution {
+    public boolean solve(int u, int[][] graph, int[] color, int currColor) {
+        color[u] = currColor;
+        for (int v : graph[u]) {
+            if (color[v] == -1) {
+                if (!solve(v, graph, color, 1 - currColor)) return false;
+            }
+            else if (color[v] == color[u]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isBipartite(int[][] graph) {
+        int n = graph.length;
+        int[] color = new int[n];
+        Arrays.fill(color, -1);
+        for (int i = 0; i < n; i++) {
+            if (color[i] == -1) {
+                if (!solve(i, graph, color, 0)) return false;
+            }
+        }
+        return true;
+    }
+}
+
+// =====================================================================
+
 // BFS
 class Solution {
 public:

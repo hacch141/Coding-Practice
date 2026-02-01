6cda06588b4e472c58bcac76d9143be73df22d58
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2026-02-01 13:30:26 +0530

    Update 01.Shortest-path-in-Undirected-Graph-having-unit-distance.cpp

--- GFG/Graph/04.Shortest Path Algorithms and Problems/01.Shortest-path-in-Undirected-Graph-having-unit-distance.cpp
+++ GFG/Graph/04.Shortest Path Algorithms and Problems/01.Shortest-path-in-Undirected-Graph-having-unit-distance.cpp
@@ -1,6 +1,49 @@
 // Shortest path in Undirected Graph having unit distance
 
 class Solution {
+    public int[] shortestPath(int[][] edges, int N, int M, int src) {
+        ArrayList<Integer>[] adj = new ArrayList[N];
+        for (int i = 0; i < N; i++) {
+            adj[i] = new ArrayList<>();
+        }
+
+        for (int[] e : edges) {
+            int u = e[0];
+            int v = e[1];
+            adj[u].add(v);
+            adj[v].add(u);
+        }
+
+        int[] dist = new int[N];
+        Arrays.fill(dist, (int) 1e9);
+        dist[src] = 0;
+
+        Queue<Integer> q = new LinkedList<>();
+        q.offer(src);
+
+        while (!q.isEmpty()) {
+            int u = q.poll();
+            for (int v : adj[u]) {
+                if (dist[u] + 1 < dist[v]) {
+                    dist[v] = dist[u] + 1;
+                    q.offer(v);
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            if (dist[i] == (int) 1e9) {
+                dist[i] = -1;
+            }
+        }
+
+        return dist;
+    }
+}
+
+// ================================================================
+
+class Solution {
   public:
     vector<int> shortestPath(vector<vector<int>>& edges, int N,int M, int src){
         // code here

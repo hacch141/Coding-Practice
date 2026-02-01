e633a588296ca61760cea9e299d4c33f7468184e
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2026-02-01 15:04:05 +0530

    Update 09.Number-of-Ways-to-Arrive-at-Destination.cpp

--- GFG/Graph/04.Shortest Path Algorithms and Problems/09.Number-of-Ways-to-Arrive-at-Destination.cpp
+++ GFG/Graph/04.Shortest Path Algorithms and Problems/09.Number-of-Ways-to-Arrive-at-Destination.cpp
@@ -1,6 +1,57 @@
 // Number of Ways to Arrive at Destination
 
 class Solution {
+    long MOD = (int)1e9 + 7;
+    long INF = Long.MAX_VALUE;
+
+    public int countPaths(int n, int[][] roads) {
+        List<int[]>[] adj = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            adj[i] = new ArrayList<>();
+        }
+
+        for (int[] r : roads) {
+            int u = r[0], v = r[1], t = r[2];
+            adj[u].add(new int[]{v, t});
+            adj[v].add(new int[]{u, t});
+        }
+
+        long[] dist = new long[n];
+        long[] dp = new long[n];
+        Arrays.fill(dist, INF);
+
+        PriorityQueue<long[]> pq = new PriorityQueue<>((a, b) -> Long.compare(a[0], b[0]));
+        pq.add(new long[]{0, 0});
+
+        dist[0] = 0;
+        dp[0] = 1;
+
+        while (!pq.isEmpty()) {
+            long[] curr = pq.poll();
+            long ut = curr[0];
+            int u = (int)curr[1];
+            for (int[] edge : adj[u]) {
+                int v = edge[0];
+                long vt = (long)edge[1];
+                long newTime = ut + vt;
+                if (newTime < dist[v]) {
+                    dist[v] = newTime;
+                    dp[v] = dp[u];
+                    pq.add(new long[]{newTime, v});
+                }
+                else if (newTime == dist[v]) {
+                    dp[v] = (dp[u] + dp[v]) % MOD;
+                }
+            }
+        }
+
+        return (int)dp[n - 1];
+    }
+}
+
+// =========================================================================
+
+class Solution {
   public:
     int countPaths(int n, vector<vector<int>>& roads) {
         // code here

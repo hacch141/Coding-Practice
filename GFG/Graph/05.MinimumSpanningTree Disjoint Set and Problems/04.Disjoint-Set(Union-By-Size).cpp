996a7a2a87b3e3d7b2cdcac07af145ce462acae5
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2026-02-02 03:04:30 +0530

    Update 04.Disjoint-Set(Union-By-Size).cpp

--- GFG/Graph/05.MinimumSpanningTree Disjoint Set and Problems/04.Disjoint-Set(Union-By-Size).cpp
+++ GFG/Graph/05.MinimumSpanningTree Disjoint Set and Problems/04.Disjoint-Set(Union-By-Size).cpp
@@ -1,6 +1,44 @@
 // Disjoint Set | Union by Size
 
 class DisjointSet {
+    int[] parent;
+    int[] size;
+
+    DisjointSet(int n) {
+        parent = new int[n + 1];
+        size = new int[n + 1];
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findPar(int node) {
+        if (node == parent[node]) {
+            return node;
+        }
+        return parent[node] = findPar(parent[node]); // path compression
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findPar(u);
+        int ulp_v = findPar(v);
+
+        if (ulp_u == ulp_v) return;
+
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        } else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+}
+
+// ==================================================================
+
+class DisjointSet {
     vector<int> parent,size;
   public:
     DisjointSet(int n) {

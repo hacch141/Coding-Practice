fc730555978982c276d7fea8ca04b7cd1a302f8b
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2026-02-06 00:25:15 +0530

    Update 04.Implement-Atoi-Function.cpp

--- GFG/String/02.Medium String Problems/04.Implement-Atoi-Function.cpp
+++ GFG/String/02.Medium String Problems/04.Implement-Atoi-Function.cpp
@@ -1,5 +1,50 @@
 // Implement Atoi Function
 
+class Solution {
+    int MN = Integer.MIN_VALUE;
+    int MX = Integer.MAX_VALUE;
+
+    public boolean isDigit(char ch) {
+        return ch >= '0' && ch <= '9';
+    }
+
+    public int myAtoi(String s) {
+        int n = s.length();
+
+        int i = 0;
+        while (i < n && s.charAt(i) == ' ') {
+            i++;
+        }
+
+        if (i == n) return 0;
+
+        int sign = 1;
+        if (s.charAt(i) == '+') {
+            i++;
+        }
+        else if (s.charAt(i) == '-') {
+            sign = -1;
+            i++;
+        }
+
+        long ans = 0;
+        while (i < n && isDigit(s.charAt(i))) {
+            ans = (ans * 10) + (s.charAt(i) - '0');
+            if (ans * sign <= MN) {
+                return MN;
+            }
+            else if(ans * sign >= MX) {
+                return MX;
+            }
+            i++;
+        }
+
+        return (int)ans * sign;
+    }
+}
+
+// ==============================================================
+
 int getNum(string &s, int &ind) {
     int n = s.length();
     int sign = 1;

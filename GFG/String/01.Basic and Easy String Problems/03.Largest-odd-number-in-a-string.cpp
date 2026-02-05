72ae124de23a39ac53de5cd457d3f49630dbd82e
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2026-02-05 19:09:41 +0530

    Update 03.Largest-odd-number-in-a-string.cpp

--- GFG/String/01.Basic and Easy String Problems/03.Largest-odd-number-in-a-string.cpp
+++ GFG/String/01.Basic and Easy String Problems/03.Largest-odd-number-in-a-string.cpp
@@ -1,6 +1,25 @@
 // Largest odd number in a string
 
 class Solution {
+    public String largestOddNumber(String num) {
+        int n = num.length();
+        int lastIndex = -1;
+        for (int i = n - 1; i >= 0; i--) {
+            if ((num.charAt(i) - '0') % 2 == 1) {
+                lastIndex = i;
+                break;
+            }
+        }
+
+        if (lastIndex == -1) return "";
+
+        return num.substring(0, lastIndex + 1);
+    }
+}
+
+// ==================================================================
+
+class Solution {
 public:
     string largestOddNumber(string num) {
         int right = num.size() - 1;

cee4274e6afc5dca7676a6e53c5d4c4a967404c5
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2026-02-06 22:12:38 +0530

    Update 01.Selection-Sort.cpp

--- GFG/Sorting/01.Selection-Sort.cpp
+++ GFG/Sorting/01.Selection-Sort.cpp
@@ -1,5 +1,25 @@
 // Selection Sort
 
+class Solution {
+    public void selectionSort(int[] arr, int n) {
+        for (int i = 0; i < n - 1; i++) {
+            int minIdx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIdx]) {
+                    minIdx = j;
+                }
+            }
+            if (minIdx != i) {
+                int temp = arr[i];
+                arr[i] = arr[minIdx];
+                arr[minIdx] = temp;
+            }
+        }
+    }
+}
+
+// =============================================================
+
 void selectionSort(int arr[], int n)
 {
   //code here

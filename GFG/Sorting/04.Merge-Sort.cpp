f4a7d022997549345875e6d07f353549efb437c2
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2026-02-06 22:21:24 +0530

    Update 04.Merge-Sort.cpp

--- GFG/Sorting/04.Merge-Sort.cpp
+++ GFG/Sorting/04.Merge-Sort.cpp
@@ -1,5 +1,35 @@
 // Merge Sort
 
+class Solution {
+    void merge(int[] arr, int l, int m, int r) {
+        int left = l;
+        int right = m + 1;
+        ArrayList<Integer> temp = new ArrayList<>();
+        while (left <= m && right <= r) {
+            if (arr[left] <= arr[right]) {
+                temp.add(arr[left++]);
+            } else {
+                temp.add(arr[right++]);
+            }
+        }
+        while (left <= m) temp.add(arr[left++]);
+        while (right <= r) temp.add(arr[right++]);
+        for (int i = l; i <= r; i++) {
+            arr[i] = temp.get(i - l);
+        }
+    }
+
+    void mergeSort(int[] arr, int l, int r) {
+        if (l >= r) return;
+        int mid = l + (r - l) / 2;
+        mergeSort(arr, l, mid);
+        mergeSort(arr, mid + 1, r);
+        merge(arr, l, mid, r);
+    }
+}
+
+// ======================================================================
+
 class Solution
 {
     public:

8808c969eccd00b47277676afbc02b84f5bc8fde
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2026-02-11 09:50:52 +0530

    Update 03.3-Sum-Problem.cpp

--- GFG/Array/03.Hard/03.3-Sum-Problem.cpp
+++ GFG/Array/03.Hard/03.3-Sum-Problem.cpp
@@ -1,5 +1,68 @@
 // 3-Sum Problem
 
+class Solution {
+    public List<List<Integer>> triplet(int n, int[] arr) {
+        List<List<Integer>> ans = new ArrayList<>();
+        Arrays.sort(arr);
+
+        for (int i = 0; i < n; i++) {
+            if (i > 0 && arr[i] == arr[i - 1]) continue;
+
+            int low = i + 1;
+            int high = n - 1;
+
+            while (low < high) {
+                int sum = arr[i] + arr[low] + arr[high];
+
+                if (sum == 0) {
+                    ans.add(Arrays.asList(arr[i], arr[low], arr[high]));
+
+                    low++;
+                    high--;
+
+                    while (low < high && arr[low] == arr[low - 1]) low++;
+                    while (low < high && arr[high] == arr[high + 1]) high--;
+                }
+                else if (sum > 0) {
+                    high--;
+                }
+                else {
+                    low++;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] arr) {
+        int n = arr.length;
+        Set<List<Integer>> set = new HashSet<>();
+
+        for (int i = 0; i < n; i++) {
+            Map<Integer, Integer> map = new HashMap<>();
+
+            for (int j = i + 1; j < n; j++) {
+                int sum = arr[i] + arr[j];
+
+                if (map.containsKey(-sum)) {
+                    List<Integer> temp = Arrays.asList(arr[i], arr[j], arr[map.get(-sum)]);
+                    Collections.sort(temp);
+                    set.add(temp);
+                }
+
+                map.put(arr[j], j);
+            }
+        }
+
+        return new ArrayList<>(set);
+    }
+}
+
+// ==================================================================================
+
 vector<vector<int>> triplet(int n, vector<int> &arr)
 {
     // Write your code here.

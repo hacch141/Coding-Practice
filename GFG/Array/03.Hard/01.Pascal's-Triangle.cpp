c58627e38d479a1caadccf215c3895de35e7ff0b
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2026-02-11 09:31:27 +0530

    Update 01.Pascal's-Triangle.cpp

--- GFG/Array/03.Hard/01.Pascal's-Triangle.cpp
+++ GFG/Array/03.Hard/01.Pascal's-Triangle.cpp
@@ -1,5 +1,29 @@
 // Pascal's Triangle
 
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    row.add(1);
+                } else {
+                    row.add(ans.get(i - 1).get(j) + ans.get(i - 1).get(j - 1));
+                }
+            }
+
+            ans.add(row);
+        }
+
+        return ans;
+    }
+}
+
+// ============================================================================
+
 vector<vector<int>> pascalTriangle(int N) {
     // Write your code here.
     if(N == 1) return {{1}};

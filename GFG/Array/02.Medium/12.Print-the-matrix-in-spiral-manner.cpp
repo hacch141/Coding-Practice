e0d4a25955bb603dee7307c1926f2158a5694e8c
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2026-02-11 08:43:56 +0530

    Update 12.Print-the-matrix-in-spiral-manner.cpp

--- GFG/Array/02.Medium/12.Print-the-matrix-in-spiral-manner.cpp
+++ GFG/Array/02.Medium/12.Print-the-matrix-in-spiral-manner.cpp
@@ -1,5 +1,51 @@
 // Print the matrix in spiral manner
 
+class Solution {
+    public List<Integer> spiralMatrix(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+
+        int topRow = 0, leftCol = 0;
+        int bottomRow = n - 1, rightCol = m - 1;
+
+        List<Integer> ans = new ArrayList<>();
+        while (topRow <= bottomRow && leftCol <= rightCol) {
+
+            // top row
+            for (int i = leftCol; i <= rightCol; i++) {
+                ans.add(matrix[topRow][i]);
+            }
+            topRow++;
+
+            // right column
+            for (int i = topRow; i <= bottomRow; i++) {
+                ans.add(matrix[i][rightCol]);
+            }
+            rightCol--;
+
+            // bottom row
+            if (topRow <= bottomRow) {
+                for (int i = rightCol; i >= leftCol; i--) {
+                    ans.add(matrix[bottomRow][i]);
+                }
+                bottomRow--;
+            }
+
+            // left column
+            if (leftCol <= rightCol) {
+                for (int i = bottomRow; i >= topRow; i--) {
+                    ans.add(matrix[i][leftCol]);
+                }
+                leftCol++;
+            }
+        }
+
+        return ans;
+    }
+}
+
+// ==============================================================================================
+
 vector<int> spiralMatrix(vector<vector<int>>&MATRIX) {
     // Write your code here.
     int n = MATRIX.size();

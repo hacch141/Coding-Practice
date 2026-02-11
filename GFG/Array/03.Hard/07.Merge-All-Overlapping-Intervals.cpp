4af7812d4e91df6188c38308fb5b14eb76cad539
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2026-02-11 22:35:33 +0530

    Update 07.Merge-All-Overlapping-Intervals.cpp

--- GFG/Array/03.Hard/07.Merge-All-Overlapping-Intervals.cpp
+++ GFG/Array/03.Hard/07.Merge-All-Overlapping-Intervals.cpp
@@ -1,5 +1,32 @@
 // Merge All Overlapping Intervals
 
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        if (intervals.length <= 1) return intervals;
+
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        List<int[]> result = new ArrayList<>();
+
+        int[] current = intervals[0];
+
+        for (int i = 1; i < intervals.length; i++) {
+
+            if (intervals[i][0] <= current[1]) {
+                current[1] = Math.max(current[1], intervals[i][1]);
+            } else {
+                result.add(current);
+                current = intervals[i];
+            }
+        }
+
+        result.add(current);
+
+        return result.toArray(new int[result.size()][]);
+    }
+}
+
+// ===========================================================
+
 #include <bits/stdc++.h>
 
 static bool compare(vector<int>& v1, vector<int>& v2) {

00722148a4b2d666bc92f2eb7eff97c803bcb3b2
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2026-02-13 09:51:52 +0530

    Update 17.Segregate-even-and-odd-nodes-in-a-Link-List.cpp

--- GFG/Linked List/17.Segregate-even-and-odd-nodes-in-a-Link-List.cpp
+++ GFG/Linked List/17.Segregate-even-and-odd-nodes-in-a-Link-List.cpp
@@ -1,5 +1,41 @@
 // Segregate even and odd nodes in a Link List
 
+class Solution {
+
+    public Node divide(int N, Node head) {
+
+        Node even = new Node(-1);
+        Node dummy1 = even;
+
+        Node odd = new Node(-1);
+        Node dummy2 = odd;
+
+        Node curr = head;
+
+        while (curr != null) {
+
+            if (curr.data % 2 == 1) {
+                odd.next = curr;
+                odd = odd.next;
+            } else {
+                even.next = curr;
+                even = even.next;
+            }
+
+            curr = curr.next;
+        }
+
+        if (dummy2.next == null || dummy1.next == null) return head;
+
+        even.next = dummy2.next;
+        odd.next = null;
+
+        return dummy1.next;
+    }
+}
+
+// ========================================================================
+
 class Solution{
 public:
     Node* divide(int N, Node *head){

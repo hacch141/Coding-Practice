cfd16aac4bad1c8a4681cef9db4fc124e9e7e9c0
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2026-02-14 14:52:52 +0530

    Update 26.Find-pairs-with-given-sum-in-doubly-linked-list.cpp

--- GFG/Linked List/26.Find-pairs-with-given-sum-in-doubly-linked-list.cpp
+++ GFG/Linked List/26.Find-pairs-with-given-sum-in-doubly-linked-list.cpp
@@ -1,5 +1,44 @@
 // Find pairs with given sum in doubly linked list
 
+class Solution {
+
+    public List<List<Integer>> findPairsWithGivenSum(Node head, int target) {
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        if (head == null) return ans;
+
+        Node start = head;
+        Node end = head;
+
+        // Move end to last node
+        while (end.next != null) {
+            end = end.next;
+        }
+
+        // Two pointer approach
+        while (start != end) {
+
+            int sum = start.data + end.data;
+
+            if (sum == target) {
+                ans.add(Arrays.asList(start.data, end.data));
+                start = start.next;
+            } 
+            else if (sum < target) {
+                start = start.next;
+            } 
+            else {
+                end = end.prev;
+            }
+        }
+
+        return ans;
+    }
+}
+
+// =========================================================================
+
 class Solution
 {
 public:

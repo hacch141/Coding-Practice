337e7d0ab25b968dda976beb71b20c10c7d44463
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2026-02-14 21:48:24 +0530

    Update 03.Jump-Game-II.cpp

--- GFG/Greedy/02.Medium-Hard/03.Jump-Game-II.cpp
+++ GFG/Greedy/02.Medium-Hard/03.Jump-Game-II.cpp
@@ -1,6 +1,32 @@
 // Jump Game II
 
 class Solution {
+    public int jump(int[] nums) {
+
+        int left = 0;
+        int right = 0;
+        int jumps = 0;
+
+        while (right < nums.length - 1) {
+
+            int farthest = 0;
+
+            for (int i = left; i <= right; i++) {
+                farthest = Math.max(farthest, i + nums[i]);
+            }
+
+            jumps++;
+            left = right + 1;
+            right = farthest;
+        }
+
+        return jumps;
+    }
+}
+
+// ===================================================================
+
+class Solution {
 public:
     int jump(vector<int>& nums) {
         int left = 0, right = 0, ans=0;

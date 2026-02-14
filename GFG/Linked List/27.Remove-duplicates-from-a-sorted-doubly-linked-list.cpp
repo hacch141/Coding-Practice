bddffea1495c232f5d3c7c8abc68f235a857da6e
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2026-02-14 14:54:28 +0530

    Update 27.Remove-duplicates-from-a-sorted-doubly-linked-list.cpp

--- GFG/Linked List/27.Remove-duplicates-from-a-sorted-doubly-linked-list.cpp
+++ GFG/Linked List/27.Remove-duplicates-from-a-sorted-doubly-linked-list.cpp
@@ -1,5 +1,35 @@
 // Remove duplicates from a sorted doubly linked list
 
+class Solution {
+
+    Node removeDuplicates(Node head) {
+
+        if (head == null || head.next == null)
+            return head;
+
+        Node curr = head;
+
+        while (curr != null && curr.next != null) {
+
+            if (curr.data == curr.next.data) {
+
+                curr.next = curr.next.next;
+
+                if (curr.next != null) {
+                    curr.next.prev = curr;
+                }
+            } 
+            else {
+                curr = curr.next;
+            }
+        }
+
+        return head;
+    }
+}
+
+// ===========================================================================
+
 class Solution
 {
 public:

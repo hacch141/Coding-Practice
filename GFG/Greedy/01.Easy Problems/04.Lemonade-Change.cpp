e0863cbd979b36c08e688ddac42d2e4b8817a37e
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2026-02-14 16:24:56 +0530

    Update 04.Lemonade-Change.cpp

--- GFG/Greedy/01.Easy Problems/04.Lemonade-Change.cpp
+++ GFG/Greedy/01.Easy Problems/04.Lemonade-Change.cpp
@@ -1,5 +1,43 @@
 // Lemonade Change
 
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+
+        int five = 0;
+        int ten = 0;
+
+        for (int bill : bills) {
+
+            if (bill == 5) {
+                five++;
+            } 
+            else if (bill == 10) {
+                if (five == 0) return false;
+                five--;
+                ten++;
+            } 
+            else { // bill == 20
+
+                // Prefer giving 10 + 5
+                if (ten > 0 && five > 0) {
+                    ten--;
+                    five--;
+                } 
+                else if (five >= 3) {
+                    five -= 3;
+                } 
+                else {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
+
+// ============================================================
+
 bool lemonadeChange(vector<int> &bill) {
   	// Write your code here.
   	int cnt20=0, cnt10=0, cnt5=0;

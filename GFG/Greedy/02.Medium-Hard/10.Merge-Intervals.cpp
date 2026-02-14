54edf2cd2567a4e6076a792172cd55b688ee3204
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2026-02-14 22:57:36 +0530

    Update 10.Merge-Intervals.cpp

--- GFG/Greedy/02.Medium-Hard/10.Merge-Intervals.cpp
+++ GFG/Greedy/02.Medium-Hard/10.Merge-Intervals.cpp
@@ -1,5 +1,42 @@
 // Merge Intervals
 
+class Solution {
+
+    public int[][] merge(int[][] intervals) {
+
+        if (intervals.length == 0)
+            return new int[0][0];
+
+        // 1️⃣ Sort by start time
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+        List<int[]> ans = new ArrayList<>();
+
+        // 2️⃣ Take first interval as current
+        int[] curr = intervals[0];
+
+        for (int i = 1; i < intervals.length; i++) {
+
+            // If overlapping
+            if (curr[1] >= intervals[i][0]) {
+                curr[1] = Math.max(curr[1], intervals[i][1]);
+            } 
+            else {
+                // No overlap → push current
+                ans.add(curr);
+                curr = intervals[i];
+            }
+        }
+
+        // Add last interval
+        ans.add(curr);
+
+        return ans.toArray(new int[ans.size()][]);
+    }
+}
+
+// =======================================================================
+
 #include <bits/stdc++.h> 
 /*
 

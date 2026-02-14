2a4e88f126ee7ebc1298334f7b5dd2661f231161
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2026-02-14 10:11:35 +0530

    Update 14.Find-the-first-node-of-loop-in-linked-list.cpp

--- GFG/Linked List/14.Find-the-first-node-of-loop-in-linked-list.cpp
+++ GFG/Linked List/14.Find-the-first-node-of-loop-in-linked-list.cpp
@@ -1,5 +1,42 @@
 // Find the first node of loop in linked list
 
+class Solution {
+
+    // Function to find first node if the linked list has a loop.
+    public int findFirstNode(Node head) {
+
+        if (head == null || head.next == null)
+            return -1;
+
+        Node slow = head;
+        Node fast = head;
+
+        // Step 1: Detect cycle
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast)
+                break;
+        }
+
+        // If no cycle
+        if (fast == null || fast.next == null)
+            return -1;
+
+        // Step 2: Find starting node of cycle
+        fast = head;
+        while (slow != fast) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        return fast.data;
+    }
+}
+
+// =====================================================================
+
 class Solution
 {
     public:

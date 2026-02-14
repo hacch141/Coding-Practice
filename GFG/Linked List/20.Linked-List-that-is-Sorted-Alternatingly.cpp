e1414fc351f239a24c4e9755123efc4905f95970
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2026-02-14 11:06:13 +0530

    Update 20.Linked-List-that-is-Sorted-Alternatingly.cpp

--- GFG/Linked List/20.Linked-List-that-is-Sorted-Alternatingly.cpp
+++ GFG/Linked List/20.Linked-List-that-is-Sorted-Alternatingly.cpp
@@ -1,5 +1,51 @@
 // Linked List that is Sorted Alternatingly
 
+class Solution {
+    public ListNode merge(ListNode head1, ListNode head2) {
+        ListNode head = new ListNode(-1);
+        ListNode dummy = head;
+        while (head1 != null && head2 != null) {
+            if (head1.val <= head2.val) {
+                dummy.next = head1;
+                head1 = head1.next;
+            }
+            else {
+                dummy.next = head2;
+                head2 = head2.next;
+            }
+            dummy = dummy.next;
+        }
+
+        if (head1 != null) dummy.next = head1;
+        if (head2 != null) dummy.next = head2;
+
+        return head.next;
+    }
+
+    public ListNode sortList(ListNode head) {
+
+        if (head == null || head.next == null) return head;
+
+        ListNode slow = head;
+        ListNode fast = head;
+        ListNode prev = null;
+
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        ListNode l1 = sortList(prev.next);
+        prev.next = null;
+        ListNode l2 = sortList(head);
+
+        return merge(l1, l2);
+    }
+}
+
+// ================================================================
+
 Node* reverse(Node* head) {
     if(!head || !head->next) return head;
     Node* prev = NULL;

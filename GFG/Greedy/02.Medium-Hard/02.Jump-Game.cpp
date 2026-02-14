5458deec683ca638dd58d0c9d535e917adbc799a
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2026-02-14 20:32:30 +0530

    Update 02.Jump-Game.cpp

--- GFG/Greedy/02.Medium-Hard/02.Jump-Game.cpp
+++ GFG/Greedy/02.Medium-Hard/02.Jump-Game.cpp
@@ -1,6 +1,21 @@
 // Jump Game
 
 class Solution {
+    public boolean canJump(int[] nums) {
+
+        int maxReach = 0;
+
+        for (int i = 0; i < nums.length && i <= maxReach; i++) {
+            maxReach = Math.max(maxReach, i + nums[i]);
+        }
+
+        return maxReach >= nums.length - 1;
+    }
+}
+
+// ===============================================================
+
+class Solution {
 public:
 
     bool canJump(vector<int>& nums) {

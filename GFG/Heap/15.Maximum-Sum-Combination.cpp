035323c88b056ee2aeb26622d8aa88c395db6f8e
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2026-02-15 16:48:07 +0530

    Update 15.Maximum-Sum-Combination.cpp

--- GFG/Heap/15.Maximum-Sum-Combination.cpp
+++ GFG/Heap/15.Maximum-Sum-Combination.cpp
@@ -1,6 +1,55 @@
 // Maximum Sum Combination
 
 class Solution {
+
+    public ArrayList<Integer> topKSumPairs(int[] A, int[] B, int k) {
+
+        int n = A.length;
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+
+        // Max Heap (based on sum)
+        PriorityQueue<int[]> maxHeap = new PriorityQueue<>((a, b) -> Integer.compare(b[0], a[0]));
+
+        Set<String> visited = new HashSet<>();
+
+        // Push largest pair
+        maxHeap.offer(new int[]{A[n-1] + B[n-1], n-1, n-1});
+        visited.add((n-1) + "#" + (n-1));
+
+        ArrayList<Integer> result = new ArrayList<>();
+
+        while (k-- > 0 && !maxHeap.isEmpty()) {
+
+            int[] curr = maxHeap.poll();
+
+            int sum = curr[0];
+            int i = curr[1];
+            int j = curr[2];
+
+            result.add(sum);
+
+            // Push (i-1, j)
+            if (i - 1 >= 0 && !visited.contains((i-1) + "#" + j)) {
+                maxHeap.offer(new int[]{A[i-1] + B[j], i-1, j});
+                visited.add((i-1) + "#" + j);
+            }
+
+            // Push (i, j-1)
+            if (j - 1 >= 0 && !visited.contains(i + "#" + (j-1))) {
+                maxHeap.offer(new int[]{A[i] + B[j-1], i, j-1});
+                visited.add(i + "#" + (j-1));
+            }
+        }
+
+        return result;
+    }
+}
+
+// =====================================================================
+
+class Solution {
   public:
     vector<int> maxCombinations(int N, int K, vector<int> &A, vector<int> &B) {
         // code here

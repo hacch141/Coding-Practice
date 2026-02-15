7a83db30cdb3f027137c0382826d03d4f4897223
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2026-02-15 15:06:32 +0530

    Update 11.Hand-of-Straights.cpp

--- GFG/Heap/11.Hand-of-Straights.cpp
+++ GFG/Heap/11.Hand-of-Straights.cpp
@@ -1,6 +1,46 @@
 // Hand of Straights
 
 class Solution {
+
+    public boolean isNStraightHand(int[] hand, int groupSize) {
+
+        if (hand.length % groupSize != 0)
+            return false;
+
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+
+        for (int card : hand) {
+            map.put(card, map.getOrDefault(card, 0) + 1);
+        }
+
+        while (!map.isEmpty()) {
+
+            int first = map.firstKey();
+            int freq = map.get(first);   // how many groups to form
+
+            for (int i = 0; i < groupSize; i++) {
+
+                int curr = first + i;
+
+                if (!map.containsKey(curr) || map.get(curr) < freq)
+                    return false;
+
+                int newCount = map.get(curr) - freq;
+
+                if (newCount == 0)
+                    map.remove(curr);
+                else
+                    map.put(curr, newCount);
+            }
+        }
+
+        return true;
+    }
+}
+
+// ============================================================================
+
+class Solution {
 public:
     bool isNStraightHand(vector<int>& hand, int groupSize) {
         if(hand.size()%groupSize != 0) return false;

329e1cc64d78af7ca0088e5d52a835798d1d5966
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2026-02-15 15:29:58 +0530

    Update 16.Median-in-a-stream.cpp

--- GFG/Heap/16.Median-in-a-stream.cpp
+++ GFG/Heap/16.Median-in-a-stream.cpp
@@ -1,5 +1,34 @@
 // Median in a stream
 
+class KthLargest {
+
+    private PriorityQueue<Integer> minHeap;
+    private int k;
+
+    public KthLargest(int k, int[] nums) {
+
+        this.k = k;
+        minHeap = new PriorityQueue<>();
+
+        for (int num : nums) {
+            add(num);
+        }
+    }
+
+    public int add(int val) {
+
+        minHeap.offer(val);
+
+        if (minHeap.size() > k) {
+            minHeap.poll();
+        }
+
+        return minHeap.peek();
+    }
+}
+
+// =========================================================================
+
 #include <bits/stdc++.h>
 
 vector<int> findMedian(vector<int> &arr, int n) {

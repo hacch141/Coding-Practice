b3a556d8b8dfcc5fa65b8e36b60f92b09a91ff35
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2026-02-15 11:18:14 +0530

    Update 06.Kth-smallest-element-in-an-array.cpp

--- GFG/Heap/06.Kth-smallest-element-in-an-array.cpp
+++ GFG/Heap/06.Kth-smallest-element-in-an-array.cpp
@@ -1,5 +1,35 @@
 // Kth smallest element in an array
 
+class Solution {
+
+    public int[] kSmallest(int[] arr, int n, int k) {
+
+        // Max Heap (reverse order)
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
+
+        for (int i = 0; i < n; i++) {
+
+            pq.offer(arr[i]);
+
+            // Maintain heap size <= k
+            if (pq.size() > k) {
+                pq.poll();
+            }
+        }
+
+        int[] result = new int[k];
+        int index = 0;
+
+        while (!pq.isEmpty()) {
+            result[index++] = pq.poll();
+        }
+
+        return result;
+    }
+}
+
+// ==================================================================
+
 #include <bits/stdc++.h>
 
 vector<int> kSmallset(int arr[], int n, int k) {

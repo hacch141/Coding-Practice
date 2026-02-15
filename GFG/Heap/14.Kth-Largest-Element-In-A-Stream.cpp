a453aafdf5649ee800b7488ac463d3af14289877
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2026-02-15 15:44:57 +0530

    Update 14.Kth-Largest-Element-In-A-Stream.cpp

--- GFG/Heap/14.Kth-Largest-Element-In-A-Stream.cpp
+++ GFG/Heap/14.Kth-Largest-Element-In-A-Stream.cpp
@@ -1,4 +1,33 @@
-// Kth Largest Element In A Stream
+/// Kth Largest Element In A Stream
+
+class KthLargest {
+
+    private PriorityQueue<Integer> minHeap;
+    private int k;
+
+    public KthLargest(int k, int[] nums) {
+
+        this.k = k;
+        minHeap = new PriorityQueue<>();
+
+        for (int num : nums) {
+            add(num);
+        }
+    }
+
+    public int add(int val) {
+
+        minHeap.offer(val);
+
+        if (minHeap.size() > k) {
+            minHeap.poll();
+        }
+
+        return minHeap.peek();
+    }
+}
+
+// ==========================================================================
 
 #include <algorithm>
 

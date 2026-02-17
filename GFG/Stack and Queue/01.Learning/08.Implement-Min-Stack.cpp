ad7d45818f30e52f2f493bf5849fbcd8dd3fba2c
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2026-02-17 20:24:03 +0530

    Update 08.Implement-Min-Stack.cpp

--- GFG/Stack and Queue/01.Learning/08.Implement-Min-Stack.cpp
+++ GFG/Stack and Queue/01.Learning/08.Implement-Min-Stack.cpp
@@ -1,5 +1,38 @@
 // Implement Min Stack
 
+class MinStack {
+
+    Stack<int[]> stack;
+
+    public MinStack() {
+        stack = new Stack<>();
+    }
+
+    public void push(int val) {
+
+        if (stack.isEmpty()) {
+            stack.push(new int[]{val, val});
+        } else {
+            int currentMin = Math.min(val, stack.peek()[1]);
+            stack.push(new int[]{val, currentMin});
+        }
+    }
+
+    public void pop() {
+        stack.pop();
+    }
+
+    public int top() {
+        return stack.peek()[0];
+    }
+
+    public int getMin() {
+        return stack.peek()[1];
+    }
+}
+
+// ===========================================================================
+
 #include <bits/stdc++.h> 
 // Implement class for minStack.
 class minStack

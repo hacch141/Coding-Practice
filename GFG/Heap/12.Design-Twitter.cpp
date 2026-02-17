39b9f6f2e77eb4f34e82c98b1ecdd6712828e832
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2026-02-17 10:28:19 +0530

    Update 12.Design-Twitter.cpp

--- GFG/Heap/12.Design-Twitter.cpp
+++ GFG/Heap/12.Design-Twitter.cpp
@@ -1,6 +1,72 @@
 // Design Twitter
 
 class Twitter {
+    Map<Integer,Set<Integer>> following;
+    Map<Integer,List<int[]>> post;
+    int time;
+
+    public Twitter() {
+        following = new HashMap<>();
+        post = new HashMap<>();
+        time = 1;
+    }
+    
+    public void postTweet(int userId, int tweetId) {
+        post.computeIfAbsent(userId, k -> new ArrayList<>()).add(new int[]{time, tweetId});
+        time++;
+    }
+    
+    public List<Integer> getNewsFeed(int userId) {
+        // <time, tweetId, tweetUserId, Idx>
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> b[0] - a[0]);
+
+        if (post.containsKey(userId)) {
+            List<int[]> p =  post.get(userId);
+            int[] lastPost = p.get(p.size() - 1);
+            pq.add(new int[]{lastPost[0], lastPost[1], userId, p.size() - 1});
+        }
+
+        if (following.containsKey(userId)) {
+            for (int f : following.get(userId)) {
+                if (post.containsKey(f)) {
+                    List<int[]> p =  post.get(f);
+                    int[] lastPost = p.get(p.size() - 1);
+                    pq.add(new int[]{lastPost[0], lastPost[1], f, p.size() - 1});
+                }
+            }
+        }
+
+        List<Integer> feed = new ArrayList<>();
+        while (feed.size() < 10 && !pq.isEmpty()) {
+            int[] curr = pq.poll();
+            int tweetId = curr[1];
+            int tweetUserId = curr[2];
+            int idx = curr[3];
+            feed.add(tweetId);
+            if (idx > 0) {
+                List<int[]> p =  post.get(tweetUserId);
+                int[] lastPost = p.get(idx - 1);
+                pq.add(new int[]{lastPost[0], lastPost[1], tweetUserId, idx - 1});
+            }
+        }
+
+        return feed;
+    }
+    
+    public void follow(int followerId, int followeeId) {
+        following.computeIfAbsent(followerId, k -> new HashSet<>()).add(followeeId);
+    }
+    
+    public void unfollow(int followerId, int followeeId) {
+        if (following.containsKey(followerId)) {
+            following.get(followerId).remove(followeeId);
+        }
+    }
+}
+
+// =======================================================================================
+
+class Twitter {
 public:
     unordered_map<int,unordered_set<int>> friends;
     priority_queue<vector<int>> post;

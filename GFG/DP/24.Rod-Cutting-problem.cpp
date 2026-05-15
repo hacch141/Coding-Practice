257ab363a1fd5856fc0a49da1f0d794af3884efd
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2026-05-15 23:05:45 +0530

    Update 24.Rod-Cutting-problem.java

--- GFG/DP/24.Rod-Cutting-problem.cpp
+++ GFG/DP/24.Rod-Cutting-problem.cpp
@@ -1,106 +1,27 @@
 // Rod Cutting
 
-// Memoization
-class Solution{
+class Solution {
+    int INF = (int)1e9;
+    int[][] dp;
     
-  private:
-    int solve(int idx, int price[], int n, vector<vector<int>>& dp) {
-        if(idx==0) return n*price[0];
-        if(dp[idx][n] != -1) return dp[idx][n];
-        int notpick = solve(idx-1,price,n,dp);
-        int pick = INT_MIN;
-        int len = idx+1;
-        if(len <= n) {
-            pick = price[idx] + solve(idx,price,n-len,dp);
-        }
-        return dp[idx][n] = max(pick,notpick);
+    public int solve(int ind, int n, int[] price, int curr) {
+        if (curr < 0) return -INF;
+        if (ind == n) return curr == 0 ? 0 : -INF;
+        
+        if (dp[ind][curr] != -1) return dp[ind][curr];
+        
+        int take = price[ind] + solve(ind, n, price, curr - (ind + 1));
+        int nottake = solve(ind + 1, n, price, curr);
+        return dp[ind][curr] = Math.max(take, nottake);
     }
     
-  public:
-    int cutRod(int price[], int n) {
-        //code here
-        vector<vector<int>> dp(n, vector<int> (n+1,-1));
-        return solve(n-1,price,n,dp);
+    public int cutRod(int[] price) {
+        // code here
+        int n = price.length;
+        
+        dp = new int[n][n + 1];
+        for (int i = 0; i < n; i++) Arrays.fill(dp[i], -1);
+
+        return solve(0, n, price, n);
     }
-};
-
-
-
-// Tabulation
-class Solution{
-  public:
-    int cutRod(int price[], int n) {
-        //code here
-        vector<vector<int>> dp(n, vector<int> (n+1,-1));
-        
-        for(int i=0; i<=n; i++) {
-            dp[0][i] = i * price[0];
-        }
-        
-        for(int i=1; i<n; i++) {
-            for(int len=0; len<=n; len++) {
-                int notpick = 0 + dp[i-1][len];
-                int pick = INT_MIN;
-                if(i+1 <= len) {
-                    pick = price[i] + dp[i][len-(i+1)];
-                }
-                dp[i][len] = max(pick, notpick);
-            }
-        }
-        return dp[n-1][n];
-    }
-};
-
-
-
-// space optimization
-class Solution{
-  public:
-    int cutRod(int price[], int n) {
-        //code here
-        vector<int> dp(n+1,0), curr(n+1,0);
-        
-        for(int i=0; i<=n; i++) {
-            dp[i] = i * price[0];
-        }
-        
-        for(int i=1; i<n; i++) {
-            for(int len=0; len<=n; len++) {
-                int notpick = 0 + dp[len];
-                int pick = INT_MIN;
-                if(i+1 <= len) {
-                    pick = price[i] + curr[len-(i+1)];
-                }
-                curr[len] = max(pick, notpick);
-            }
-            dp =curr;
-        }
-        return dp[n];
-    }
-};
-
-
-// only in one array
-class Solution{
-  public:
-    int cutRod(int price[], int n) {
-        //code here
-        vector<int> dp(n+1,0);
-        
-        for(int i=0; i<=n; i++) {
-            dp[i] = i * price[0];
-        }
-        
-        for(int i=1; i<n; i++) {
-            for(int len=0; len<=n; len++) {
-                int notpick = 0 + dp[len];
-                int pick = INT_MIN;
-                if(i+1 <= len) {
-                    pick = price[i] + dp[len-(i+1)];
-                }
-                dp[len] = max(pick, notpick);
-            }
-        }
-        return dp[n];
-    }
-};
+}

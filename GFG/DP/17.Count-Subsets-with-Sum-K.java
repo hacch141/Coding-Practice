dff271c6c365d426b0d9f75f3b45a740461a4174
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2026-05-15 19:51:26 +0530

    Update 17.Count-Subsets-with-Sum-K.java

--- GFG/DP/17.Count-Subsets-with-Sum-K.java
+++ GFG/DP/17.Count-Subsets-with-Sum-K.java
@@ -20,3 +20,31 @@ class Solution {
         return dp[target];
     }
 }
+
+class Solution {
+    int[][] dp;
+    
+    // Function to calculate the number of subsets with a given sum
+    public int solve(int ind, int n, int[] nums, int target) {
+        if (target < 0) return 0;
+        if (ind == n) return target == 0 ? 1 : 0;
+        
+        if (dp[ind][target] != -1) return dp[ind][target];
+        
+        int take = solve(ind + 1, n, nums, target - nums[ind]);
+        int nottake = solve(ind + 1, n, nums, target);
+        return dp[ind][target] = take + nottake;
+    }
+    
+    public int perfectSum(int[] nums, int target) {
+        // code here
+        int n = nums.length;
+
+        dp = new int[n][target + 1];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dp[i], -1);
+        }
+
+        return solve(0, n, nums, target);
+    }
+}

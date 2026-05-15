f5645fd867a9fa4f18d0600b9ad43866e98d93a2
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2026-05-15 12:57:05 +0530

    Update 07.Geek's-Training.java

--- GFG/DP/07.Geek's-Training.cpp
+++ GFG/DP/07.Geek's-Training.cpp
@@ -2,110 +2,28 @@
 
 // Memoization
 class Solution {
-  public:
-  
-    int solve(int idx, int prev, vector<vector<int>>& points, int& n, vector<vector<int>>& dp) {
-        if(idx==0) {
-            int maxi = 0;
-            for(int task=0; task<3; task++) {
-                if(task!=prev) {
-                    maxi = max(maxi,points[0][task]);
-                }
-            }
-            return dp[0][prev] = maxi;
-        }
-        if(dp[idx][prev]!=-1) return dp[idx][prev];
+    public int maximumPoints(int mat[][]) {
+        // code here
+        int n = mat.length;
+        int[][] dp = new int[n][3];
         
-        int maxi = 0;
-        for(int task=0; task<3; task++) {
-            if(task!=prev) {
-                maxi = max(maxi, (points[idx][task] + solve(idx-1,task,points,n,dp)) );
-            }
-        }
-        return dp[idx][prev] = maxi;
-    }
-  
-    int maximumPoints(vector<vector<int>>& points, int n) {
-        // Code here
-        vector<vector<int>> dp(n, vector<int>(4,-1));
-        return solve(n-1,3,points,n,dp);
-    }
-};
-// T : O(N*4*3)
-// S : O(N*4)
-
-
-// =================================================================================
-
-
-// Tabulation
-class Solution {
-  public:
-  
-    int maximumPoints(vector<vector<int>>& points, int n) {
-        // Code here
-        vector<vector<int>> dp(n, vector<int> (4,0));
-        for(int act=0; act<4; act++) {
-            for(int i=0; i<3; i++) {
-                if(i!=act) {
-                    dp[0][act] = max(dp[0][act],points[0][i]);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (i == 0) {
+                    dp[i][j] = mat[i][j];
                 }
-            }
-        }
-        
-        for(int day=1; day<n; day++) {
-            for(int act=0; act<4; act++) {
-                for(int i=0; i<3; i++) {
-                    if(i != act) {
-                        int point = points[day][i] + dp[day-1][i];
-                        dp[day][act] = max(dp[day][act],point);
+                else {
+                    int prev = 0;
+                    for (int k = 0; k < 3; k++) {
+                        if (k != j) {
+                            prev = Math.max(prev, dp[i - 1][k]);
+                        }
                     }
+                    dp[i][j] = mat[i][j] + prev;
                 }
             }
         }
         
-        return dp[n-1][3];
+        return Math.max(Math.max(dp[n - 1][0], dp[n - 1][1]), dp[n - 1][2]);
     }
-};
-// T : O(N*4*3)
-// S : O(N*4)
-
-
-// =================================================================================
-
-
-// Space Optimization
-class Solution {
-  public:
-  
-    int maximumPoints(vector<vector<int>>& points, int n) {
-        // Code here
-        vector<int> dp(4,0), prev(4,0);
-        for(int act=0; act<4; act++) {
-            for(int i=0; i<3; i++) {
-                if(i!=act) {
-                    dp[act] = max(dp[act],points[0][i]);
-                }
-            }
-        }
-        
-        for(int day=1; day<n; day++) {
-            for(int act=0; act<4; act++) {
-                for(int i=0; i<3; i++) {
-                    if(i != act) {
-                        int point = points[day][i] + dp[i];
-                        prev[act] = max(prev[act],point);
-                    }
-                }
-            }
-            dp = prev;
-        }
-        
-        return dp[3];
-    }
-};
-// T : O(N*4*3)
-// S : O(4*2)
-
-
-// https://takeuforward.org/data-structure/dynamic-programming-ninjas-training-dp-7/
+}

2da17becc45c10d88752a3b604a6ae5892a5a0f3
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2026-05-15 13:50:15 +0530

    Update and rename 11.Triangle-Path-Sum.cpp to 11.Triangle-Path-Sum.java

---
+++ GFG/DP/11.Triangle-Path-Sum.java
@@ -0,0 +1,14 @@
+// Triangle Path Sum
+
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        for(int i = n - 2; i >= 0; i--) {
+            for(int j = 0; j < triangle.get(i).size(); j++) {
+                int new_val = triangle.get(i).get(j) + Math.min(triangle.get(i + 1).get(j), triangle.get(i + 1).get(j + 1));
+                triangle.get(i).set(j, new_val);
+            }
+        }
+        return triangle.get(0).get(0);
+    }
+}

fdef38b5d2eec7ff8c9f8480dd7949e7c6cbb6d8
Harsh Chaudhari <86292378+hacch141@users.noreply.github.com>
2026-05-17 16:15:33 +0530

    Create 56.Count-Square-Submatrices-with-All-Ones.java

---
+++ GFG/DP/56.Count-Square-Submatrices-with-All-Ones.java
@@ -0,0 +1,21 @@
+// Count Square Submatrices with All Ones
+
+class Solution {
+    public int countSquares(int[][] matrix) {
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int ans = 0;
+        int[][] dp = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (i == 0 || j == 0) {
+                    dp[i][j] = matrix[i][j];
+                } else if (matrix[i][j] == 1) {
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j], Math.min(dp[i - 1][j - 1], dp[i][j - 1]));
+                }
+                ans += dp[i][j];
+            }
+        }
+        return ans;
+    }
+}
